63d6ac707aa8ac90b67669985bcd78063510b610
arkingco <arkingco@gmail.com>
2022-06-03 17:29:00 +0900

    [feat] 경기컴포넌트리스트

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,23 @@
 export interface Rank {
-    rank: number;
-    userId: string;
-    ppp: number;
-    statusMessage: string;
-    winRate: number;
+  rank: number;
+  userId: string;
+  ppp: number;
+  statusMessage: string;
+  winRate: number;
+}
+export interface player {
+  userId: string;
+  userImageUri: string;
+  wins: number;
+  losses: number;
+  winRate: number;
+  pppChange: number;
+}
+export interface matches {
+  matchId: number;
+  type?: string;
+  time?: string;
+  status?: string;
+  team1: player;
+  team2: player;
 }
