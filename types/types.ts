d00a6bc800f92acddac3cb8d3263edbf9e1e5dc8
arkingco <arkingco@gmail.com>
2022-06-04 14:18:39 +0900

    [Docs] 타입 페이지 수정

--- types/types.ts
+++ types/types.ts
@@ -5,19 +5,3 @@ export interface Rank {
   statusMessage: string;
   winRate: number;
 }
-export interface player {
-  userId: string;
-  userImageUri: string;
-  wins: number;
-  losses: number;
-  winRate: number;
-  pppChange: number;
-}
-export interface matches {
-  matchId: number;
-  type?: string;
-  time?: string;
-  status?: string;
-  team1: player;
-  team2: player;
-}
