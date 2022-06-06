99edfc966b4856edef75ee2004c07423ba67c1ef
su1715 <su1715@sookmyung.ac.kr>
2022-06-06 17:47:20 +0900

    [Fix]table폴더 tables로 수정

--- pages/match.tsx
+++ pages/match.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import MatchBoardList from "../components/MatchBoardList";
 import { MatchData } from "../types/matchTypes";
-import { getData } from "../util/getData";
+import { getData } from "../utils/getData";
 
 export default function Match() {
   const [matchData, setMatchData] = useState<MatchData | null>(null);
   useEffect(() => {
     (async () => {
       try {
-        const data = await getData(`/pingpong/match/table/${1}?type=single`);
+        const data = await getData(`/pingpong/match/tables/${1}?type=single`);
         setMatchData(data);
       } catch (e) {
         console.log(e);
