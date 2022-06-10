f2e5a37a90150845731c25a3df5297e1681e08a0
pearpearB <smilyoung@gmail.com>
2022-06-10 17:41:23 +0900

    [Feat] 메인페이지 부분검색 구현하기 #21

--- components/main/SearchBar.tsx
+++ components/main/SearchBar.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react';
-import { SearchData } from '../../types/mainType';
+import React, { useState } from 'react';
 import { getData } from '../../utils/axios';
 
 export default function SearchBar() {
   const [keyword, setKeyword] = useState<string>('');
-  const [showSearchBox, setShowSearchBox] = useState<boolean>(false);
-  const [searchResult, setSearchResult] = useState<SearchData | null>(null);
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const [searchResult, setSearchResult] = useState<string[] | null>(null);
 
-  const showSearchBoxHandler = () => {
-    setShowSearchBox(!showSearchBox);
+  const showDropDownHandler = () => {
+    setShowDropDown(!showDropDown);
   };
 
   const keywordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(event.target.value);
   };
 
+  const getSearchResultHandler = async (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (keyword) {
+      try {
+        const data = await getData(`/pingpong/users/searches/${keyword}`);
+        setSearchResult(data.users);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }; // keyword.replace(/[^a-zA-Z0-9\-]/g, ''); 정규표현식 적용하기
+
   return (
     <div>
       <input
         type='text'
+        value={keyword}
         onChange={keywordHandler}
-        onFocus={showSearchBoxHandler}
-        onBlur={showSearchBoxHandler}
+        onKeyUp={getSearchResultHandler}
+        onFocus={showDropDownHandler}
+        onBlur={showDropDownHandler}
         placeholder='챔피온 검색하기'
       />
       <button>&#128269;</button>
-      {showSearchBox && (
+      {showDropDown && (
         <div>
           {searchResult
-            ? searchResult.users.map((item: string) => <div>{item}</div>)
+            ? searchResult?.map((item, i) => <div key={i}>{item}</div>)
             : '검색 결과가 없습니다.'}
         </div>
       )}
