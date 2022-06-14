4ba6533bb35e0d6ae393e24f922871e0ccd757ea
pearpearB <smilyoung@gmail.com>
2022-06-14 15:21:28 +0900

    [Feat] 추가된 라이브데이터 리코일 추가 #1

--- utils/recoil/main.ts
+++ utils/recoil/main.ts
@@ -1,8 +1,13 @@
 import { atom } from 'recoil';
 import { v1 } from 'uuid';
-import { UserData } from '../../types/mainType';
+import { UserData, LiveData } from '../../types/mainType';
 
-export const userState = atom<UserData | null>({
+export const userState = atom<UserData>({
   key: `userState/${v1()}`, // unique ID (다른 atoms/selectors을 구별하기 위해서)
-  default: null, // default value (aka initial value)
+  default: { userId: '', userImageUri: '' }, // default value (aka initial value)
+});
+
+export const liveState = atom<LiveData>({
+  key: `liveState/${v1()}`,
+  default: { notiCount: 0, gameId: null },
 });
