c88857099966890bc6e94a1fad315f349494a704
tamagoyakii <rlawlgus4063@naver.com>
2022-06-16 13:41:38 +0900

    [Feat] 서버 api 연결

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const END_POINT = 'http://localhost:3000';
+// const END_POINT = 'http://localhost:3000';
+const END_POINT = 'http://211.253.28.235:9090';
 
 export const getData = async (path: string) => {
   try {
-    const res = await axios.get(`${END_POINT}/api${path}`);
+    // const res = await axios.get(`${END_POINT}/api${path}`);
+    const res = await axios.get(`${END_POINT}${path}`);
     if (res.statusText !== 'OK') throw new Error('네트워크 에러');
     return await res.data;
   } catch (e) {
@@ -14,7 +16,8 @@ export const getData = async (path: string) => {
 
 export const postData = async (path: string, body: Object) => {
   try {
-    const res = await axios.post(`${END_POINT}/api${path}`, body);
+    // const res = await axios.post(`${END_POINT}/api${path}`, body);
+    const res = await axios.post(`${END_POINT}${path}`, body);
     if (Math.round(res.status / 200) !== 1) throw new Error('네트워크 에러');
     return await res.status;
   } catch (e) {
@@ -24,7 +27,8 @@ export const postData = async (path: string, body: Object) => {
 
 export const deleteData = async (path: string) => {
   try {
-    const res = await axios.delete(`${END_POINT}/api${path}`);
+    // const res = await axios.delete(`${END_POINT}/api${path}`);
+    const res = await axios.delete(`${END_POINT}${path}`);
     if (res.statusText !== 'OK') throw new Error('네트워크 에러');
     return await res.data;
   } catch (e) {
