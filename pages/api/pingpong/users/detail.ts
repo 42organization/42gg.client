e2c48712ef5625bd83295cb457af2f2faaecf8fc
su1715 <su1715@sookmyung.ac.kr>
2022-06-22 16:50:43 +0900

    [fix] EditProfile API 수정사항 반영

--- pages/api/pingpong/users/detail.ts
+++ pages/api/pingpong/users/detail.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ProfileInfo } from './../../../../../types/userTypes';
+import { ProfileInfo } from '../../../../types/userTypes';
 
 let profile = {
   userId: '',
