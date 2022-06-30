e769740e9ef67df7478f30ea1643f2fd950b20d4
arkingco <arkingco@gmail.com>
2022-06-30 19:13:58 +0900

    [Feat] 게임 데이터가 없을 때(빈 배열) 빈 결과 컴포넌트 리턴 게임 데이터가 없다는 기준을 GameResultList 페이지에서 캐치해서 리턴 보내준다

---
+++ components/game/GameResultEmptyArray.tsx
@@ -0,0 +1,11 @@
+import styles from 'styles/game/GameResultEmpty.module.scss';
+
+export default function GameResultEmptyArray() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Record</div>
+      <div className={styles.emoji}>🤔</div>
+      <div className={styles.gameStatusMessage}>게임 결과가 없습니다</div>
+    </div>
+  );
+}
