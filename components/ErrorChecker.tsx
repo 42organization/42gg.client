95204f4678a2b0a4f7396c09d77ac5af92527fb8
KimDae-hyun <dhkim5608@naver.com>
2022-07-07 19:08:23 +0900

    [Feat] Login Checker 추가하여 Layout과 분리

--- components/ErrorChecker.tsx
+++ components/ErrorChecker.tsx
@@ -1,6 +1,6 @@
 import { errorState } from 'utils/recoil/error';
 import { useRecoilValue } from 'recoil';
-import ErrorPage from './ErrorPage';
+import ErrorPage from 'pages/error';
 import styles from 'styles/Layout/Layout.module.scss';
 
 interface ErrorCheckerProps {
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import { useSetRecoilState } from 'recoil';
 import { useEffect } from 'react';
 import { errorState } from 'utils/recoil/error';
-import ErrorPage from 'components/ErrorPage';
+import ErrorPage from './error';
 
 const Error = () => {
   const setErrorMessage = useSetRecoilState(errorState);
