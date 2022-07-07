db894fa14a9c9516a8417949cdc0e8b0a98d808d
tamagoyakii <rlawlgus4063@naver.com>
2022-07-07 18:09:08 +0900

    [Feat] 페이지 메뉴얼로 가는 링크 추가 #251

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,14 +1,24 @@
-import Link from 'next/link';
 import styles from 'styles/Layout/Footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
-      <div>
-        <a href='https://github.com/42organization'>github |</a>
-        <Link href={`/manual`}> manual</Link>
+      <div className={styles.link}>
+        <div onClick={() => window.open('https://github.com/42organization')}>
+          Github
+        </div>
+        <div className={styles.bar}>|</div>
+        <div
+          onClick={() =>
+            window.open(
+              'https://github.com/42organization/42arcade.gg.client#주요-기능-소개'
+            )
+          }
+        >
+          Page Manual
+        </div>
       </div>
-      <div>© 2022 42GG</div>
+      <div className={styles.copyright}>© 2022 42GG</div>
     </footer>
   );
 }
