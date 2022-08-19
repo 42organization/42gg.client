af743b801e8209ea1d6d925fe4efb45947063434
tamagoyakii <rlawlgus4063@naver.com>
2022-08-19 21:08:01 +0900

    [Feat] current Match 문구 추가 및 임포트 순서 변경 #350

--- pages/users/detail.tsx
+++ pages/users/detail.tsx
@@ -1,11 +1,11 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSetRecoilState } from 'recoil';
 import { modalState } from 'utils/recoil/modal';
 import Profile from 'components/user/Profile';
 import GameResult from 'components/game/GameResult';
-import styles from 'styles/user/user.module.scss';
-import { useEffect } from 'react';
 import RankProfile from 'components/user/RankProfile';
+import styles from 'styles/user/user.module.scss';
 
 export default function user() {
   const setModalInfo = useSetRecoilState(modalState);
@@ -23,6 +23,7 @@ export default function user() {
           <h1 className={styles.title}>{intraId}</h1>
           <Profile intraId={intraId} />
           <RankProfile intraId={intraId} />
+          <h2 className={styles.subtitle}>current match</h2>
           <GameResult intraId={intraId} />
         </>
       )}
