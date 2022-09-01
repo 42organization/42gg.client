4a326002704736914f1d6b9459de75b4e914e8e2
pearpearB <smilyoung@gmail.com>
2022-09-01 16:00:24 +0900

    [Feat] utils/handleTime 함수들 JSDoc 적용 #365

--- utils/handleTime.ts
+++ utils/handleTime.ts
@@ -1,18 +1,34 @@
+/**
+ * width 길이만큼 origin 앞에 0을 채움
+ * @param {string} origin 시간 혹은 분
+ * @param {number} width 원하는 길이
+ * @return 0이 채워진 문자열
+ * */
 export const fillZero = (origin: string, width: number) => {
   const fillLen = width - origin.length;
   return (fillLen > 0 ? new Array(fillLen).fill(0).join('') : '') + origin;
 };
 
+/**
+ * 시간 문자열을 받아 HH:MM 형식의 게임 시작 시간으로 반환
+ * @param {Date | string} d 시간
+ * @return 게임 시작 시간(HH:MM)
+ * */
 export const gameTimeToString = (d: Date | string) => {
   const date = typeof d === 'string' ? new Date(d) : d;
   const hour = date.getHours();
   const min = date.getMinutes();
   const hourString = fillZero(hour.toString(), 2);
   const minString = fillZero(min.toString(), 2);
-
   return `${hourString}:${minString}`;
 };
 
+/**
+ * 현재시간이 게임시작까지 일정 시간(minute/주로 5분) 이내로 남았는지 확인
+ * @param {string} gameTimeString 게임시간
+ * @param {number} min 분
+ * @return Boolean
+ * */
 export const isBeforeMin = (gameTimeString: string, min: number) => {
   const gameTime = new Date(gameTimeString);
   const afterMin = new Date();
@@ -20,22 +36,12 @@ export const isBeforeMin = (gameTimeString: string, min: number) => {
   return gameTime.getTime() <= afterMin.getTime();
 };
 
-export const dateToString = (d: Date) => {
-  const year = d.getFullYear();
-  const month = d.getMonth() + 1;
-  const date = d.getDate();
-  const hour = d.getHours();
-  const min = d.getMinutes();
-
-  return `${year}-${month}-${date} ${hour}:${min}`;
-};
-
-export const minuitesAgo = (min: number) => {
-  const date = new Date();
-  date.setMinutes(date.getMinutes() - min);
-  return dateToString(date);
-};
-
+/**
+ * 현재시간이 게임시간으로부터 일정 시간(minute)이 지났는지 확인
+ * @param {string} gameTimeString 게임시간
+ * @param {number} min 분
+ * @return Boolean
+ * */
 export const isAfterMin = (gameTimeString: string, min: number) => {
   const gameTime = new Date(gameTimeString);
   const beforeMin = new Date();
@@ -43,10 +49,46 @@ export const isAfterMin = (gameTimeString: string, min: number) => {
   return gameTime.getTime() <= beforeMin.getTime();
 };
 
+/**
+ * 시간을 YYYY-MM-DD HH:MM 형식으로 반환
+ * @param {Date} d 시간 문자열
+ * @return 시간(YYYY-MM-DD HH:MM)
+ * */
+export const dateToString = (d: Date) => {
+  const year = d.getFullYear();
+  const month = d.getMonth() + 1;
+  const date = d.getDate();
+  const hour = d.getHours();
+  const min = d.getMinutes();
+  return `${year}-${month}-${date} ${hour}:${min}`;
+};
+
+/**
+ * 현재 시간에서 특정 분(minute) 전 시간을 반환
+ * @description 목업 데이터를 위해 임시로 만든 함수
+ * @param {number} min 분(minute)
+ * @return 몇 분 전 시간(YYYY-MM-DD HH:MM)
+ * */
+export const minuitesAgo = (min: number) => {
+  const date = new Date();
+  date.setMinutes(date.getMinutes() - min);
+  return dateToString(date);
+};
+
+/**
+ * 게임시간으로부터 현재까지 경과된 시간을 초(second) 단위로 반환
+ * @param {string} gameTime 게임종료 시간
+ * @return 초
+ * */
 export const getElapsedTimeSeconds = (gameTime: string) => {
   return (Number(new Date()) - Number(new Date(gameTime))) / 1000;
 };
 
+/**
+ * 게임종료 시간을 받아 현재 시간으로부터 몇 년/개월/일/시간/분/방금 전인지 반환
+ * @param {string} gameTime 게임종료 시간
+ * @return 현재 시간으로부터 얼마 전
+ * */
 export const getTimeAgo = (gameTime: string) => {
   const elapsedTimeSeconds = getElapsedTimeSeconds(gameTime) - 60 * 10;
   const timeUnits = [
@@ -56,7 +98,6 @@ export const getTimeAgo = (gameTime: string) => {
     { unit: '시간', second: 60 * 60 },
     { unit: '분', second: 60 },
   ];
-
   for (const timeUnit of timeUnits) {
     const elapsedTime = Math.floor(elapsedTimeSeconds / timeUnit.second);
     if (elapsedTime > 0) return `${elapsedTime}${timeUnit.unit} 전`;
