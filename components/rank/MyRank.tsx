12ceaf161c1f51a087a020e1c3c73d8dbeb337aa
pearpearB <smilyoung@gmail.com>
2022-09-03 20:34:00 +0900

    [Fix] 랭크페이지 코드 정리

--- components/rank/MyRank.tsx
+++ components/rank/MyRank.tsx
@@ -1,10 +1,10 @@
 import { useSetRecoilState, useRecoilValue } from 'recoil';
-import { myRankPosition, isScrollState } from 'utils/recoil/myRank';
+import { myRankPosition, isMyRankScroll } from 'utils/recoil/myRank';
 import styles from 'styles/rank/RankList.module.scss';
 
 export default function MyRank() {
   const myRank = useRecoilValue(myRankPosition);
-  const setIsScroll = useSetRecoilState(isScrollState);
+  const setIsScroll = useSetRecoilState(isMyRankScroll);
 
   return (
     <div>
