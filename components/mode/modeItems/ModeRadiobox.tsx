20966ce167d4cf24da9a06c8df7c448403e3f53b
tamagoyakii <rlawlgus4063@naver.com>
2022-09-03 16:30:22 +0900

    [Feat] modeItem 폴더 추가 #370

---
+++ components/mode/modeItems/ModeRadiobox.tsx
@@ -0,0 +1,33 @@
+import { Mode } from 'types/mainType';
+
+import styles from 'styles/mode/ModeRadiobox.module.scss';
+
+interface ModeRadiobox {
+  mode: Mode;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function ModeRadiobox({ mode, onChange }: ModeRadiobox) {
+  const modes = [
+    { type: 'both', name: '전체' },
+    { type: 'normal', name: '일반' },
+    { type: 'rank', name: '랭크' },
+  ];
+
+  return (
+    <div className={styles.modeButtons}>
+      {modes.map(({ type, name }) => (
+        <label key={type} htmlFor={type} className={styles.modeButton}>
+          <input
+            type='radio'
+            id={type}
+            value={type}
+            onChange={onChange}
+            checked={mode === type}
+          />
+          <div className={styles.modeButtonText}>{name}</div>
+        </label>
+      ))}
+    </div>
+  );
+}
