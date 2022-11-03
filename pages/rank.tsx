e4e57498135efbc555c6324097dffad30c552af9
tamagoyakii <rlawlgus4063@naver.com>
2022-11-03 16:17:53 +0900

    [Style] ranking/vip 색상 다르게 구현 #418

--- pages/rank.tsx
+++ pages/rank.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
-import RankMode from 'components/mode/RankMode';
+import RankMode from 'components/mode/modeWraps/RankMode';
 import MyRank from 'components/rank/MyRank';
 import RankList from 'components/rank/RankList';
 import styles from 'styles/rank/RankList.module.scss';
 
 export default function Rank() {
   const [mode, setMode] = useState('');
-  const makePageName = (mode?: string) => (mode === 'rank' ? 'Ranking' : 'VIP');
+  const makePageName = (mode?: string) => {
+    return mode === 'rank' ? 'Ranking' : 'VIP';
+  };
+
   return (
     <div className={styles.pageWrap}>
-      <h1 className={styles.title}>{makePageName(mode)}</h1>
+      <h1 className={mode === 'rank' ? styles.rankTitle : styles.vipTitle}>
+        {makePageName(mode)}
+      </h1>
       <MyRank />
-      <RankMode setModeProps={setMode}>
+      <RankMode setMode={setMode}>
         <RankList />
       </RankMode>
     </div>
