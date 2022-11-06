3738438b4470472b88ee74b8b9606d6ba3d49e13
pearpearB <smilyoung@gmail.com>
2022-11-06 20:26:38 +0900

    [Refactor] 알림 if else에서 객체로 리팩토링 #451

--- components/Layout/NotiItem.tsx
+++ components/Layout/NotiItem.tsx
@@ -5,63 +5,59 @@ import { openNotiBarState } from 'utils/recoil/layout';
 import { gameTimeToString } from 'utils/handleTime';
 import styles from 'styles/Layout/NotiItem.module.scss';
 
-type NotiItemProps = {
+interface NotiItemProps {
   data: Noti;
-};
+}
 
 export default function NotiItem({ data }: NotiItemProps) {
-  const setOpenNotiBar = useSetRecoilState(openNotiBarState);
-  const noti = data.createdAt.slice(5, 16).replace('T', ' ');
-  const title = makeTitle(data.type);
-  const content = () => {
-    if (data.type === 'imminent' && data.enemyTeam && data.time)
-      return (
-        <>
-          {makeEnemyUsers(data.enemyTeam)}님과 경기{' '}
-          {makeImminentMinute(data.time, data.createdAt)}분 전 입니다.
-          서두르세요!
-        </>
-      );
-    else return makeContent(data);
+  const date = data.createdAt.slice(5, 16).replace('T', ' ');
+  const noti: {
+    [key: string]: { [key: string]: string | JSX.Element | undefined };
+  } = {
+    imminent: {
+      title: '경기 준비',
+      content: makeImminentContent(data.enemyTeam, data.time, data.createdAt),
+    },
+    announce: { title: '공 지', content: makeAnnounceContent(data.message) },
+    matched: {
+      title: '매칭 성사',
+      content: makeContent(data.time, '에 신청한 매칭이 성사되었습니다.'),
+    },
+    canceledbyman: {
+      title: '매칭 취소',
+      content: makeContent(
+        data.time,
+        '에 신청한 매칭이 상대에 의해 취소되었습니다.'
+      ),
+    },
+    canceledbytime: {
+      title: '매칭 취소',
+      content: makeContent(
+        data.time,
+        '에 신청한 매칭이 상대 없음으로 취소되었습니다.'
+      ),
+    },
   };
-
-  const makeEnemyUsers = (enemyTeam: string[]) => {
-    return enemyTeam.map((intraId: string, i: number) => (
-      <span key={intraId} onClick={() => setOpenNotiBar(false)}>
-        <Link href={`/users/detail?intraId=${intraId}`}>{intraId}</Link>
-        {data.enemyTeam && i < data.enemyTeam.length - 1 ? ', ' : ''}
-      </span>
-    ));
-  };
-
-  const makeImminentMinute = (gameTime: string, createdAt: string) => {
-    return Math.floor(
-      (Number(new Date(gameTime)) - Number(new Date(createdAt))) / 60000
-    );
-  };
-
   return (
     <div
       className={data.isChecked ? `${styles.readWrap}` : `${styles.unreadWrap}`}
     >
-      <span className={styles.title}>{title}</span>
-      <div className={styles.content}>{content()}</div>
-      <div className={styles.date}>{noti}</div>
+      <span className={styles.title}>{noti[data.type].title}</span>
+      <div className={styles.content}>{noti[data.type].content}</div>
+      <div className={styles.date}>{date}</div>
     </div>
   );
 }
 
-function makeTitle(type: string) {
-  if (type === 'imminent') return '경기 준비';
-  else if (type === 'matched') return '매칭 성사';
-  else if (type.includes('canceled')) return '매칭 취소';
-  else return '공 지';
+function makeContent(time: string | undefined, message: string) {
+  if (time) return gameTimeToString(time) + message;
 }
 
-function makeHyperlink(message: string) {
-  const url = message.split('https')[1];
-  const content = message.split('https')[0].split('=>')[0];
-  const linkedContent = message.split('https')[0].split('=>')[1];
+function makeAnnounceContent(message: string | undefined) {
+  if (message && !message.includes('https')) return message;
+  const url = message?.split('https')[1];
+  const content = message?.split('https')[0].split('=>')[0];
+  const linkedContent = message?.split('https')[0].split('=>')[1];
   return (
     <>
       {content} {'=>'}
@@ -70,17 +66,32 @@ function makeHyperlink(message: string) {
   );
 }
 
-function makeContent(data: Noti) {
-  if (data.type !== 'announce' && data.time) {
-    const gameTime = gameTimeToString(data.time);
-    if (data.type === 'matched')
-      return `${gameTime}에 신청한 매칭이 성사되었습니다.`;
-    else if (data.type === 'canceledbyman')
-      return `${gameTime}에 신청한 매칭이 상대에 의해 취소되었습니다.`;
-    else if (data.type === 'canceledbytime')
-      return `${gameTime}에 신청한 매칭이 상대 없음으로 취소되었습니다.`;
-  } else {
-    if (data.message?.includes('https')) return makeHyperlink(data.message);
-    else return `${data.message}`;
-  }
+function makeImminentContent(
+  enemyTeam: string[] | undefined,
+  time: string | undefined,
+  createdAt: string
+) {
+  const setOpenNotiBar = useSetRecoilState(openNotiBarState);
+  const makeEnemyUsers = (enemyTeam: string[]) => {
+    return enemyTeam.map((intraId: string, i: number) => (
+      <span key={intraId} onClick={() => setOpenNotiBar(false)}>
+        <Link href={`/users/detail?intraId=${intraId}`}>{intraId}</Link>
+        {enemyTeam && i < enemyTeam.length - 1 ? ', ' : ''}
+      </span>
+    ));
+  };
+  const makeImminentMinute = (gameTime: string, createdAt: string) =>
+    Math.floor(
+      (Number(new Date(gameTime)) - Number(new Date(createdAt))) / 60000
+    );
+  return (
+    <>
+      {enemyTeam && time && (
+        <>
+          {makeEnemyUsers(enemyTeam)}님과 경기{' '}
+          {makeImminentMinute(time, createdAt)}분 전 입니다. 서두르세요!
+        </>
+      )}
+    </>
+  );
 }
