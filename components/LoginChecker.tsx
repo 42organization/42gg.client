d1c16d2e599aa25ff3546d97fe61cd6b048fc275
arkingco <arkingco@gmail.com>
2022-11-13 17:26:33 +0900

    fix : welcomeModal 나오게 수정 #495

--- components/LoginChecker.tsx
+++ components/LoginChecker.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { loginState } from 'utils/recoil/login';
-import { modalState } from 'utils/recoil/modal';
+import { firstVisitedState } from 'utils/recoil/modal';
 // import Load from 'pages/load';
 import Login from 'pages/login';
 import styles from 'styles/Layout/Layout.module.scss';
@@ -14,7 +14,7 @@ interface LoginCheckerProps {
 export default function LoginChecker({ children }: LoginCheckerProps) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [loggedIn, setLoggedIn] = useRecoilState(loginState);
-  const setModal = useSetRecoilState(modalState);
+  const setFirstVisited = useSetRecoilState(firstVisitedState);
   const router = useRouter();
   const presentPath = router.asPath;
   const token = presentPath.split('?token=')[1];
@@ -22,7 +22,7 @@ export default function LoginChecker({ children }: LoginCheckerProps) {
   useEffect(() => {
     if (token) {
       localStorage.setItem('42gg-token', token);
-      setModal({ modalName: 'MAIN-WELCOME' });
+      setFirstVisited(true);
       router.replace(`/`);
     }
     if (localStorage.getItem('42gg-token')) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from 'next';
 import SearchBar from 'components/main/SearchBar';
 import Section from 'components/main/Section';
 import styles from 'styles/main/Home.module.scss';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { firstVisitedState, modalState } from 'utils/recoil/modal';
 
 const Home: NextPage = () => {
+  const [firstVisited, setFirstVisited] = useRecoilState(firstVisitedState);
+  const setModal = useSetRecoilState(modalState);
+
+  if (firstVisited) {
+    setModal({ modalName: 'MAIN-WELCOME' });
+    setFirstVisited(false);
+  }
   return (
     <div className={styles.container}>
       <div className={styles.search}>
