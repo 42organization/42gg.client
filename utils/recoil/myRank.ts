70fbe8535946873ee4b11fa83e7ade486a6128ae
tamagoyakii <rlawlgus4063@naver.com>
2022-12-06 14:25:00 +0900

    [Feat] 페이지 및 스크롤 상태 추가 #67

--- utils/recoil/myRank.ts
+++ utils/recoil/myRank.ts
@@ -4,10 +4,20 @@ import { v1 } from 'uuid';
 
 export const myRankState = atom<MyRank>({
   key: `myRankState/${v1()}`,
-  default: { rank: 0, normal: 0, challenge: 0 },
+  default: { rank: 0, normal: 0, challenge: 0, clicked: false },
 });
 
-export const scrollState = atom<boolean>({
-  key: `scrollState/${v1()}`,
+export const pageState = atom<number>({
+  key: `pageState/${v1()}`,
+  default: 1,
+});
+
+export const myRankScrollState = atom<boolean>({
+  key: `myRankScrollState/${v1()}`,
+  default: false,
+});
+
+export const topScrollState = atom<boolean>({
+  key: `topScrollState/${v1()}`,
   default: false,
 });
