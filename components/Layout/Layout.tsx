a61b507842337cb2ee02dae57f3ee7c660ee1a78
HyeongwooIM <hyeongwoo1279@gmail.com>
2022-12-15 13:53:06 +0900

    [Feat] setModal = useSetRecoilState 수정 #586

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -27,7 +27,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
   const [reloadMatch, setReloadMatch] = useRecoilState(reloadMatchState);
   const setSeasonList = useSetRecoilState(seasonListState);
   const setError = useSetRecoilState(errorState);
-  const setModal = useRecoilState(modalState);
+  const setModal = useSetRecoilState(modalState);
   const presentPath = useRouter().asPath;
   const announcementTime = localStorage.getItem('announcementTime');
 
