faa3b1f25e9b83d0f928d46f54886d4f42343e3e
Raehyeon Jeong <raehy19@gmail.com>
2023-01-30 15:41:35 +0900

    [Feat] : add more admin pages #621

--- components/admin/SideNav.tsx
+++ components/admin/SideNav.tsx
@@ -28,6 +28,54 @@ export default function SideNav() {
           <div className={styles.contentText}>유저 관리</div>
         </div>
       </Link>
+      <Link href='/admin/feedback'>
+        <div
+          className={`${styles.content} ${
+            selectedPath === '/feedback' && styles.selected
+          }`}
+        >
+          <div className={styles.img}>
+            <Image src={userLogo} alt='userLogo' width={18} height={18} />
+          </div>
+          <div className={styles.contentText}>피드백 관리</div>
+        </div>
+      </Link>
+      <Link href='/admin/announcement'>
+        <div
+          className={`${styles.content} ${
+            selectedPath === '/announcement' && styles.selected
+          }`}
+        >
+          <div className={styles.img}>
+            <Image src={userLogo} alt='userLogo' width={18} height={18} />
+          </div>
+          <div className={styles.contentText}>공지사항 관리</div>
+        </div>
+      </Link>
+      <Link href='/admin/notification'>
+        <div
+          className={`${styles.content} ${
+            selectedPath === '/notification' && styles.selected
+          }`}
+        >
+          <div className={styles.img}>
+            <Image src={userLogo} alt='userLogo' width={18} height={18} />
+          </div>
+          <div className={styles.contentText}>알림 관리</div>
+        </div>
+      </Link>
+      <Link href='/admin/penalty'>
+        <div
+          className={`${styles.content} ${
+            selectedPath === '/penalty' && styles.selected
+          }`}
+        >
+          <div className={styles.img}>
+            <Image src={userLogo} alt='userLogo' width={18} height={18} />
+          </div>
+          <div className={styles.contentText}>페널티 관리</div>
+        </div>
+      </Link>
     </div>
   );
 }
---
+++ pages/admin/notification.tsx
@@ -0,0 +1,3 @@
+export default function Notification() {
+  return <div>notification page</div>;
+}
