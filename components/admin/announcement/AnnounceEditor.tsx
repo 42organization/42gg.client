e700a8567bf627a494e5f68d9bfbc28e5e679724
Raehyeon Jeong <raehy19@gmail.com>
2023-02-01 17:46:01 +0900

    [Feat] : 공지사항 모달 미리보기와 입력가능한 에디터 추가 #625

--- components/admin/announcement/AnnounceEditor.tsx
+++ components/admin/announcement/AnnounceEditor.tsx
@@ -1,11 +1,75 @@
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.bubble.css';
+import styles from 'styles/admin/announcement/AnnounceEditor.module.scss';
+import { useState } from 'react';
 
-const QuillNoSSRWrapper = dynamic(() => import('react-quill'), {
+const Quill = dynamic(() => import('react-quill'), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
 });
 
+const editorModules = {
+  toolbar: [
+    [{ header: [] }, { size: [] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    [{ align: [] }],
+    ['link'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'list',
+  'bullet',
+  'indent',
+  'align',
+  'link',
+];
+
 export default function AnnounceEditor() {
-  return <QuillNoSSRWrapper theme='snow' />;
+  const [content, setContent] = useState('');
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.announceModalView}>
+        <div className={styles.announceModalContainer}>
+          <div className={styles.modalTitle}>Notice!</div>
+          <Quill
+            className={styles.quillViewer}
+            readOnly={true}
+            formats={formats}
+            value={content}
+            theme='bubble'
+          />
+          <div className={styles.checkBox}>
+            <input type='checkbox' id='neverSeeAgain' name='neverSeeAgain' />
+            <label htmlFor='neverSeeAgain'>
+              <div>하루 동안 열지 않기</div>
+            </label>
+          </div>
+          <div className={styles.buttons}>
+            <div className={styles.positive}>
+              <input type='button' value='닫기' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Quill
+        className={styles.quillEditor}
+        modules={editorModules}
+        formats={formats}
+        theme='snow'
+        onChange={(e) => setContent(e)}
+      />
+    </div>
+  );
 }
