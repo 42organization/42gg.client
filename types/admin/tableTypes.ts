f42dc510b63abbbae184bde5d08646bedae958db
mike2ox <firefinger07@gmail.com>
2023-02-17 11:28:43 +0900

    [Style] add userManagementTable format #661

--- types/admin/tableTypes.ts
+++ types/admin/tableTypes.ts
@@ -8,5 +8,13 @@ export const tableColumnNames = {
     'createdTime',
     'isChecked',
   ],
+  userInfo: {
+    name: '사용자 정보',
+    columns: ['id', 'roleType', 'intraId', 'statusMessage', 'etc'],
+    etc: {
+      type: 'button',
+      value: ['자세히', '패널티 부여'],
+    },
+  },
   // TODO: 각 페이지별 column name을 contants object로 분류, 각 페이지에서 indexing해서 사용
 };
