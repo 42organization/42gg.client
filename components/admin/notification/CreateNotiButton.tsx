6a6dec4224fcc4edc69abe6a61fdfa82035a4d41
Dain Jeong <dainjeong1004@gmail.com>
2023-02-20 17:17:53 +0900

    [Feat] CreateNotiButton 파일 불필요한 주석 삭제 #650

--- components/admin/notification/CreateNotiButton.tsx
+++ components/admin/notification/CreateNotiButton.tsx
@@ -1,21 +1,10 @@
 import { useSetRecoilState } from 'recoil';
 import { modalState } from 'utils/recoil/modal';
 import style from 'styles/admin/notification/CreateNotificationButton.module.scss';
-// import AdminNotiAllModal from 'components/modal/admin/AdminNotiAllModal';
 
 export default function CreateNotiButton() {
   const setModal = useSetRecoilState(modalState);
 
-  // // TODO Create Modal Distinguishing Criteria(param)
-  // const openModal = (param?: string) => {
-  //   if (!param) {
-  //     // TODO all notification modal
-  //     console.log('Create all notification');
-  //   } else {
-  //     // TODO someone notification modal
-  //     console.log(`Create ${param} notification`);
-  //   }
-  // };
   return (
     <>
       <div className={style.container}>
