ea4d998d0c71bc08e39358bcae22fc6899e521e6
Raehyeon Jeong <raehy19@gmail.com>
2023-02-27 13:51:38 +0900

    [Style] : 파일명 첫글짜 대문자로 수정 #677

--- pages/admin/announcement.tsx
+++ pages/admin/announcement.tsx
@@ -1,6 +1,6 @@
 import AnnounceEdit from 'components/admin/announcement/AnnounceEdit';
 import AnnounceList from 'components/admin/announcement/AnnounceList';
-import styles from 'styles/admin/announcement/announcement.module.scss';
+import styles from 'styles/admin/announcement/Announcement.module.scss';
 export default function Announcement() {
   return (
     <div className={styles.container}>
