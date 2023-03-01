a2f355ac8c1a03073da4d1ad41fea9377d0f1980
Dain Jeong <dainjeong1004@gmail.com>
2023-03-01 15:25:26 +0900

    [Feat] 특정 유저 알림 모달 내에 [유저 검색하기] 기능 추가 #669

--- components/modal/admin/AdminNotiUserModal.tsx
+++ components/modal/admin/AdminNotiUserModal.tsx
@@ -1,17 +1,66 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import instance from 'utils/axios';
 import { modalState } from 'utils/recoil/modal';
 import { toastState } from 'utils/recoil/toast';
-import instance from 'utils/axios';
-import { finished } from 'stream';
+import { errorState } from 'utils/recoil/error';
+import { GoSearch } from 'react-icons/go';
+import { IoIosCloseCircle } from 'react-icons/io';
 import styles from 'styles/admin/modal/AdminNoti.module.scss';
+import { finished } from 'stream';
 
-//noti가 비어있을 때 적용안되게 수정필요
+let timer: ReturnType<typeof setTimeout>;
+
+const MAX_SEARCH_LENGTH = 15;
 
 export default function AdminNotiUserModal(props: any) {
   const setModal = useSetRecoilState(modalState);
   const setSnackBar = useSetRecoilState(toastState);
   const notiContent = useRef<HTMLTextAreaElement>(null);
+
+  const [keyword, setKeyword] = useState<string>('');
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const [searchResult, setSearchResult] = useState<string[]>([]);
+  const setError = useSetRecoilState(errorState);
+  const searchBarRef = useRef<HTMLDivElement>(null);
+
+  const getSearchResultHandler = useCallback(async () => {
+    try {
+      const res = await instance.get(`/pingpong/users/searches?q=${keyword}`);
+      setSearchResult(res?.data.users);
+    } catch (e) {
+      setError('MS02');
+    }
+  }, [keyword, setError]);
+
+  useEffect(() => {
+    const checkId = /^[a-z|A-Z|0-9|-]+$/;
+    if (keyword === '' || (keyword.length && !checkId.test(keyword))) {
+      clearTimeout(timer);
+      setSearchResult([]);
+    } else if (checkId.test(keyword)) {
+      debounce(getSearchResultHandler, 500)();
+    }
+  }, [keyword, getSearchResultHandler]);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      searchBarRef.current &&
+      !searchBarRef.current.contains(event.target as Node)
+    )
+      setShowDropDown(false);
+  };
+
+  const keywordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
 
   const handleClick = useCallback(() => {
     setSnackBar({
@@ -33,11 +82,63 @@ export default function AdminNotiUserModal(props: any) {
         <div className={styles.bodyWrap}>
           <div className={styles.intraWrap}>
             <div className={styles.bodyText}>intra ID</div>
-            <textarea
+            <div className={styles.adminSearchBar} ref={searchBarRef}>
+              <input
+                type='text'
+                onChange={keywordHandler}
+                onFocus={() => setShowDropDown(true)}
+                placeholder='유저 검색하기'
+                maxLength={MAX_SEARCH_LENGTH}
+                value={keyword}
+              />
+              <div className={styles.icons}>
+                {keyword ? (
+                  <span
+                    className={styles.reset}
+                    onClick={() => {
+                      // setKeyword;
+                      setKeyword('');
+                    }}
+                  >
+                    <IoIosCloseCircle />
+                  </span>
+                ) : (
+                  <span
+                    onClick={() => {
+                      // initSearch();
+                      setKeyword('');
+                      setShowDropDown(false);
+                    }}
+                  >
+                    <GoSearch />
+                  </span>
+                )}
+              </div>
+              {showDropDown && keyword && (
+                <div className={styles.dropdown}>
+                  {searchResult.length ? (
+                    searchResult.map((intraId: string) => (
+                      <div
+                        key={intraId}
+                        onClick={() => {
+                          setKeyword(intraId);
+                          setShowDropDown(false);
+                        }}
+                      >
+                        {intraId}
+                      </div>
+                    ))
+                  ) : (
+                    <div className={styles.noneText}>검색 결과가 없습니다.</div>
+                  )}
+                </div>
+              )}
+            </div>
+            {/* <textarea
               className={styles.intraBlank}
               name='intraID'
               placeholder={'intra ID를 입력하세요'}
-            />
+            /> */}
           </div>
           <div className={styles.messageWrap}>
             <div className={styles.bodyText}>message</div>
@@ -69,4 +170,13 @@ export default function AdminNotiUserModal(props: any) {
       </label>
     </div>
   );
+}
+
+function debounce(callback: () => void, timeout: number) {
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback();
+    }, timeout);
+  };
 }
