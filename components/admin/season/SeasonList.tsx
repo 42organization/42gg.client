fa515444317d42e029202d384785bd236bb5ec61
Raehyeon Jeong <raehy19@gmail.com>
2023-03-06 23:48:31 +0900

    [Feat] : mui tab 사용해 get 요청 both, normal, rank 테이블 분리 #692

--- components/admin/season/SeasonList.tsx
+++ components/admin/season/SeasonList.tsx
@@ -6,10 +6,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Tabs,
+  Tab,
 } from '@mui/material';
 import styles from 'styles/admin/season/SeasonList.module.scss';
 import { tableFormat } from 'constants/admin/table';
-import { useEffect, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import instance from 'utils/axios';
 
 interface ISeason {
@@ -28,15 +30,24 @@ interface ISeasonTable {
   seasonList: ISeason[];
 }
 
+const VAL_SEASON_MODE: { [key: number]: string } = {
+  0: 'both',
+  1: 'rank',
+  2: 'normal',
+};
 export default function SeasonList() {
   const [seasonList, setSeasonList] = useState<ISeasonTable>({
     mode: '',
     seasonList: [],
   });
+  const [tabVal, setTabVal] = useState<number>(0);
+  const [selectedSeasonMode, setSelectedSeasonMode] = useState<string>(
+    VAL_SEASON_MODE[tabVal]
+  );
 
-  const getSeasonList = async () => {
+  const getSeasonList = async (mode: string) => {
     try {
-      const res = await instance.get(`pingpong/admin/season/both`);
+      const res = await instance.get(`pingpong/admin/season/${mode}`);
       console.log(res.data);
       setSeasonList({ ...res.data });
     } catch (e: any) {
@@ -45,11 +56,23 @@ export default function SeasonList() {
   };
 
   useEffect(() => {
-    getSeasonList();
-  }, []);
+    getSeasonList(selectedSeasonMode);
+  }, [selectedSeasonMode]);
 
   return (
     <div className={styles.container}>
+      <Tabs
+        value={tabVal}
+        onChange={(e: SyntheticEvent, newVal: number) => {
+          setTabVal(newVal);
+          setSelectedSeasonMode(VAL_SEASON_MODE[newVal]);
+        }}
+      >
+        <Tab label='BOTH' />
+        <Tab label='RANK' />
+        <Tab label='NORMAL' />
+      </Tabs>
+
       <TableContainer className={styles.tableContainer} component={Paper}>
         <Table className={styles.table} aria-label='customized table'>
           <TableHead className={styles.tableHeader}>
