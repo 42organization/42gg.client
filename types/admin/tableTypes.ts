51592de3887b2e7dc629dc037afb3acba17bea65
mike2ox <firefinger07@gmail.com>
2023-03-07 16:50:11 +0900

    [Chore] define user, feedback interface #623

--- types/admin/tableTypes.ts
+++ types/admin/tableTypes.ts
@@ -15,3 +15,31 @@ export type TableFormat = {
     };
   };
 };
+
+export interface IUser {
+  id: number;
+  intraId: string;
+  statusMessage: string;
+  roleType: string; // TODO : type으로 변경
+}
+
+export interface IUserTable {
+  userSearchAdminDtos: IUser[];
+  totalPage: number;
+  currentPage: number;
+}
+
+export interface IFeedback {
+  id: number;
+  intraId: string;
+  category: number; // 1: bug, 2: suggestion, 3: question
+  content: string;
+  createdTime: Date;
+  isSolved: boolean;
+}
+
+export interface IFeedbackTable {
+  feedbackList: IFeedback[];
+  totalPage: number;
+  currentPage: number;
+}
