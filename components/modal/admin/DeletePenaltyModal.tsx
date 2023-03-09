dba25e11bc047b7cbd493e16efd236d7177b56f1
mike2ox <firefinger07@gmail.com>
2023-03-09 14:49:27 +0900

    [Feat] add api request #718

--- components/modal/admin/DeletePenaltyModal.tsx
+++ components/modal/admin/DeletePenaltyModal.tsx
@@ -1,7 +1,15 @@
 import { FiAlertTriangle } from 'react-icons/fi';
+import { useResetRecoilState } from 'recoil';
 import styles from 'styles/admin/modal/DeletePenaltyModal.module.scss';
+import instance from 'utils/axios';
+import { modalState } from 'utils/recoil/modal';
 
 export default function DeletePenaltyModal({ intraId }: { intraId: string }) {
+  const resetModal = useResetRecoilState(modalState);
+  const deletePenalty = async (intraId: string) => {
+    await instance.delete(`pingpong/admin/penalty/users/${intraId}`);
+    resetModal();
+  };
   return (
     <div className={styles.modalWrap}>
       <div className={styles.body}>
@@ -10,8 +18,18 @@ export default function DeletePenaltyModal({ intraId }: { intraId: string }) {
         </div>
         <div className={styles.content}>{intraId}의 패널티 삭제할건가?</div>
         <div className={styles.btns}>
-          <button className={`${styles.btn} ${styles.first}`}>확인</button>
-          <button className={`${styles.btn} ${styles.second}`}>취소</button>
+          <button
+            className={`${styles.btn} ${styles.first}`}
+            onClick={() => deletePenalty(intraId)}
+          >
+            확인
+          </button>
+          <button
+            className={`${styles.btn} ${styles.second}`}
+            onClick={resetModal}
+          >
+            취소
+          </button>
         </div>
       </div>
     </div>
