3de7cd625ea01dfe242ddaa216c5b7fc01a4ebcd
mike2ox <firefinger07@gmail.com>
2023-03-24 09:35:21 +0900

    [Chore] delete unused method at import line #760

--- components/admin/announcement/AnnounceEdit.tsx
+++ components/admin/announcement/AnnounceEdit.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { userState } from 'utils/recoil/layout';
