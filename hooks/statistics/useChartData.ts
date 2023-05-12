0d2c306cd947ed07c3e80b97e63475fbf1081431
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-05-12 15:48:57 +0900

    [refactor] custom hook 제작 statistics #775

---
+++ hooks/statistics/useChartData.ts
@@ -0,0 +1,22 @@
+import { Graphs, GraphValue } from 'types/chartTypes';
+
+type ChartData = {
+  labels: string[];
+  data: number[];
+};
+
+export const useChartData = (chart: Graphs): ChartData => {
+  //const chartLabel:string = chart ? chart.graphs[0].graphName : '';
+  const chartLabels = chart.graphs[0].graphData.map(
+    (item: GraphValue) => item.date
+  );
+  const chartData = chart.graphs[0].graphData.map(
+    (item: GraphValue) => item.count
+  );
+  return {
+    labels: chartLabels,
+    data: chartData,
+  };
+};
+
+export default useChartData;
