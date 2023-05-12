f5160bb71ee1d3156ab7d7ff755a745a963ac1ae
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-05-12 15:48:14 +0900

    [refactor] custom hook 제작 game #775

--- components/game/GameResult.tsx
+++ components/game/GameResult.tsx
@@ -1,8 +1,7 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { SeasonMode } from 'types/mainType';
+import { useGameResult } from 'hooks/game/useGameResult';
 import GameResultList from 'components/game/GameResultList';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 interface GameResultProps {
   mode?: SeasonMode;
@@ -10,13 +9,10 @@ interface GameResultProps {
 }
 
 export default function GameResult({ mode, season }: GameResultProps) {
-  const [path, setPath] = useState('');
   const queryClient = new QueryClient();
-  const router = useRouter();
-  const asPath = router.asPath;
-  const intraId = router.query.intraId;
+  const { path } = useGameResult({ mode, season });
 
-  const makeQuery = () => {
+  /* const makeQuery = () => {
     if (asPath === '/' || asPath.includes('token')) {
       return { count: 3, status: 'live', gameId: '' };
     }
@@ -31,27 +27,7 @@ export default function GameResult({ mode, season }: GameResultProps) {
       query.count = 5;
     }
     return query;
-  };
-
-  const makePath = () => {
-    if (asPath === '/' || asPath.includes('token')) {
-      setPath(`/pingpong/games?count=${3}&status=${'live'}&gameId=`);
-      return;
-    }
-    const userQuery = intraId ? `/users/${intraId}` : '';
-    const seasonQuery = mode === 'rank' && `season=${season}`;
-    const modeQuery = mode !== 'both' && `mode=${mode}`;
-    const countQuery = router.pathname === '/users/detail' && `count=${5}`;
-    const query = [modeQuery, seasonQuery, countQuery, 'gameId=']
-      .filter((item) => item)
-      .join('&');
-    setPath(`/pingpong/games${userQuery}?${query}`);
-    return;
-  };
-
-  useEffect(() => {
-    makePath();
-  }, [asPath, intraId, mode, season]);
+  }; */
 
   return (
     <div>
--- components/game/GameResultList.tsx
+++ components/game/GameResultList.tsx
@@ -1,38 +1,17 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
 import { Game } from 'types/gameTypes';
-import InfScroll from 'utils/infinityScroll';
-import { clickedGameItemState } from 'utils/recoil/game';
 import GameResultEmptyItem from './GameResultEmptyItem';
 import GameResultBigItem from './big/GameResultBigItem';
 import GameResultSmallItem from './small/GameResultSmallItem';
 import styles from 'styles/game/GameResultItem.module.scss';
+import useGameResultList from 'hooks/game/useGameResultList';
 
 interface GameResultListProps {
   path: string;
 }
 
 export default function GameResultList({ path }: GameResultListProps) {
-  const { data, fetchNextPage, status, remove, refetch } = InfScroll(path);
-  const [isLast, setIsLast] = useState<boolean>(false);
-  const [clickedGameItem, setClickedGameItem] =
-    useRecoilState(clickedGameItemState);
-  const pathName = useRouter().pathname;
-
-  useEffect(() => {
-    remove();
-    refetch();
-  }, [path]);
-
-  useEffect(() => {
-    if (status === 'success') {
-      const gameList = data?.pages;
-      if (gameList.length === 1 && gameList[0].games.length)
-        setClickedGameItem(gameList[0].games[0].gameId);
-      setIsLast(gameList[gameList.length - 1].isLast);
-    }
-  }, [data]);
+  const { data, status, fetchNextPage, isLast, clickedGameItem, pathName } =
+    useGameResultList(path);
 
   if (status === 'loading') return <GameResultEmptyItem status={status} />;
 
