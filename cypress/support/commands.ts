b09baea5467f2f76b8ae45e73edc2b72813fe9c9
Yoouyeon <jyeon.yoon59@gmail.com>
2023-05-12 14:47:19 +0900

    [Test] login command 추가 #772

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -12,6 +12,33 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('login', (username: string, password: string) => {
+  // intra login
+  cy.visit('https://profile.intra.42.fr/users/auth/keycloak_student');
+  cy.get('#username').type(Cypress.env('normal_username'));
+  cy.get('#password').type(Cypress.env('normal_password'));
+  cy.get('#kc-login').click();
+  // check if logged in successfully
+  cy.url().should('include', 'https://profile.intra.42.fr/');
+  // 42gg login
+  cy.visit(`${Cypress.env('SERVER_ENDPOINT')}/oauth2/authorization/42`);
+  cy.wait(2000);
+  // check if logged in successfully
+  cy.origin(Cypress.env('home'), () => {
+    cy.url().should('include', Cypress.env('home'));
+    cy.get('body').then(($body) => {
+      // body 내 공지 모달이 있는 경우에 모달 닫기
+      if ($body.find('[class^=AnnouncementModal]').length) {
+        cy.get('input#neverSeeAgain').click();
+        cy.get('input[value=닫기]').click();
+      }
+      // body 내 welcome 모달이 있는 경우에 모달 닫기
+      if ($body.find('[class^=WelcomeModal]').length) {
+        cy.get('input[value=홈으로]').click();
+      }
+    });
+  });
+});
 //
 //
 // -- This is a child command --
