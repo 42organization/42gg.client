e229b1b5e6ad2027b9e43e6f08d3589be03ad9d0
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-05-15 14:57:33 +0900

    [refactor] custom hook 제작 modal/afterGame #775

---
+++ hooks/modal/aftergame/useCurrentGame.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { AfterGame } from 'types/scoreTypes';
+import instance from 'utils/axios';
+import { errorState } from 'utils/recoil/error';
+import { liveState } from 'utils/recoil/layout';
+
+const useCurrentGame = () => {
+  const setError = useSetRecoilState(errorState);
+  const { currentMatchMode } = useRecoilValue(liveState);
+  const [currentGame, setCurrentGame] = useState<AfterGame>({
+    gameId: 0,
+    mode: currentMatchMode,
+    startTime: '2022-07-13 11:50',
+    isScoreExist: false,
+    matchTeamsInfo: {
+      myTeam: { teamScore: 0, teams: [] },
+      enemyTeam: { teamScore: 0, teams: [] },
+    },
+  });
+
+  useEffect(() => {
+    const getCurrentGameHandler = async () => {
+      try {
+        const res = await instance.get(`/pingpong/games/players`);
+        setCurrentGame(res.data);
+      } catch (e) {
+        setError('JH03');
+      }
+    };
+
+    getCurrentGameHandler();
+  }, []);
+
+  return { currentGame };
+};
+
+export default useCurrentGame;
