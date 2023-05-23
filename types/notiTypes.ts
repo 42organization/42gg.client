009eb3a98a1d4fb7be5424fe8578c107724f6599
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-05-23 19:44:25 +0900

    [Feat] [GGFE-18] Noti 타입 변경

--- types/notiTypes.ts
+++ types/notiTypes.ts
@@ -2,13 +2,20 @@ export interface NotiList {
   noti: Noti[];
 }
 
+// export interface Noti {
+//   id: number;
+//   type: string;
+//   message?: string;
+//   time?: string;
+//   isChecked: boolean;
+//   myTeam?: string[];
+//   enemyTeam?: string[];
+//   createdAt: string;
+// }
 export interface Noti {
   id: number;
   type: string;
-  message?: string;
-  time?: string;
   isChecked: boolean;
-  myTeam?: string[];
-  enemyTeam?: string[];
+  message: string;
   createdAt: string;
 }
