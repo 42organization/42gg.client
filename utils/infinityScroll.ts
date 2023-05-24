f27ec3bd666963d14b96106b6eba70ef541dcd07
yoouyeon <jyeon.yoon59@gmail.com>
2023-05-24 14:20:22 +0900

    [Feat] [GGFE-14] infinity scroll page 단위로 변경

--- utils/infinityScroll.ts
+++ utils/infinityScroll.ts
@@ -4,16 +4,17 @@ import { errorState } from 'utils/recoil/error';
 import { instance } from './axios';
 
 export default function InfScroll(path: string) {
+  console.log(path);
   const setError = useSetRecoilState(errorState);
 
-  const getList = ({ pageParam = 0 }) =>
-    instance.get(`${path}${pageParam}`, {}).then((res) => {
+  const getGameList = ({ pageParam = 0 }) =>
+    instance.get(`${path}&page=${pageParam}`, {}).then((res) => {
       return res?.data;
     });
 
-  return useInfiniteQuery('infiniteList', getList, {
-    getNextPageParam: (pages) => {
-      return pages.lastGameId;
+  return useInfiniteQuery('infiniteList', getGameList, {
+    getNextPageParam: (lastPage, allPages) => {
+      return allPages.length;
     },
     onError: (e: any) => {
       if (e.response.data.code === 'UF001') setError('JB07');
