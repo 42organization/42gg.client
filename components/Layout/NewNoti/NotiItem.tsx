bca5a32dd8507f8cc92531a722d88d6e2b82d6f4
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-05-30 15:30:41 +0900

    [Feat] [GGFE-18] enemyId 정규식 적용

---
+++ components/Layout/NewNoti/NotiItem.tsx
@@ -0,0 +1,93 @@
+import Link from 'next/link';
+import { Noti } from 'types/notiTypes';
+import styles from 'styles/Layout/NotiItem.module.scss';
+
+import { HeaderContextState, HeaderContext } from '../HeaderContext';
+import { useContext } from 'react';
+
+interface NotiItemProps {
+  data: Noti;
+}
+
+export default function NotiItem({ data }: NotiItemProps) {
+  const date = data.createdAt.slice(5).replace('T', ' ');
+
+  const parseEnemyIdMessage = (
+    type: string,
+    message: string
+  ): [string[], string] => {
+    let enemyId: string[] = [];
+    let enemyMessage = '';
+    if (type === 'IMMINENT') {
+      const regList = /<intraId::(.+?)>/;
+      const regId = /^[a-zA-Z0-9]*$/;
+      const parseList = message.split(regList).filter((str) => str !== '');
+      enemyId = parseList.filter((id) => regId.test(id) !== false);
+      enemyMessage = parseList.filter((id) => regId.test(id) === false)[0];
+    }
+    return [enemyId, enemyMessage];
+  };
+
+  const enemyIdMessage = parseEnemyIdMessage(data.type, data.message);
+
+  const noti: {
+    [key: string]: { [key: string]: string | JSX.Element | undefined };
+  } = {
+    IMMINENT: {
+      title: '경기 준비',
+      content: MakeImminentContent(enemyIdMessage[0], enemyIdMessage[1]),
+    },
+    ANNOUNCE: {
+      title: '공 지',
+      content: MakeAnnounceContent(data.message),
+    },
+    MATCHED: {
+      title: '매칭 성사',
+      content: data.message,
+    },
+  };
+
+  return (
+    <div
+      className={data.isChecked ? `${styles.readWrap}` : `${styles.unreadWrap}`}
+    >
+      <span className={styles.title}>{noti[data.type].title}</span>
+      <div className={styles.content}>{noti[data.type].content}</div>
+      <div className={styles.date}>{date}</div>
+    </div>
+  );
+}
+
+function MakeAnnounceContent(message: string | undefined) {
+  if (message && !message.includes('https')) return message;
+  const url = message?.split('https')[1];
+  const content = message?.split('https')[0].split('=>')[0];
+  const linkedContent = message?.split('https')[0].split('=>')[1];
+  return (
+    <>
+      {content} {'=>'}
+      <span onClick={() => window.open(`https${url}`)}>{linkedContent}</span>
+    </>
+  );
+}
+
+function MakeImminentContent(enemyTeam: string[], message: string) {
+  const HeaderState = useContext<HeaderContextState | null>(HeaderContext);
+  const makeEnemyUsers = (enemyTeam: string[]) => {
+    return enemyTeam.map((intraId: string, i: number) => (
+      <span key={intraId} onClick={() => HeaderState?.resetOpenNotiBarState()}>
+        <Link href={`/users/detail?intraId=${intraId}`}>{intraId}</Link>
+        {enemyTeam && i < enemyTeam.length - 1 ? ', ' : ''}
+      </span>
+    ));
+  };
+  return (
+    <>
+      {enemyTeam.length && (
+        <>
+          {makeEnemyUsers(enemyTeam)} {message}
+        </>
+      )}
+    </>
+  );
+}
