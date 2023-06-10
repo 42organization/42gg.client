0bb885e5c3e153782a968017e6c719a930200ce8
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-06-10 16:45:35 +0900

    [Style] [GGFE-50] Fira Sans 폰트 추가

--- components/Layout/MenuBar/MenuBar.tsx
+++ components/Layout/MenuBar/MenuBar.tsx
@@ -3,9 +3,19 @@ import styles from 'styles/Layout/MenuBar.module.scss';
 import MenuBarProvider from './MenuBarProvider';
 import NewMenuContext from './MenuBarProvider';
 import { HeaderContextState, HeaderContext } from '../HeaderContext';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'utils/recoil/layout';
+import { User } from 'types/mainType';
+import Link from 'next/link';
+import PlayerImage from 'components/PlayerImage';
+import { profileState } from 'utils/recoil/user';
+import { ProfileBasic } from 'types/userTypes';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const MenuBar = () => {
   const HeaderState = useContext<HeaderContextState | null>(HeaderContext);
+  const user = useRecoilValue<User>(userState);
+  const userLevel = useRecoilValue<ProfileBasic>(profileState).level;
 
   return (
     <NewMenuContext>
@@ -21,6 +31,25 @@ const MenuBar = () => {
             <button onClick={HeaderState?.resetOpenMenuBarState}>
               &#10005;
             </button>
+
+            <div className={styles.menuProfileWrapper}>
+              <PlayerImage
+                src={user.userImageUri}
+                styleName={'mainPageProfile'}
+                size={18}
+              />
+              <div className={styles.userInfoWrapper}>
+                <div className={styles.userId}>{user.intraId}</div>
+                <div className={styles.userLevel}>LV.{userLevel}</div>
+              </div>
+              <Link
+                className={styles.myImage}
+                href={`/users/detail?intraId=${user.intraId}`}
+              >
+                <IoIosArrowForward />
+              </Link>
+            </div>
+
             <nav>
               <div className={styles.menu}>
                 <MenuBarProvider.mainMenu />
