9d8e3a663e001920b8b4ad3681405512c35f6c2b
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-06-12 15:40:43 +0900

    [Style] [GGFE-44] 빵빠레 데이터 없을시 안뜨게 변경

--- components/rank/topRank/RankListMain.tsx
+++ components/rank/topRank/RankListMain.tsx
@@ -8,7 +8,10 @@ interface RankListMainProps {
 
 export default function RankListMain({ rank, isMain }: RankListMainProps) {
   const bangElements = Array.from({ length: 5 }, (_, index) => (
-    <div key={index} className={`${!isMain && styles.bang}`} />
+    <div
+      key={index}
+      className={`${rank?.rankList[1].rank === 1 && !isMain && styles.bang}`}
+    />
   ));
 
   return (
