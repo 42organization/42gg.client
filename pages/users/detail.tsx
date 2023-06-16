129af8bf621bcd2ab026b108de07f6482cb41e93
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-06-16 18:32:00 +0900

    [Feat] [GGFE-70] 프로필부분에 recrnt record 디자인 변경

--- pages/users/detail.tsx
+++ pages/users/detail.tsx
@@ -1,12 +1,9 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import BasicProfile from 'components/user/BasicProfile';
 import GameResult from 'components/game/GameResult';
 import RankProfile from 'components/user/RankProfile';
 import styles from 'styles/user/user.module.scss';
 import Section from 'components/main/Section';
-
-import { FaChevronRight } from 'react-icons/fa';
 
 export default function User() {
   const router = useRouter();
@@ -18,19 +15,6 @@ export default function User() {
         <div key={intraId}>
           <BasicProfile profileId={intraId} />
           <RankProfile profileId={intraId} />
-          <div className={styles['sectionWrap']}>
-            <div className={styles['titleWrap']}>
-              <span>Recent Record</span>
-              <Link
-                href={{
-                  pathname: '/game',
-                  query: { intraId: intraId },
-                }}
-              >
-                <FaChevronRight color='white' size='13' />
-              </Link>
-            </div>
-          </div>
           <div>
             <Section
               path={`game?intraId=${intraId}`}
