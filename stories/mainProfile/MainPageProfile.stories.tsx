5cdcb7dd875284b89db191dd4a59f9db3e51470a
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-06-19 16:35:00 +0900

    [Feat] [GGFE-72] noti, menu drawer로 + currentMatch 분리

--- stories/mainProfile/MainPageProfile.stories.tsx
+++ stories/mainProfile/MainPageProfile.stories.tsx
@@ -1,12 +1,11 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import MainPageProfile from 'components/Layout/MainPageProfile';
 import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { userState } from 'utils/recoil/layout';
-import { currentMatchState, openCurrentMatchState } from 'utils/recoil/match';
 
 const meta: Meta<typeof MainPageProfile> = {
-  title: 'MainPage/MainPageProfile',
+  title: 'MainPageProfile/MainPageProfile',
   component: MainPageProfile,
   tags: ['autodocs'],
 };
@@ -26,147 +25,11 @@ function useSetUser() {
   }, []);
 }
 
-const MatchOne = () => {
+const MainDefault = () => {
   useSetUser();
-
-  const setOpenCurrentMatch = useSetRecoilState(openCurrentMatchState);
-  setOpenCurrentMatch(false);
-
-  const setCurrentMatch = useSetRecoilState(currentMatchState);
-  setCurrentMatch({
-    match: [],
-  });
-  return <MainPageProfile />;
-};
-
-const MatchTwo = () => {
-  useSetUser();
-
-  const setOpenCurrentMatch = useSetRecoilState(openCurrentMatchState);
-  setOpenCurrentMatch(true);
-
-  const setCurrentMatch = useSetRecoilState(currentMatchState);
-  setCurrentMatch({
-    match: [
-      {
-        startTime: '2100-01-02T10:00',
-        endTime: '2101-01-02T10:00',
-        isMatched: true,
-        myTeam: ['Storybook'],
-        enemyTeam: ['Cypress'],
-        isImminent: false,
-      },
-    ],
-  });
-  return <MainPageProfile />;
-};
-
-const MatchThree = () => {
-  SetUser();
-
-  const setOpenCurrentMatch = useSetRecoilState(openCurrentMatchState);
-  setOpenCurrentMatch(true);
-
-  const setCurrentMatch = useSetRecoilState(currentMatchState);
-  setCurrentMatch({
-    match: [
-      {
-        startTime: '2100-01-02T10:00',
-        endTime: '2101-01-02T10:00',
-        isMatched: true,
-        myTeam: ['Storybook'],
-        enemyTeam: ['Cypress'],
-        isImminent: true,
-      },
-    ],
-  });
-  return <MainPageProfile />;
-};
-
-const MatchFour = () => {
-  SetUser();
-
-  const setOpenCurrentMatch = useSetRecoilState(openCurrentMatchState);
-  setOpenCurrentMatch(true);
-
-  const setCurrentMatch = useSetRecoilState(currentMatchState);
-  setCurrentMatch({
-    match: [
-      {
-        startTime: '2100-01-02T10:00',
-        endTime: '2101-01-02T10:00',
-        isMatched: false,
-        myTeam: ['Storybook'],
-        enemyTeam: [],
-        isImminent: false,
-      },
-      {
-        startTime: '2101-01-02T10:00',
-        endTime: '2102-01-02T10:00',
-        isMatched: false,
-        myTeam: ['Storybook'],
-        enemyTeam: [],
-        isImminent: false,
-      },
-    ],
-  });
-  return <MainPageProfile />;
-};
-
-const MatchFive = () => {
-  SetUser();
-
-  const setOpenCurrentMatch = useSetRecoilState(openCurrentMatchState);
-  setOpenCurrentMatch(true);
-
-  const setCurrentMatch = useSetRecoilState(currentMatchState);
-  setCurrentMatch({
-    match: [
-      {
-        startTime: '2100-01-02T10:00',
-        endTime: '2101-01-02T10:00',
-        isMatched: false,
-        myTeam: ['Storybook'],
-        enemyTeam: [],
-        isImminent: false,
-      },
-      {
-        startTime: '2101-01-02T10:00',
-        endTime: '2102-01-02T10:00',
-        isMatched: false,
-        myTeam: ['Storybook'],
-        enemyTeam: [],
-        isImminent: false,
-      },
-      {
-        startTime: '2102-01-02T10:00',
-        endTime: '2103-01-02T10:00',
-        isMatched: false,
-        myTeam: ['Storybook'],
-        enemyTeam: [],
-        isImminent: false,
-      },
-    ],
-  });
   return <MainPageProfile />;
 };
 
 export const Case1: Story = {
-  render: () => <MatchOne />,
-};
-
-export const Case2: Story = {
-  render: () => <MatchTwo />,
-};
-
-export const Case3: Story = {
-  render: () => <MatchThree />,
-};
-
-export const Case4: Story = {
-  render: () => <MatchFour />,
-};
-
-export const Case5: Story = {
-  render: () => <MatchFive />,
+  render: () => <MainDefault />,
 };
