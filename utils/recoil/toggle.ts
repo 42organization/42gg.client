5c18c62219acb2a358baaa6b1b102bf2c3b0eb46
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-06-20 13:53:05 +0900

    [Style] [GGFE-66] 주석제거, rankmain 크기 변경

--- utils/recoil/toggle.ts
+++ utils/recoil/toggle.ts
@@ -1,20 +1,8 @@
 import { ToggleMode } from 'types/rankTypes';
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 import { v1 } from 'uuid';
 
 export const toggleState = atom<ToggleMode>({
   key: `Toggle/${v1()}`,
   default: 'RANK',
 });
-
-/* export const ToggleSelector = selector({
-	key: 'toggleSelector',
-	get: ({get}) => {
-		const toggle = get(toggleState);
-		if (toggle === 'RANK') {
-			return get('RANK')
-		} else {
-			return get('NORMAL');
-		}
-	}
-}); */
