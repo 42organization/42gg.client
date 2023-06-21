4f95bae9af8d30e5d6536e77af0d43951a90953e
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-06-21 16:34:25 +0900

    [Style] [GGFE-98] ranklistmain 위치 수정

--- components/main/Section.tsx
+++ components/main/Section.tsx
@@ -16,7 +16,7 @@ export default function Section({ sectionTitle, path }: SectionProps) {
   const router = useRouter();
   const pathCheck: pathType = {
     game: <GameResult />,
-    rank: <RankList toggleMode={'RANK'} isMain={true} />,
+    rank: <RankList isMain={true} />,
   };
 
   return (
