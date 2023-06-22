9d8d5130110b34329fbc2fe6f9a051ac2fbc6b26
Sungwook Kim <sungwook@c10r3s1.42seoul.kr>
2023-06-22 16:04:48 +0900

    [Fix] [GGFE-83] fix update when noauth

--- hooks/modal/useEditProfileModal.ts
+++ hooks/modal/useEditProfileModal.ts
@@ -37,20 +37,17 @@ const useEditProfileModal = ({
       editedProfile.snsNotiOpt = 'NONE';
     }
 
-    setProfile((prev) => ({
-      ...prev,
-      ...editedProfile,
-    }));
-
     try {
       await instance.put(`/pingpong/users/${intraId}`, editedProfile);
+      setProfile((prev) => ({
+        ...prev,
+        ...editedProfile,
+      }));
       alert('프로필이 성공적으로 등록되었습니다.');
     } catch (e: any) {
       if (e.response.status === 403) {
         alert('카카오 유저는 프로필을 수정할 수 없습니다.');
-        return;
-      }
-      setError('JH02');
+      } else setError('JH02');
     }
     setModal({ modalName: null });
   };
