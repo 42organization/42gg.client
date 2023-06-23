9e2ca7b9603ffb21e1377304184ffd5a40cff279
Sungwook Kim <sungwook@c10r3s1.42seoul.kr>
2023-06-23 18:34:14 +0900

    [Feat] [GGFE-102] add userType in kakakoEditModal

--- components/modal/profile/KakaoEditModal.tsx
+++ components/modal/profile/KakaoEditModal.tsx
@@ -1,23 +1,49 @@
 import styles from 'styles/modal/menu/KakaoEditModal.module.scss';
 import { instance } from 'utils/axios';
 import router from 'next/router';
+import { useEffect, useState } from 'react';
+import { errorState } from 'utils/recoil/error';
+import { useSetRecoilState } from 'recoil';
 
-export default function NormalGame() {
-  const clickLinkHandler = async () => {
+type UserType = 'kakao' | 'fortytwo' | 'both' | '';
+
+export default function KakaoEditModal() {
+  const [userType, setUserType] = useState<UserType>('');
+  const setError = useSetRecoilState<string>(errorState);
+
+  const userTypeHandler = async () => {
     try {
+      const res = await instance.post('/pingpong/users/oath');
+      setUserType(res.data.oauthType);
+    } catch (e: any) {
+      setError('SW04');
+    }
+  };
+
+  useEffect(() => {
+    userTypeHandler();
+  }, [userType]);
+
+  const clickLinkHandler = () => {
+    if (userType === 'fortytwo') {
       router.push(
         `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/oauth2/authorization/kakao`
       );
-    } catch (e: any) {
-      alert(e.response.data.message);
+    } else if (userType === 'kakao') {
+      router.push(
+        `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/oauth2/authorization/42`
+      );
     }
+    setUserType('both');
   };
 
   const clickUnlinkHandler = async () => {
     try {
       await instance.delete('/pingpong/users/oauth/kakao');
+      alert('카카오톡 연동이 해제되었습니다.');
+      setUserType('fortytwo');
     } catch (e: any) {
-      alert(e.response.data.message);
+      alert('카카오톡 연동 해제에 실패했습니다.');
     }
   };
 
@@ -25,8 +51,12 @@ export default function NormalGame() {
     <div className={styles.container}>
       <div className={styles.resultContainer}>카카오톡 연동</div>
       <div className={styles.buttons}>
-        <button onClick={clickLinkHandler}>연동하기</button>
-        <button onClick={clickUnlinkHandler}>연동 해제하기</button>
+        <button onClick={clickLinkHandler} disabled={userType === 'both'}>
+          연동하기
+        </button>
+        <button onClick={clickUnlinkHandler} disabled={userType !== 'both'}>
+          연동 해제하기
+        </button>
       </div>
     </div>
   );
