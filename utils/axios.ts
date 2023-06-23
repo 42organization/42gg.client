183f3700a3801b0036ea253a0ff7d579118b0638
Sungwook Kim <sungwook@c10r3s1.42seoul.kr>
2023-06-23 16:11:35 +0900

    fix

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { loginState } from 'utils/recoil/login';
 
@@ -9,75 +10,57 @@ const manageBaseURL = process.env.NEXT_PUBLIC_MANAGE_SERVER_ENDPOINT ?? '/';
 const instance = axios.create({ baseURL });
 const instanceInManage = axios.create({ baseURL: manageBaseURL });
 
-// Function to refresh the access token
-const refreshAccessToken = async () => {
-  try {
-    const refreshToken = Cookies.get('refresh_token'); // Get the refresh token from the cookie
-    const response = await fetch(
-      `${baseURL}/pingpong/users/accesstoken?refreshToken=${refreshToken}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    const data = await response.json();
-    const newAccessToken = data.accessToken;
-
-    // Update the access token in localStorage
-    localStorage.setItem('42gg-token', newAccessToken);
-
-    return newAccessToken;
-  } catch (error) {
-    // Handle error while refreshing the access token
-    throw new Error('Failed to refresh access token');
-  }
-};
-
-// Axios interceptor for refreshing the access token
-instance.interceptors.response.use(
-  function onSuccess(response) {
-    return response;
+instance.interceptors.request.use(
+  function setConfig(config) {
+    config.headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('42gg-token')}`,
+    };
+    config.withCredentials = true; // Add the withCredentials option
+    return config;
   },
-  async function onError(error) {
-    const originalRequest = error.config;
-    const setLoggedIn = useSetRecoilState<boolean>(loginState);
-
-    if (
-      error.response &&
-      error.response.status === 401
-      //  !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-
-      try {
-        const newAccessToken = await refreshAccessToken();
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        return instance(originalRequest);
-      } catch (error) {
-        setLoggedIn(false);
-        // Handle error while refreshing the access token or retrying the original request
-        return Promise.reject(error);
-      }
-    }
-
+  function getError(error) {
     return Promise.reject(error);
   }
 );
 
-// Request interceptor to set headers and withCredentials option
-const requestInterceptor = function setConfig(config: any) {
-  config.headers = {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('42gg-token')}`,
-  };
-  config.withCredentials = true; // Add the withCredentials option
-  return config;
-};
+instanceInManage.interceptors.request.use(
+  function setConfig(config) {
+    config.headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('42gg-token')}`,
+    };
+    config.withCredentials = true; // Add the withCredentials option
+    return config;
+  },
+  function getError(error) {
+    return Promise.reject(error);
+  }
+);
 
-// Add request interceptor to both instances
-instance.interceptors.request.use(requestInterceptor);
-instanceInManage.interceptors.request.use(requestInterceptor);
+const responseInterceptor = instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401) {
+      try {
+        const refreshToken = Cookies.get('refresh_token');
+        const res = await instance.post(
+          `/pingpong/users/accesstoken?refreshToken=${refreshToken}`
+        );
+        localStorage.setItem('42gg-token', res.data.accessToken);
+      } catch (error) {
+        const setLoggedIn = useSetRecoilState<boolean>(loginState);
+        setLoggedIn(false);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+useEffect(() => {
+  return () => {
+    instance.interceptors.request.eject(responseInterceptor);
+  };
+}, [responseInterceptor]);
 
 export { instance, instanceInManage };
