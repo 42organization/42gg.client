4bef48676d0279dcd06aa7ad6ae40a1b544c32ae
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-06-23 16:38:03 +0900

    [Feat] [GGFE-121] season 역순 출력

--- components/mode/modeItems/SeasonDropDown.tsx
+++ components/mode/modeItems/SeasonDropDown.tsx
@@ -1,5 +1,6 @@
 import { Season } from 'types/seasonTypes';
 import styles from 'styles/mode/SeasonDropDown.module.scss';
+import { slice } from 'cypress/types/lodash';
 
 interface SeasonDropDownProps {
   seasonList: Season[];
@@ -12,12 +13,11 @@ export default function SeasonDropDown({
   value,
   onSelect,
 }: SeasonDropDownProps) {
+  console.log(seasonList);
   return (
     <div className={styles.selectBox}>
       <select className={styles.select} onChange={onSelect} value={value}>
         {seasonList
-          .slice(0)
-          .reverse()
           .map((season: Season) => (
             <option key={season.id} value={season.id}>
               {season.name}
