bdd8d9a4afa46676d44f385277c0196f32aadbba
Sungwook Kim <sungwook@c10r3s1.42seoul.kr>
2023-06-23 19:49:18 +0900

    [Fix] [GGFE-102] fix post method

--- components/modal/profile/KakaoEditModal.tsx
+++ components/modal/profile/KakaoEditModal.tsx
@@ -13,7 +13,7 @@ export default function KakaoEditModal() {
 
   const userTypeHandler = async () => {
     try {
-      const res = await instance.post('/pingpong/users/oauth');
+      const res = await instance.get('/pingpong/users/oauth');
       setUserType(res.data.oauthType);
     } catch (e: any) {
       setError('SW04');
