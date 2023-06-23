45c9841cf7453b0ea24854a8e2c42e8e80332bc8
Sungwook Kim <sungwook@c10r3s1.42seoul.kr>
2023-06-23 19:02:31 +0900

    [Fix] [GGFE-102] api error

--- components/modal/profile/KakaoEditModal.tsx
+++ components/modal/profile/KakaoEditModal.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { errorState } from 'utils/recoil/error';
 import { useSetRecoilState } from 'recoil';
 
-type UserType = 'kakao' | 'fortytwo' | 'both' | '';
+type UserType = 'kakao' | 'fortyTwo' | 'both' | '';
 
 export default function KakaoEditModal() {
   const [userType, setUserType] = useState<UserType>('');
@@ -13,7 +13,7 @@ export default function KakaoEditModal() {
 
   const userTypeHandler = async () => {
     try {
-      const res = await instance.post('/pingpong/users/oath');
+      const res = await instance.post('/pingpong/users/oauth');
       setUserType(res.data.oauthType);
     } catch (e: any) {
       setError('SW04');
@@ -25,7 +25,7 @@ export default function KakaoEditModal() {
   }, [userType]);
 
   const clickLinkHandler = () => {
-    if (userType === 'fortytwo') {
+    if (userType === 'fortyTwo') {
       router.push(
         `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/oauth2/authorization/kakao`
       );
@@ -41,7 +41,7 @@ export default function KakaoEditModal() {
     try {
       await instance.delete('/pingpong/users/oauth/kakao');
       alert('카카오톡 연동이 해제되었습니다.');
-      setUserType('fortytwo');
+      setUserType('fortyTwo');
     } catch (e: any) {
       alert('카카오톡 연동 해제에 실패했습니다.');
     }
@@ -49,7 +49,9 @@ export default function KakaoEditModal() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.resultContainer}>카카오톡 연동</div>
+      <div className={styles.resultContainer}>
+        {userType === 'kakao' ? '42 intra 연동' : '카카오톡 연동'}
+      </div>
       <div className={styles.buttons}>
         <button onClick={clickLinkHandler} disabled={userType === 'both'}>
           연동하기
