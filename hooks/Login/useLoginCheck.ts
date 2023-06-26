df23c495966d17013580e98998b4fefce6dd112b
Sungwook Kim <sungwook@c2r2s2.42seoul.kr>
2023-06-26 21:47:22 +0900

    edit login

--- hooks/Login/useLoginCheck.ts
+++ hooks/Login/useLoginCheck.ts
@@ -19,29 +19,29 @@ const useLoginCheck = (): useLoginCheckReturn => {
   const router: NextRouter = useRouter();
   const [refreshToken, setRefreshToken] = useState<string>('');
 
-  const getAccessToken = async () => {
-    try {
-      const res = await instance.post(
-        `/pingpong/users/accesstoken?refreshToken=${refreshToken}`
-      );
-      setAccessToken(res.data.accessToken);
-    } catch (error) {
-      setError('SW05');
-    }
-  };
+//  const getAccessToken = async () => {
+//    try {
+//      const res = await instance.post(
+//        `/pingpong/users/accesstoken?refreshToken=${refreshToken}`
+//      );
+//      setAccessToken(res.data.accessToken);
+//    } catch (error) {
+//      setError('SW05');
+//    }
+//  };
 
   useEffect(() => {
-	setRefreshToken(Cookies.get('refresh_token') || '');
-	if (refreshToken)
-    	getAccessToken();
-    if (accessToken) {
-      localStorage.setItem('42gg-token', accessToken);
-      setFirestVisited(true);
-      router.replace('/');
-    }
-    if (localStorage.getItem('42gg-token')) {
-      setLoggedIn(true);
-    }
+	//setRefreshToken(Cookies.get('refresh_token') || '');
+	//if (refreshToken)
+    //	getAccessToken();
+    //if (accessToken) {
+    //  localStorage.setItem('42gg-token', accessToken);
+    //  router.replace('/');
+    //}
+    //if (localStorage.getItem('42gg-token')) {
+		setLoggedIn(true);
+		setFirestVisited(true);
+    //}
     setIsLoading(false);
   }, []);
 
