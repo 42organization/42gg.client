c1a6575a2524965a5e1fd720acb1971514f458a7
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-06-29 12:51:26 +0900

    [Chore] [GGFE-132] openMenuBarState, openNotiBarState 제거

--- hooks/modal/useReportHandler.ts
+++ hooks/modal/useReportHandler.ts
@@ -1,9 +1,8 @@
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { Modal } from 'types/modalTypes';
 import { modalState } from 'utils/recoil/modal';
 import { instance } from 'utils/axios';
 import { errorState } from 'utils/recoil/error';
-import { openMenuBarState } from 'utils/recoil/layout';
 interface Report {
   category: string;
   content: string;
@@ -14,7 +13,6 @@ type useReportHandlerReturn = () => Promise<void>;
 const useReportHandler = (report: Report): useReportHandlerReturn => {
   const setModal = useSetRecoilState<Modal>(modalState);
   const setError = useSetRecoilState<string>(errorState);
-  const resetOpenMenuBar = useResetRecoilState(openMenuBarState);
   const reportResponse: { [key: string]: string } = {
     SUCCESS: '의견 주셔서 감사합니다 ❤️',
     REJECT: '마음을 담아 의견을 보내주세요 ❤️',
@@ -25,7 +23,6 @@ const useReportHandler = (report: Report): useReportHandlerReturn => {
       try {
         await instance.post('/pingpong/feedback', report);
         setModal({ modalName: null });
-        resetOpenMenuBar();
         alert(reportResponse.SUCCESS);
       } catch (e) {
         setError('JH06');
