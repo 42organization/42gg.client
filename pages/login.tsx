8fe7b23f939cac0df0c308979f03195962f129c6
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-01 15:49:07 +0900

    [Feat][GGFE-161] 테스트용 megaphone login 페이지에서 삭제

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import StyledButton from 'components/StyledButton';
 import styles from 'styles/Login.module.scss';
-import Megaphone from 'components/Layout/Megaphone';
 
 function Login() {
   const router = useRouter();
@@ -28,7 +27,6 @@ function Login() {
 
   return (
     <>
-      <Megaphone/>
       <div className={styles.loginContainer}>
         <div className={styles.innerContainer}>
           <div className={styles.logoContainer}>
