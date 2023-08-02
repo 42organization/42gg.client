5e120872cad8be44b9446448a360e3994a0a00ae
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-02 17:47:38 +0900

    [Feat] [GGFE-161] fetch -> useMockAxiosGet으로 변경

--- components/Layout/MegaPhone.tsx
+++ components/Layout/MegaPhone.tsx
@@ -1,75 +1,69 @@
 import { useState, useEffect } from 'react';
-import useAxiosGet from 'hooks/useAxiosGet';
+import { useMockAxiosGet } from 'hooks/useAxiosGet';
 import styles from 'styles/Layout/Megaphone.module.scss';
 
 interface IMegaphoneContent {
-    megaphoneId: number;
-    content: string;
-    intraId: string;
+  megaphoneId: number;
+  content: string;
+  intraId: string;
 }
 
-type MegaphoneList = Array<IMegaphoneContent>
+type MegaphoneList = Array<IMegaphoneContent>;
 
 const defaultContents: MegaphoneList = [
-    {
-        megaphoneId: 1,
-        content: '문구를 입력해주세요',
-        intraId: 'admin',
-    },
-]
+  {
+    megaphoneId: 1,
+    content: '문구를 입력해주세요',
+    intraId: 'admin',
+  },
+];
 
 const Megaphone = () => {
-    const [contents, setContents] = useState<MegaphoneList>(defaultContents);
-    const [play, setPlay] = useState('running');
-    
-    // 실제 api 호출해서 내용 받기
-    // const getMegaphoneHandler = useAxiosGet({
-    //     url: `pingpong/megaphones`,
-    //     setState: (data) => {
-    //         setContents(data.megaphoneList);
-    //     },
-    //     err: 'HJ01',
-    //     type: 'setError'
-    // })
+  const [contents, setContents] = useState<MegaphoneList>(defaultContents);
+  const [play, setPlay] = useState('running');
 
-    // test api
-    const getMegaphoneHandler = async () => {
-        const res = await fetch(`http://localhost:3000/api/pingpong/megaphones`);
-        const data = await res.json();
-        console.log(data);
-        await setContents(data.megaphoneList);
+  // 나중에 useAxiosGet으로 변경 필요
+  const getMegaphoneHandler = useMockAxiosGet<any>({
+    url: `pingpong/megaphones`,
+    setState: (data) => {
+      setContents(data.megaphoneList);
+    },
+    err: 'HJ01',
+    type: 'setError',
+  });
+
+  useEffect(() => {
+    getMegaphoneHandler();
+  }, []);
+
+  const clickPause = () => {
+    if (play === 'pause') {
+      setPlay('running');
+    } else {
+      setPlay('pause');
+      setTimeout(() => setPlay('running'), 5000);
     }
+  };
 
-    useEffect(() => {
-        getMegaphoneHandler();
-    }, []);
+  const pauseStyle: { [key: string]: string } = {
+    pause: styles.pause,
+    running: styles.running,
+  };
 
-    const clickPause = () => {
-        if (play === 'pause') {
-            setPlay('running');
-        } else {
-            setPlay('pause');
-            setTimeout(() => setPlay('running'), 5000);
-        }
-    }
-    
-    const pauseStyle: {[key: string]: string} = {
-        pause: styles.pause,
-        running: styles.running,
-    };
-
-    return (
-        <div className={styles.rollingBanner}
-        onClick={() => clickPause()}>
-            <div className={styles.wrapper}>
-                <ul className={`${styles.megaphoneContents} ${pauseStyle[play]}`}>
-                    {contents.map((content, idx) => (
-                            <li key={idx}> {idx} {content.intraId} : {content.content}&nbsp;&nbsp;</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.rollingBanner} onClick={() => clickPause()}>
+      <div className={styles.wrapper}>
+        <ul className={`${styles.megaphoneContents} ${pauseStyle[play]}`}>
+          {contents.map((content, idx) => (
+            <li key={idx}>
+              {' '}
+              {idx} {content.intraId} : {content.content}&nbsp;&nbsp;
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Megaphone;
