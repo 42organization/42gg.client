9ac7549edae765f58e391af87f637a050053729c
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-02 15:05:53 +0900

    [Style] [GGFE-143] StoreModeWrap 상단 메뉴 스타일링

--- components/mode/modeWraps/StoreModeWrap.tsx
+++ components/mode/modeWraps/StoreModeWrap.tsx
@@ -1,8 +1,10 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { FaCoins } from 'react-icons/fa';
 import { StoreMode } from 'types/storeTypes';
 import { ICoin } from 'types/userTypes';
 import { useMockAxiosGet } from 'hooks/useAxiosGet';
 import StoreModeRadioBox from 'components/mode/modeItems/StoreModeRadioBox';
+import styles from 'styles/mode/StoreModeWrap.module.scss';
 
 type StoreModeWrapProps = {
   currentMode: StoreMode;
@@ -14,19 +16,25 @@ export function StoreModeWrap({
   setStoreMode,
 }: StoreModeWrapProps) {
   const [coin, setCoin] = useState<ICoin>({ coin: 0 });
+  useEffect(() => {
+    getCoin();
+    // TODO : 코인이 바뀔 때 마다 다시 불러와야 한다.
+  }, []);
   const getCoin = useMockAxiosGet({
     url: '/users/coin',
     setState: setCoin,
     err: 'JY01',
     type: 'setError',
   });
-  useEffect(() => {
-    getCoin();
-    // TODO : 코인이 바뀔 때 마다 다시 불러와야 한다.
-  }, []);
   return (
     <div>
-      <div>보유 코인: {coin.coin}</div>
+      <div className={styles.topMenu}>
+        <button>매뉴얼</button>
+        <div className={styles.coins}>
+          {coin.coin}
+          <FaCoins />
+        </div>
+      </div>
       <StoreModeRadioBox
         mode={currentMode}
         onChange={(e) => setStoreMode(e.target.value as StoreMode)}
