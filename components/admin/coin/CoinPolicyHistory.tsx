4a2cdd0c6cd27e4d41222390f1c1afd966f3adb2
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-03 10:26:57 +0900

    [Feat] [GGFE-162] 관리자 재화정책 수정

--- components/admin/coin/CoinPolicyHistory.tsx
+++ components/admin/coin/CoinPolicyHistory.tsx
@@ -8,46 +8,49 @@ import {
   TableRow,
 } from '@mui/material';
 
+const coinPolicyHistoryTableTitle: { [key: string]: string } = {
+  coinPolicyId: '정책 id',
+  createUser: '등록 유저',
+  attendance: '출석 획득',
+  nomal: '일반게임 획득',
+  rank_win: '랭크게임 승리 획득',
+  rank_lose: '랭크게임 패배 획득',
+  created_at: '등록 날짜',
+};
 
+function CoinPolicyHistory() {
+  const test = [
+    'coinPolicyId',
+    'createUser',
+    'attendance',
+    'normal',
+    'rank_win',
+    'rank_lose',
+    'created_at',
+  ];
 
-const coinPolicyHistoryTableTitle: { [key: string]: string} = {
-    coinpollicyId : '정책 id',
-    createUser : '등록 유저',
-    attendance : '출석 획득',
-    nomal : '일반게임 획득',
-    rank_win : '랭크게임 승리 획득',
-    rank_lose : '랭크게임 패배 획득',
-    created_at : '등록 날짜'
-}
-
-
-function CoinPolicyHistory () {
-
-    const test = ['attendance', 'normal', 
-                'rankWin', 'rankLose'];
-
-    return (
-        <>
-            <TableContainer component={Paper}>
-                <Table aria-label='customized table'>
-                    <TableHead>
-                        <TableRow>
-                            {test.map((content, idx) => (
-                                <TableCell key={idx}>
-                                    {coinPolicyHistoryTableTitle[content]}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>비어있습니다</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    )
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              {test.map((content, idx) => (
+                <TableCell key={idx}>
+                  {coinPolicyHistoryTableTitle[content]}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>비어있습니다</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
 
 export default CoinPolicyHistory;
