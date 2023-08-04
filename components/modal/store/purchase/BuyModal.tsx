eb1743cec4af2a645f10fac270b840d05cfd6dd4
hyobin <hyobb109@gmail.com>
2023-08-04 16:34:25 +0900

    [Feat] [GGFE-164] 구매, 선물 모달에 아이템 이름,가격 추가

--- components/modal/store/purchase/BuyModal.tsx
+++ components/modal/store/purchase/BuyModal.tsx
@@ -1,21 +1,29 @@
 import useBuyModal from 'hooks/modal/store/purchase/useBuyModal';
 import styles from 'styles/modal/store/BuyModal.module.scss';
+import { PriceTag } from 'types/modalTypes';
 
-export default function BuyModal() {
+export default function BuyModal({ product, price }: PriceTag) {
   const { onPurchase, onCancel } = useBuyModal();
 
   return (
     <div className={styles.container}>
       <div className={styles.phrase}>
-        <div className={styles.emoji}>🏓</div>
+        <div className={styles.emoji}>🛍️</div>
         <div className={styles.message}>구매하시겠습니까?</div>
+        <div className={styles.itemInfo}>
+          <div className={styles.itemName}>아이템: {product}</div>
+          <div className={styles.itemPrice}>가격: {price}</div>
+        </div>
+        <div className={styles.warning}>
+          <p>⚠ 구매한 아이템은 환불이 불가합니다 ⚠</p>
+        </div>
       </div>
       <div className={styles.buttons}>
         <div className={styles.negative}>
-          <input onClick={onCancel} type='button' value='취소' />
+          <input onClick={onCancel} type='button' value='아니오' />
         </div>
         <div className={styles.positive}>
-          <input onClick={onPurchase} type='button' value='확인' />
+          <input onClick={onPurchase} type='button' value='예' />
         </div>
       </div>
     </div>
--- components/modal/store/purchase/GiftModal.tsx
+++ components/modal/store/purchase/GiftModal.tsx
@@ -1,14 +1,22 @@
 import useGiftModal from 'hooks/modal/store/purchase/useGiftModal';
 import styles from 'styles/modal/store/GiftModal.module.scss';
+import { PriceTag } from 'types/modalTypes';
 
-export default function GiftModal() {
+export default function GiftModal({ product, price }: PriceTag) {
   const { onPurchase, onCancel } = useGiftModal();
 
   return (
     <div className={styles.container}>
       <div className={styles.phrase}>
-        <div className={styles.emoji}>🏓</div>
-        <div className={styles.message}>선물하시겠습니까?</div>
+        <div className={styles.emoji}>🎁</div>
+        <div className={styles.message}>선물하기</div>
+        <div className={styles.itemInfo}>
+          <div className={styles.itemName}>아이템: {product}</div>
+          <div className={styles.itemPrice}>가격: {price}</div>
+        </div>
+        <div className={styles.warning}>
+          <p>⚠ 선물한 아이템은 환불 및 취소가 불가합니다 ⚠</p>
+        </div>
       </div>
       <div className={styles.buttons}>
         <div className={styles.negative}>
