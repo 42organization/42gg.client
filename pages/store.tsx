a6fa7959d1f2289e9667aef361c7c180dd9afc96
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-07 12:45:04 +0900

    [Chore] [GGFE-144] 불필요한 클릭 핸들러 삭제

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 import { StoreMode } from 'types/storeTypes';
 import { StoreModeWrap } from 'components/mode/modeWraps/StoreModeWrap';
 import { Inventory } from 'components/store/Inventory';
@@ -8,19 +7,10 @@ import ItemsList from 'components/shop/ItemsList';
 
 export default function Store() {
   const [mode, setMode] = useState<StoreMode>('BUY');
-  const router = useRouter();
-
-  const clickTitleHandler = () => {
-    router.push(`/store`, undefined, {
-      shallow: true,
-    });
-  };
 
   return (
     <div className={styles.pageWrap}>
-      <h1 className={styles.title} onClick={clickTitleHandler}>
-        GG Store
-      </h1>
+      <h1 className={styles.title}>GG Store</h1>
       <StoreModeWrap currentMode={mode} setStoreMode={setMode} />
       <div className={styles.storeContainer}>
         {mode === 'BUY' ? (
