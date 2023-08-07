bcceca982c0dfd6807b6d54c69912cf626aaef6d
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-07 16:27:13 +0900

    [Feat] [GGFE-147] 확성기 삭제 기능 추가

--- components/modal/admin/AdminDeleteMegaphoneModal.tsx
+++ components/modal/admin/AdminDeleteMegaphoneModal.tsx
@@ -4,6 +4,8 @@ import { modalState } from 'utils/recoil/modal';
 import { ImegaphoneInfo } from 'types/admin/adminReceiptType';
 import styles from 'styles/admin/modal/AdminDeleteMegaphoneModal.module.scss';
 
+import { mockInstance } from 'utils/mockAxios';
+
 export default function AdminDeleteMegaphoneModal(props: ImegaphoneInfo) {
   const { megaphoneId, content, intraId } = props;
   const resetModal = useResetRecoilState(modalState);
@@ -11,21 +13,20 @@ export default function AdminDeleteMegaphoneModal(props: ImegaphoneInfo) {
 
   // 수정 필요 작동안함
   // instanceInManage, try catch로 변경
-  // const deleteMegaphoneHandler = async (id: number) => {
-  //   try {
-  //     await fetch(`http://localhost:3000/api/pingpong/admin/megaphones/${id}`, {
-  //       method: 'DELETE',
-  //     });
-  //     alert(`${id}번 확성기가 삭제되었습니다`);
-  //   } catch (e: any) {
-  //     if (e.response.status === 400) {
-  //       alert(`${id}번 확성기는 삭제할 수 없습니다`);
-  //     } else {
-  //       setError('HJ04');
-  //     }
-  //   }
-  //   resetModal();
-  // };
+  const deleteMegaphoneHandler = async (id: number) => {
+    console.log(id);
+    try {
+      await mockInstance.delete(`/admin/megaphones/${id}`);
+    } catch (e: any) {
+      if (e.response.status === 403) {
+        alert(`${id}번 확성기는 삭제할 수 없습니다`);
+      } else {
+        setError('HJ04');
+      }
+    }
+    alert(`${id}번 확성기가 삭제되었습니다`);
+    resetModal();
+  };
 
   return (
     <div className={styles.whole}>
@@ -56,7 +57,12 @@ export default function AdminDeleteMegaphoneModal(props: ImegaphoneInfo) {
           <button className={styles.cancelBtn} onClick={() => resetModal()}>
             취소
           </button>
-          <button className={styles.deleteBtn}>삭제</button>
+          <button
+            className={styles.deleteBtn}
+            onClick={() => deleteMegaphoneHandler(megaphoneId)}
+          >
+            삭제
+          </button>
         </div>
       </div>
     </div>
