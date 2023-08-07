056adb9265842574522a651b940758d7654271d3
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-08-07 13:49:18 +0900

    [Style] [GGFE-141] imageuri 변경

--- pages/api/pingpong/users/user.ts
+++ pages/api/pingpong/users/user.ts
@@ -9,10 +9,10 @@ export default function handler(
     intraId: 'sangmipa',
     isAdmin: true,
     userImageUri:
-      'https://42gg-public-test-image.s3.ap-northeast-2.amazonaws.com/images/yoahn-1a995822-19d6-499a-a494-0a566395a64b.jpeg',
+      '/Users/sangminpark/Desktop/42gg.client/public/image/fallBackSrc.jpeg',
     isAttended: true,
     tierImageUri:
-      'https://42gg-public-test-image.s3.ap-northeast-2.amazonaws.com/images/cheolee-69dec7cb-63ce-4d29-8591-c3ac83da7bf9.jpeg',
+      '/Users/sangminpark/Desktop/42gg.client/public/image/fallBackSrc.jpeg',
   };
 
   res.status(200).json(userData);
