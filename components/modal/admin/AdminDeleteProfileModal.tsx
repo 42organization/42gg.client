709c86cf895b25d0d6329dae63fd6449115f492d
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-09 00:33:33 +0900

    [Feat] [GGFE-170] 관리자 프로필 삭제 모달 스타일 적용

--- components/modal/admin/AdminDeleteProfileModal.tsx
+++ components/modal/admin/AdminDeleteProfileModal.tsx
@@ -40,20 +40,27 @@ export default function AdminDeleteProfileModal(props: IprofileInfo) {
       </div>
       <div className={styles.body}>
         <div className={styles.bodyWrap}>
+          <div className={styles.contentWrap}>
+            <div className={styles.bodyText}>현재 프로필 :</div>
+            <div className={styles.profileImage}>
+              <PlayerImage
+                src={imageUrl}
+                size={18}
+                styleName={'mainPageProfile'}
+              />
+            </div>
+          </div>
           <div className={styles.intraWrap}>
             <div className={styles.bodyText}>사용자 :</div>
             <input className={styles.intraBlank} value={intraId} readOnly />
           </div>
-          <div className={styles.contentWrap}>
-            <div className={styles.bodyText}>현재 프로필</div>
-            <PlayerImage
-              src={imageUrl}
-              size={18}
-              styleName={'mainPageProfile'}
-            />
-          </div>
           <div className={styles.checkWrap}>
-            {profileId}번 {intraId}님의 프로필을 삭제하시겠습니까?
+            <div className={styles.checkTextMain}>
+              {profileId}번 {intraId}님의 프로필을 삭제하시겠습니까?
+            </div>
+            <div className={styles.checkTextSub}>
+              해당 유저의 이전 이미지로 설정됩니다.
+            </div>
           </div>
         </div>
         <div className={styles.buttonWrap}>
