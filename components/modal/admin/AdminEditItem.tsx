1ae1c68db6093ae08bad1740a36e20d401efe626
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-10 08:55:25 +0900

    [Feat][GGFE-148] 관리자 아이템 수정 모달 수정 + 스토리북

--- components/modal/admin/AdminEditItem.tsx
+++ components/modal/admin/AdminEditItem.tsx
@@ -1,12 +1,13 @@
 import useUploadImg from 'hooks/useUploadImg';
 import Image from 'next/image';
-import { useCallback } from 'react';
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { IitemInfo } from 'types/admin/adminStoreTypes';
 import { mockInstance } from 'utils/mockAxios';
-import { errorState } from 'utils/recoil/error';
 import { modalState } from 'utils/recoil/modal';
 import { toastState } from 'utils/recoil/toast';
+import styles from 'styles/admin/modal/AdminEditItem.module.scss';
+import { userState } from 'utils/recoil/layout';
+import { useRef } from 'react';
 
 export default function AdminEditItemModal(props: IitemInfo) {
   const { itemId, itemName, content, imageUrl, originalPrice, discount } =
@@ -14,7 +15,14 @@ export default function AdminEditItemModal(props: IitemInfo) {
   const setModal = useSetRecoilState(modalState);
   const setSnackBar = useSetRecoilState(toastState);
 
+  const creator = useRecoilValue(userState).intraId;
+
   const { imgData, imgPreview, uploadImg } = useUploadImg();
+
+  const nameRef = useRef();
+  const contentRef = useRef();
+  const priceRef = useRef();
+  const discountRef = useRef();
 
   // api 연결 시 instanceInManage로 변경 필요
   const editItemHandler = async () => {
@@ -24,7 +32,7 @@ export default function AdminEditItemModal(props: IitemInfo) {
       // content: ,
       // price: ,
       // discount: ,
-      // creatorIntra: ,
+      creatorIntra: creator,
     };
     formData.append(
       'updateItemInfo',
@@ -56,31 +64,81 @@ export default function AdminEditItemModal(props: IitemInfo) {
   };
 
   return (
-    <div className={Styles.modal}>
-      <div>
-        <label htmlFor='imageUrl'>이미지</label>
-        <input type='image' />
-        <Image src={imageUrl} width={20} height={20} alt='no' />
+    <div className={styles.whole}>
+      <div className={styles.title}>
+        <div className={styles.titleText}>아이템 수정</div>
+        <hr className={styles.hr} />
       </div>
-      <div>
-        <label htmlFor='itemName'>아이템명</label>
-        <input type='text' value={itemName} />
+      <div className={styles.body}>
+        <div className={styles.bodyWrap}>
+          <label className={styles.imageWrap}>
+            <Image
+              src={imgPreview ? imgPreview : imageUrl}
+              alt='Item Image'
+              width={90}
+              height={80}
+            />
+            <input
+              type='file'
+              style={{ display: 'none' }}
+              onChange={uploadImg}
+            />
+          </label>
+          <div className={styles.idNameWrap}>
+            <div className={styles.idWrap}>
+              <div className={styles.bodyText}>아이템 ID :</div>
+              <input className={styles.idBlank} value={itemId} readOnly />
+            </div>
+            <div className={styles.nameWrap}>
+              <div className={styles.bodyText}>아이템명 :</div>
+              <input
+                className={styles.nameBlank}
+                type='text'
+                ref={nameRef}
+                defaultValue={itemName}
+              />
+            </div>
+          </div>
+          <div className={styles.contentWrap}>
+            <div className={styles.bodyText}>설명 :</div>
+            <input
+              className={styles.contentBlank}
+              type='text'
+              name='content'
+              defaultValue={content}
+            />
+          </div>
+          <div className={styles.priceDiscountWrap}>
+            <div className={styles.priceWrap}>
+              <div className={styles.bodyText}>원가 :</div>
+              <input
+                className={styles.priceBlank}
+                type='number'
+                name='price'
+                defaultValue={originalPrice}
+              />
+            </div>
+            <div className={styles.discountWrap}>
+              <div className={styles.bodyText}>할인율 :</div>
+              <input
+                className={styles.discountBlank}
+                type='number'
+                name='discount'
+                defaultValue={discount}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonWrap}>
+          <button className={styles.editBtn}>수정</button>
+          <button
+            className={styles.cancelBtn}
+            onClick={() => setModal({ modalName: null })}
+          >
+            취소
+          </button>
+        </div>
       </div>
-      <div>
-        <label htmlFor='content'>아이템명</label>
-        <input type='text' value={content} />
-      </div>
-      <div>
-        <label htmlFor='price'>가격</label>
-        <input type='number' value={originalPrice} />
-      </div>
-      <div>
-        <label htmlFor='discount'>할인율</label>
-        <input type='number' value={discount} />
-      </div>
-      <div>{itemId} 번 아이템을 수정하시겠습니까?</div>
-      <button onClick={() => setModal({ modalName: null })}>취소</button>
-      {/* <button onClick={() => editItemHandler(itemId)}>삭제</button> */}
     </div>
   );
 }
