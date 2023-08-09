c0e9223f353a47bc3bf3b83319da2d3ee4998f08
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-09 15:39:27 +0900

    [Style] [GGFE-153] 확성기 사용모달 디자인 수정

--- components/modal/store/inventory/ItemCautionContainer.tsx
+++ components/modal/store/inventory/ItemCautionContainer.tsx
@@ -1,12 +1,14 @@
+import styles from 'styles/modal/store/InventoryModal.module.scss';
+
 type ItemCautionContainerProps = {
   caution: string[];
 };
 
 export function ItemCautionContainer({ caution }: ItemCautionContainerProps) {
   return (
-    <div>
-      <h4>주의사항</h4>
-      <ul>
+    <div className={styles.itemCautionContainer}>
+      <div className={styles.title}>주의사항</div>
+      <ul className={styles.list}>
         {caution.map((instruction, idx) => (
           <li key={idx}>{instruction}</li>
         ))}
--- components/modal/store/inventory/UseMegaphone.tsx
+++ components/modal/store/inventory/UseMegaphone.tsx
@@ -28,26 +28,32 @@ export default function UseMegaphone({ receiptId }: UseMegaphoneProps) {
     <div className={styles.container}>
       <div className={styles.title}>확성기 등록</div>
       <div className={styles.phrase}>
-        <MegaphoneContainer play={'running'} clickPause={() => void 0}>
-          {content.length === 0 ? (
-            <li>등록할 확성기 내용을 입력해주세요.</li>
-          ) : (
-            <li>
-              {user.intraId} : {content}
-            </li>
-          )}
-        </MegaphoneContainer>
-        <div className={styles.textArea}>
-          <textarea
-            value={content}
-            rows={1}
-            name='newMegaphoneContent'
-            onChange={(e) => setContent(e.target.value)}
-            maxLength={MAX_LENGTH}
-            placeholder='확성기 내용을 입력해주세요.'
-          ></textarea>
-          <div>
-            {content.length}/{MAX_LENGTH}
+        <div className={styles.section}>
+          <div className={styles.sectionTitle}>미리보기</div>
+          <MegaphoneContainer play={'running'} clickPause={() => void 0}>
+            {content.length === 0 ? (
+              <li>등록할 확성기 내용을 입력해주세요.</li>
+            ) : (
+              <li>
+                {user.intraId} : {content}
+              </li>
+            )}
+          </MegaphoneContainer>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.sectionTitle}>확성기 메시지 편집</div>
+          <div className={styles.textArea}>
+            <textarea
+              value={content}
+              rows={1}
+              name='newMegaphoneContent'
+              onChange={(e) => setContent(e.target.value)}
+              maxLength={MAX_LENGTH}
+              placeholder='확성기 내용을 입력해주세요.'
+            ></textarea>
+            <div>
+              {content.length}/{MAX_LENGTH}
+            </div>
           </div>
         </div>
         <ItemCautionContainer caution={caution} />
