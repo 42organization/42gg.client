467399ac924af6084bcda2f445dd1214c95919b2
박형준 <hyungjpa@baghyeongjun-ui-MacBookAir.local>
2023-08-10 16:47:46 +0900

    [Feat][GGFE-148] 아이템 수정, 삭제 mock api 수정

--- pages/api/pingpong/admin/items/[itemId].ts
+++ pages/api/pingpong/admin/items/[itemId].ts
@@ -6,11 +6,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   // body: 이미지 담겨서 formData로 오는데 여기서 어떻게 조작하는지 모르겠음
 
-  if (method === 'PUT') {
+  if (method === 'PUT' || method === 'DELETE') {
     if (parseInt(itemId) < 3) res.status(204).end();
     else res.status(400).end();
-  }
-  if (method === 'DELETE') {
-    res.status(204).end();
   }
 }
