e45c3942d2925511dbe883363513899ce0c20dd3
hyobin <hyobb109@gmail.com>
2023-08-12 19:49:41 +0900

    [Feat] [GGFE-171] 코인 내역 모달 추가

---
+++ components/modal/store/CoinHistoryContainer.tsx
@@ -0,0 +1,39 @@
+import { useMockAxiosGet } from 'hooks/useAxiosGet';
+import { useEffect, useState } from 'react';
+import CoinHistoryDetails from './CoinHistoryDetails';
+import { ICoinHistoryList } from 'types/userTypes';
+import styles from 'styles/modal/store/CoinHistoryContainer.module.scss';
+
+// TODO: 무한 스크롤 구현
+export default function CoinHistoryContainer() {
+  const [coinHistoryList, setCoinHistoryList] = useState<ICoinHistoryList>({
+    useCoinList: [],
+    totalPage: 0,
+  });
+
+  useEffect(() => {
+    getCoinHistoryList();
+  }, []);
+
+  const getCoinHistoryList = useMockAxiosGet({
+    url: '/users/coin?page=1&size=5',
+    setState: setCoinHistoryList,
+    err: 'HB03',
+    type: 'setError',
+  });
+
+  return (
+    <div className={styles.container}>
+      {coinHistoryList.useCoinList.length === 0 ? (
+        <div className={styles.empty}>GG코인 내역이 존재하지 않습니다.</div>
+      ) : (
+        coinHistoryList.useCoinList.map((coinHistory) => (
+          <CoinHistoryDetails
+            key={coinHistory.createdAt.toString()}
+            details={coinHistory}
+          />
+        ))
+      )}
+    </div>
+  );
+}
