7f94b2110dd53fbe575628dbb7e4e45379e8d96d
hyobin <hyobb109@gmail.com>
2023-08-12 19:49:03 +0900

    [Feat] [GGFE-171] 코인 내역 mock api

--- pages/api/pingpong/users/coin.ts
+++ pages/api/pingpong/users/coin.ts
@@ -1,15 +1,101 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ICoin } from 'types/userTypes';
+import { ICoinHistory, ICoinHistoryList, ICoin } from 'types/userTypes';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ICoin>
-) {
-  const coin = 100;
+const coin = 100;
 
-  const coinData: ICoin = {
-    coin: coin,
+const coinData: ICoin = {
+  coin: coin,
+};
+
+function createCoinHistoryReq(useCoinList: ICoinHistory[], totalPage: number) {
+  const res: ICoinHistoryList = {
+    useCoinList: useCoinList,
+    totalPage: totalPage,
   };
+  return res;
+}
 
-  res.status(200).json(coinData);
+const coinTrans1: ICoinHistory = {
+  history: '출석',
+  amount: 1,
+  createdAt: new Date('2023-08-07 15:57:25'),
+};
+
+const coinTrans2: ICoinHistory = {
+  history: '일반게임 참가',
+  amount: 3,
+  createdAt: new Date('2023-08-06 15:57:25'),
+};
+
+const coinTrans3: ICoinHistory = {
+  history: '확성기 구매',
+  amount: -42,
+  createdAt: new Date('2023-08-06 15:57:25'),
+};
+
+const coinTrans4: ICoinHistory = {
+  history: '랭크게임 승리',
+  amount: 5,
+  createdAt: new Date('2023-03-31 00:30:25'),
+};
+
+const coinTrans5: ICoinHistory = {
+  history: '프로필사진 변경권 구매',
+  amount: -50,
+  createdAt: new Date('2023-03-31 00:30:25'),
+};
+
+const coinTrans6: ICoinHistory = {
+  history: '출석',
+  amount: 1,
+  createdAt: new Date('2023-03-25 00:30:25'),
+};
+
+const coinTrans7: ICoinHistory = {
+  history: '출석',
+  amount: 1,
+  createdAt: new Date('2023-03-22 00:30:25'),
+};
+
+const coinTrans8: ICoinHistory = {
+  history: '프로필 이미지띠 구매',
+  amount: -10,
+  createdAt: new Date('2023-03-21 00:30:25'),
+};
+
+const coinTrans9: ICoinHistory = {
+  history: '출석',
+  amount: 1,
+  createdAt: new Date('2023-03-11 00:30:25'),
+};
+
+const coinTrans10: ICoinHistory = {
+  history: '출석',
+  amount: 1,
+  createdAt: new Date('2023-03-01 00:30:25'),
+};
+
+const CoinHistoryList: ICoinHistory[] = [
+  coinTrans1,
+  coinTrans2,
+  coinTrans3,
+  coinTrans4,
+  coinTrans5,
+  coinTrans6,
+  coinTrans7,
+  coinTrans8,
+  coinTrans9,
+  coinTrans10,
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { page, size } = req.query;
+
+  if (page && size) {
+    // TEST : slice로 리스트 자르기 (10 넣으면 빈배열) && page 숫자 변경해서 테스트 가능
+    const TotalCoinHistory = createCoinHistoryReq(CoinHistoryList.slice(0), 1);
+    res.status(200).json(TotalCoinHistory);
+  } else {
+    res.status(200).json(coinData);
+  }
 }
