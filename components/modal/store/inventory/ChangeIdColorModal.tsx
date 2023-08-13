f854a2d576dc8883b17f98e176ade4bf18062fe0
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-14 01:12:15 +0900

    [Feat] [GGFE-154] 색깔 미리보기 컴포넌트 추가

--- components/modal/store/inventory/ChangeIdColorModal.tsx
+++ components/modal/store/inventory/ChangeIdColorModal.tsx
@@ -8,6 +8,7 @@ import {
   ModalButtonContainer,
   ModalButton,
 } from 'components/modal/ModalButton';
+import IdPreviewComponent from './IdPreviewComponent';
 import styles from 'styles/modal/store/InventoryModal.module.scss';
 
 type ChangeIdColorModalProps = UseItemRequest;
@@ -15,21 +16,32 @@ type ChangeIdColorModalProps = UseItemRequest;
 export default function ChangeIdColorModal({
   receiptId,
 }: ChangeIdColorModalProps) {
+  const resetModal = useResetRecoilState(modalState);
+  const user = useRecoilValue(userState);
+  const [color, setColor] = useState<string>('#000000');
   return (
     <div className={styles.container}>
       <div className={styles.title}>아이디 색상 변경</div>
       <div className={styles.phrase}>
         <div className={styles.section}>
           <div className={styles.sectionTitle}>미리보기</div>
-          {/* 랭크와 일반 버전 두개 미리 보기 */}
+          <IdPreviewComponent color={color} />
         </div>
         <div className={styles.section}>
           <div className={styles.sectionTitle}>색상 선택</div>
           <div className={styles.colorPicker}>
-            <HexColorPicker />
-            <HexColorInput />
+            <HexColorPicker color={color} onChange={setColor} />
+            <HexColorInput color={color} onChange={setColor} prefixed alpha />
           </div>
         </div>
+        <ModalButtonContainer>
+          <ModalButton
+            style='negative'
+            value='취소'
+            onClick={() => resetModal()}
+          />
+          <ModalButton style='positive' value='등록' onClick={() => void 0} />
+        </ModalButtonContainer>
       </div>
     </div>
   );
