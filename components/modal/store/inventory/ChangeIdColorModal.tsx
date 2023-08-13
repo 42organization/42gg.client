15e94d8e2816c66b9212197bc4d191b1ed62f790
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-14 01:18:45 +0900

    [Feat] [GGFE-154] intraId 적용

--- components/modal/store/inventory/ChangeIdColorModal.tsx
+++ components/modal/store/inventory/ChangeIdColorModal.tsx
@@ -8,10 +8,17 @@ import {
   ModalButtonContainer,
   ModalButton,
 } from 'components/modal/ModalButton';
+import { ItemCautionContainer } from './ItemCautionContainer';
 import IdPreviewComponent from './IdPreviewComponent';
 import styles from 'styles/modal/store/InventoryModal.module.scss';
 
 type ChangeIdColorModalProps = UseItemRequest;
+
+// TODO : 주의사항 구체화 필요
+const caution = [
+  '아이템을 사용한 후에는 취소가 불가능합니다.',
+  '색상 변경은 아이템 당 1회만 가능합니다.',
+];
 
 export default function ChangeIdColorModal({
   receiptId,
@@ -25,7 +32,7 @@ export default function ChangeIdColorModal({
       <div className={styles.phrase}>
         <div className={styles.section}>
           <div className={styles.sectionTitle}>미리보기</div>
-          <IdPreviewComponent color={color} />
+          <IdPreviewComponent intraId={user.intraId} color={color} />
         </div>
         <div className={styles.section}>
           <div className={styles.sectionTitle}>색상 선택</div>
@@ -34,6 +41,7 @@ export default function ChangeIdColorModal({
             <HexColorInput color={color} onChange={setColor} prefixed alpha />
           </div>
         </div>
+        <ItemCautionContainer caution={caution} />
         <ModalButtonContainer>
           <ModalButton
             style='negative'
--- components/modal/store/inventory/IdPreviewComponent.tsx
+++ components/modal/store/inventory/IdPreviewComponent.tsx
@@ -1,10 +1,11 @@
 import styles from 'styles/rank/RankList.module.scss';
 
 type IdPreviewProps = {
+  intraId: string;
   color: string;
 };
 
-export default function IdPreviewComponent({ color }: IdPreviewProps) {
+export default function IdPreviewComponent({ intraId, color }: IdPreviewProps) {
   // TODO : RankListItem 2개로 분리 후 import해서 사용할 것.
   // NOTE : 아래는 임시 구현임.
   return (
@@ -14,7 +15,7 @@ export default function IdPreviewComponent({ color }: IdPreviewProps) {
         className={`${styles.rankItemWrap} ${styles.standard} ${styles.Ranking}`}
       >
         <div className={styles.intraId} style={{ color: color }}>
-          intraId
+          {intraId}
         </div>
       </div>
       {/* NORMAL */}
@@ -22,7 +23,7 @@ export default function IdPreviewComponent({ color }: IdPreviewProps) {
         className={`${styles.rankItemWrap} ${styles.standard} ${styles.Vip}`}
       >
         <div className={styles.intraId} style={{ color: color }}>
-          intraId
+          {intraId}
         </div>
       </div>
     </div>
