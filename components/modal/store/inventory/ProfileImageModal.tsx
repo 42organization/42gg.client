f96910e514f85883e0f525acc59cabcebbbb747b
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-16 10:55:25 +0900

    [Style] [GGFE-156] 이미지 프리뷰 스타일 적용

--- components/modal/store/inventory/ProfileImageModal.tsx
+++ components/modal/store/inventory/ProfileImageModal.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useRecoilValue, useResetRecoilState } from 'recoil';
+import Image from 'next/image';
 import { FaArrowRight } from 'react-icons/fa';
 import { UseItemRequest } from 'types/inventoryTypes';
 import { modalState } from 'utils/recoil/modal';
 import { userState } from 'utils/recoil/layout';
+import useUploadImg from 'hooks/useUploadImg';
 import { useMockAxiosGet } from 'hooks/useAxiosGet';
 import { ItemCautionContainer } from './ItemCautionContainer';
 import {
@@ -11,18 +13,41 @@ import {
   ModalButton,
 } from 'components/modal/ModalButton';
 import styles from 'styles/modal/store/InventoryModal.module.scss';
+import { TbQuestionMark } from 'react-icons/tb';
 
 type ProfileImageProps = UseItemRequest;
 
 export default function ProfileImageModal({ receiptId }: ProfileImageProps) {
+  const user = useRecoilValue(userState);
   const resetModal = useResetRecoilState(modalState);
+  const { imgData, imgPreview, uploadImg } = useUploadImg();
   return (
     <div className={styles.container}>
       <div className={styles.title}>프로필 이미지 변경</div>
       <div className={styles.phrase}>
-        {/* 기존 프로필 */}
-        <FaArrowRight />
-        {/* 새로운 프로필 (업로드와 프리뷰) */}
+        <div className={styles.section}>
+          <div className={styles.profilePreviewContainer}>
+            <Image
+              src={user.userImageUri}
+              width={80}
+              height={80}
+              alt='profile image'
+            />
+            <FaArrowRight />
+            {imgPreview ? (
+              <Image
+                src={imgPreview}
+                width={80}
+                height={80}
+                alt='new profile image'
+              />
+            ) : (
+              <div className={styles.profilePreview}>
+                <TbQuestionMark />
+              </div>
+            )}
+          </div>
+        </div>
         <ModalButtonContainer>
           <ModalButton
             style='negative'
