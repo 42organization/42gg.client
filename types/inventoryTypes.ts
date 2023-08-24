85209d0b63a3daa387f1f5f7207fc258d1e8ad5f
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-24 11:18:15 +0900

    [Test] [GGFE-166] 변경된 아이템 데이터 적용

--- types/inventoryTypes.ts
+++ types/inventoryTypes.ts
@@ -9,12 +9,9 @@ export type ItemType =
 
 export type InventoryItem = {
   receiptId: number; // 거래내역 아이디
-  createdAt: string; // 구매날짜 // TODO : 표시할 수 있는 방법 생각해보기
   itemName: string; // 아이템 이름
   imageUri: string; // 아이템 이미지 uri
-  itemPrice: number; // 아이템 가격 // REVIEW - 필요 여부 논의
   purchaserIntra: string; // 구매자 Intra Id
-  ownerIntra: string; // 소유자 Intra Id // REVIEW - ownerIntra 필요 여부 논의
   itemType: ItemType; // 아이템 타입
   itemStatus: InventoryItemStatus;
 };
