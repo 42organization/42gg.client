923c591b190ac3de9efd14e8fb5bc45851e2dc70
yoouyeon <jyeon.yoon59@gmail.com>
2023-08-25 13:49:34 +0900

    [Fix] [GGFE-211] 추가된 user 속성 story에 적용

--- stories/drawer/MenuBar.stories.tsx
+++ stories/drawer/MenuBar.stories.tsx
@@ -37,6 +37,12 @@ const MenuHeader = ({ open }: { open: boolean }) => {
       intraId: 'Storybook',
       isAdmin: false,
       userImageUri: 'https://picsum.photos/200/300?grayscale',
+      isAttended: false,
+      tierImageUri:
+        'https://cdn.pixabay.com/photo/2022/07/29/05/52/table-tennis-7351159_1280.png',
+      tierName: '무지개 탁구채',
+      level: 40,
+      edge: 'BASIC',
     });
 
     setProfile({
@@ -49,6 +55,11 @@ const MenuHeader = ({ open }: { open: boolean }) => {
       maxExp: 1000,
       expRate: 0,
       snsNotiOpt: 'SLACK',
+      tierImageUri:
+        'https://cdn.pixabay.com/photo/2022/07/29/05/52/table-tennis-7351159_1280.png',
+      tierName: '무지개 탁구채',
+      edge: 'BASIC',
+      backgroundType: 'BASIC',
     });
   }, []);
 
