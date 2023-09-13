94d4bae55edb35c6bd38da4fcc27c61d7714fd97
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-13 16:59:12 +0900

    [Style] [GGFE-251] Reality Hyper Regular 폰트 제거

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -7,7 +7,7 @@ export default function Admin() {
         width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        fontFamily: 'Reality Hyper Regular',
+        fontFamily: 'inter', // $admin-font
         fontSize: '2rem',
       }}
     >
