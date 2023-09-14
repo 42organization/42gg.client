2026e1fcb6b457e8fcff620b6e46477b6b110fb0
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-14 15:27:22 +0900

    [Style] [GGFE-251] $admin-font (Inter) 제거

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -7,7 +7,6 @@ export default function Admin() {
         width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        fontFamily: 'inter', // $admin-font
         fontSize: '2rem',
       }}
     >
