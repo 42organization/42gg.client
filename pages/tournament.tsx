dbd27108dbeabdaacb7599dd25efefb5df692cde
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-11-08 16:07:46 +0900

    [Feat] 테스트용 데이터 생성

--- pages/tournament.tsx
+++ pages/tournament.tsx
@@ -6,6 +6,33 @@ import styles from 'styles/tournament/TournamentContainer.module.scss';
 
 // 내부의 토너먼트 보여주는 부분만 Component화 하면될까? 다른곳에서도 쓰일까?
 // 진행중인 토너먼트의 Bracket을 보여주는 부분도 다른곳에서도 쓸수있지않나?
+const tempData = {
+  tournaments: [
+    {
+      tournametId: 5,
+      title: '5회 루키 토너먼트',
+      contents: '블라블라',
+      startDate: '2023-11-11',
+      status: '종료',
+      type: 'rookie',
+      winnerId: 'hannkim',
+      winnerImage: '',
+      endDate: '2023-11-11',
+    },
+    {
+      tournametId: 6,
+      title: '6회 마스터 토너먼트',
+      contents: '블라블라 하이하이',
+      startDate: '2023-11-12',
+      status: '진행중',
+      type: 'master',
+      winnerId: 'hannkim',
+      winnerImage: '',
+      endDate: '2023-11-12',
+    },
+  ],
+  totalPage: 100,
+};
 
 export default function Tournament() {
   const setModal = useSetRecoilState<Modal>(modalState);
@@ -26,8 +53,9 @@ export default function Tournament() {
       <div className={styles.tournamentContainer}>
         <div className={styles.tournamentTextWait}> 대기중인 토너먼트 </div>
         <div className={styles.waitTournamentBox}>
-          <TournamentCard tournamentName='제 1회 루키 토너먼트' />
-          <TournamentCard tournamentName='제 1회 마스터 토너먼트' />
+          {tempData.tournaments.map((tournament, index) => (
+            <TournamentCard key={index} tournamentName={tournament.title} />
+          ))}
           <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
           <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
           <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
