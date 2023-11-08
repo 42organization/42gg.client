383ab8e1e609c65a6f6868bfe739be0c9b9ca017
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-11-08 15:19:35 +0900

    [Feat] 대기중인 토너먼트를 보여주는 Card 컴포넌트 생성

--- components/tournament/TournamentCard.tsx
+++ components/tournament/TournamentCard.tsx
@@ -1,3 +1,10 @@
-export default function TournamentCard() {
-  return <div>토너먼트 카드</div>;
+import { TournamentInfo } from 'types/modalTypes';
+import styles from 'styles/tournament/TournamentCard.module.scss';
+
+export default function TournamentCard({ tournamentName }: TournamentInfo) {
+  return (
+    <div className={styles.tournamentCardContainer}>
+      <div className={styles.text}>{tournamentName}</div>
+    </div>
+  );
 }
--- pages/tournament.tsx
+++ pages/tournament.tsx
@@ -1,6 +1,7 @@
 import { useSetRecoilState } from 'recoil';
 import { Modal } from 'types/modalTypes';
 import { modalState } from 'utils/recoil/modal';
+import TournamentCard from 'components/tournament/TournamentCard';
 import styles from 'styles/tournament/TournamentContainer.module.scss';
 
 // 내부의 토너먼트 보여주는 부분만 Component화 하면될까? 다른곳에서도 쓰일까?
@@ -23,12 +24,15 @@ export default function Tournament() {
     <div className={styles.pageWrap}>
       <h1 className={styles.title}>Tournament</h1>
       <div className={styles.tournamentContainer}>
-        <h4 className={styles.tournamentText}> 열려있는 토너먼트 </h4>
+        <div className={styles.tournamentTextWait}> 대기중인 토너먼트 </div>
         <div className={styles.waitTournamentBox}>
-          <div onClick={handleTournament}> 제1회 루키 토너먼트 </div>
-          <div onClick={handleTournament}> 제2회 루키 토너먼트 </div>
+          <TournamentCard tournamentName='제 1회 루키 토너먼트' />
+          <TournamentCard tournamentName='제 1회 마스터 토너먼트' />
+          <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
+          <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
+          <TournamentCard tournamentName='무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트 무엇인가 겁나 긴 이름의 토너먼트' />
         </div>
-        <h4 className={styles.tournamentText}> 진행중인 토너먼트 </h4>
+        <div className={styles.tournamentTextOpen}> 진행중인 토너먼트 </div>
         <div className={styles.openTournamentBox}>
           <div className={styles.tournamentText}> 무언가 토너먼트의 사진 </div>
         </div>
