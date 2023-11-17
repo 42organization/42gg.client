4777c6b2ab969ae5fb5f44d1255706013b62a489
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-11-17 16:40:58 +0900

    [Feat] 대기중인 토너먼트 인피니트 스크롤 #1090

--- pages/tournament.tsx
+++ pages/tournament.tsx
@@ -1,112 +1,43 @@
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { TournamentInfo } from 'types/tournamentTypes';
 import { instance } from 'utils/axios';
+import { InfiniteScroll } from 'utils/infinityScroll';
+import { mockInstance } from 'utils/mockAxios';
 import { errorState } from 'utils/recoil/error';
+import { InfiniteScrollComponent } from 'components/store/InfiniteScrollComponent';
 import TournamentCard from 'components/tournament/TournamentCard';
 import styles from 'styles/tournament/TournamentContainer.module.scss';
-
-//나중에 삭제 필요
-interface TempData {
-  tournaments: TournamentInfo[];
-  totalPage: number;
-}
-
-// 내부의 토너먼트 보여주는 부분만 Component화 하면될까? 다른곳에서도 쓰일까?
-// 진행중인 토너먼트의 Bracket을 보여주는 부분도 다른곳에서도 쓸수있지않나?
-const tempData: TempData = {
-  tournaments: [
-    {
-      tournamentId: 5,
-      title: '5회 루키 토너먼트',
-      contents: '블라블라',
-      startTime: new Date(),
-      status: '종료',
-      type: 'ROOKIE',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '7회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '8회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '9회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '10회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '11회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-    {
-      tournamentId: 6,
-      title: '12회 마스터 토너먼트',
-      contents: '블라블라 하이하이',
-      startTime: new Date(),
-      status: '진행중',
-      type: 'MASTER',
-      endTime: new Date(),
-    },
-  ],
-  totalPage: 100,
-};
 
 export default function Tournament() {
   const setError = useSetRecoilState(errorState);
 
-  // const openTorunamentInfo = useQuery<TournamentInfo>(
-  //   'openTorunamentInfo',
-  //   () =>
-  //     instance
-  //       .get('pingpong/tournaments?page=*&type=*&status=진행중')
-  //       .then((res) => res.data),
-  //   { retry: 1, staleTime: 60000 /* 60초 */ }
+  // const Info = useQuery<TournamentInfo>(
+  //     'openTorunamentInfo',
+  //     () =>
+  //       mockInstance
+  //         .get('tournaments?page=1&status=진행중')
+  //         .then((res) => res.data),
+  //     { retry: 1, staleTime: 60000 /* 60초 */ }
   // );
 
-  // const waitTournamentInfo = useQuery<TournamentInfo>(
-  //   'waitTournamentInfo',
-  //   () =>
-  //     instance
-  //       .get('pingpong/tournaments?page=*&type=*&status=예정')
-  //       .then((res) => res.data),
-  //   { retry: 1, staleTime: 60000 /* 60초 */ }
-  // );
+  function fetchWaitTournamentData(page: number) {
+    return mockInstance
+      .get(`tournaments?page=${page}&type=rookie&status=예정&size=4`)
+      .then((res) => {
+        return res.data;
+      });
+  }
 
-  // if (openTorunamentInfo.isError || waitTournamentInfo.isError) {
-  //   setError('junhjeon');
-  // }
+  const { data, error, isLoading, hasNextPage, fetchNextPage } = InfiniteScroll(
+    'waitTournament',
+    fetchWaitTournamentData,
+    'JY04'
+  );
+
+  // useEffect(() => {
+  //   console.log(data?.pages);
+  // }, [data]);
 
   return (
     <div className={styles.pageWrap}>
@@ -114,9 +45,24 @@ export default function Tournament() {
       <div className={styles.tournamentContainer}>
         <div className={styles.tournamentTextWait}> 대기중인 토너먼트 </div>
         <div className={styles.waitTournamentBox}>
-          {tempData.tournaments.map((tournament, index) => (
-            <TournamentCard key={index} {...tournament} />
+          {data?.pages.map((page, pageIndex) => (
+            <div key={pageIndex}>
+              {page.tournaments.map(
+                (tournament: TournamentInfo, tournamentIndex: number) => (
+                  <TournamentCard key={tournamentIndex} {...tournament} />
+                )
+              )}
+              {/* 실제로는 tournamnetId 를 key값으로 사용하는게 좋음, 현재는 mockdata에서 id 값들이 겹치기 때문에 Index로 사용
+              {page.tournaments.map((tournament: TournamentInfo, tournamentIndex: number) => (
+                <TournamentCard key={tournament.tournamentId} {...tournament} />
+              ))} 
+            */}
+            </div>
           ))}
+          <InfiniteScrollComponent
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+          />
         </div>
         <div className={styles.tournamentTextOpen}> 진행중인 토너먼트 </div>
         <div className={styles.openTournamentBox}>
