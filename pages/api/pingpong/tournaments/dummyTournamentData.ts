2d236e5ef9f57fb89a81037e2d79bcc4022931df
Clearsu <clearwater1219@gmail.com>
2023-11-17 16:24:33 +0900

    [Feat] mock 데이터 수정 #1088

---
+++ pages/api/pingpong/tournaments/dummyTournamentData.ts
@@ -0,0 +1,72 @@
+import { IUser } from 'types/admin/adminUserTypes';
+import { TournamentInfo } from 'types/tournamentTypes';
+
+const jincpark: IUser = {
+  id: 1,
+  intraId: 'jincpark',
+  userImageUri:
+    'https://42gg-public-image.s3.ap-northeast-2.amazonaws.com/images/jincpark.jpeg',
+  statusMessage: 'I am fine, thank you. And you?',
+  roleType: 'USER',
+};
+
+const jaehyuki: IUser = {
+  id: 2,
+  intraId: 'jaehyuki',
+  userImageUri:
+    'https://42gg-public-image.s3.ap-northeast-2.amazonaws.com/images/jaehyuki-7f6689c3-bf24-4e87-a04b-de9d61f1bef8.jpeg',
+  statusMessage: 'I am fine, thank you. And you?',
+  roleType: 'USER',
+};
+
+const junhjeon: IUser = {
+  id: 3,
+  intraId: 'junhjeon',
+  userImageUri:
+    'https://42gg-public-image.s3.ap-northeast-2.amazonaws.com/images/junhjeon.jpeg',
+  statusMessage: 'I am fine, thank you. And you?',
+  roleType: 'USER',
+};
+
+const users: IUser[] = [jincpark, jaehyuki, junhjeon];
+
+const dummyTournaments: TournamentInfo[] = [];
+
+for (let i = 28; i >= 1; i--) {
+  let status;
+  if (i === 28) {
+    status = '예정';
+  } else if (i === 27) {
+    status = '진행중';
+  } else {
+    status = '종료';
+  }
+
+  const rookieTournament: TournamentInfo = {
+    tournamentId: i * 2,
+    title: `${i}회 루키 토너먼트`,
+    contents: '블라블라',
+    startTime: new Date(),
+    endTime: new Date(),
+    status: status,
+    type: 'rookie',
+    winner: users[i % 3],
+    player_cnt: 8,
+  };
+
+  const masterTournament: TournamentInfo = {
+    tournamentId: i * 2 - 1,
+    title: `${i}회 마스터 토너먼트`,
+    contents: '블라블라',
+    startTime: new Date(),
+    endTime: new Date(),
+    status: status,
+    type: 'master',
+    winner: users[i % 3],
+    player_cnt: 8,
+  };
+
+  dummyTournaments.push(masterTournament, rookieTournament);
+}
+
+export default dummyTournaments;
