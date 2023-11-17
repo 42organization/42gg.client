8d2af4ca89cc0b29da3c606293e82f1c1b6bcf16
Clearsu <clearwater1219@gmail.com>
2023-11-17 23:03:37 +0900

    [Feat] 토너먼트전체조회 API 확정 #1088

--- types/tournamentTypes.ts
+++ types/tournamentTypes.ts
@@ -1,12 +1,11 @@
-import { IUser } from './admin/adminUserTypes';
-
 export interface TournamentInfo {
   tournamentId: number;
   title: string;
   contents: string;
   status: string; // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE'
   type: 'custom' | 'master' | 'rookie';
-  winner?: IUser;
+  winnerIntraId: string;
+  winnerImageUrl: string;
   startTime: Date;
   endTime: Date;
   player_cnt: number;
