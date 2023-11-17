1fab28d5aab4fab0ced436e48a3c26ec8eefad16
Clearsu <clearwater1219@gmail.com>
2023-11-17 16:23:56 +0900

    [Feat] prop 추가 #1088

--- pages/tournament-record.tsx
+++ pages/tournament-record.tsx
@@ -10,7 +10,7 @@ export default function TournamentRecord() {
         <button>Master</button>
         <button>Custom</button>
       </div>
-      <WinnerSwipeView />
+      <WinnerSwipeView type='rookie' />
       <div className={styles.winnerInfoContainer}>
         <p className={styles.userId}>cadet2147</p>
         <p className={styles.gameInfo}>
