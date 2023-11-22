774587ecba81c2b466c72ab7c383f7b10eb25d90
Clearsu <clearwater1219@gmail.com>
2023-11-22 20:19:29 +0900

    [Feat] 토너먼트 정보 표시 #1104

--- pages/tournament-record.tsx
+++ pages/tournament-record.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import { TournamentInfo } from 'types/tournamentTypes';
 import WinnerSwiper from 'components/tournament-record/WinnerSwiper';
 import styles from 'styles/tournament-record/TournamentRecord.module.scss';
 
 export default function TournamentRecord() {
+  const [tournamentInfo, setTournamentInfo] = useState<TournamentInfo>();
+  const endTime = tournamentInfo ? new Date(tournamentInfo.endTime) : null;
+
   return (
     <div className={styles.pageWrap}>
       <h1 className={styles.title}>명예의 전당</h1>
@@ -10,14 +15,18 @@ export default function TournamentRecord() {
         <button>Master</button>
         <button>Custom</button>
       </div>
-      <WinnerSwiper type='ROOKIE' size={5} />
+      <WinnerSwiper
+        type='ROOKIE'
+        size={5}
+        setTournamentInfo={setTournamentInfo}
+      />
       <div className={styles.winnerInfoContainer}>
-        <p className={styles.userId}>cadet2147</p>
+        <p className={styles.userId}>{tournamentInfo?.winnerIntraId}</p>
         <p className={styles.gameInfo}>
-          제 5회 정기 토너먼트 루키리그{' '}
+          {tournamentInfo?.title}{' '}
           <span className={styles.highlighted}>우승자</span>
         </p>
-        <p className={styles.date}>2023.11.11</p>
+        <p className={styles.date}>{endTime?.toLocaleDateString()}</p>
       </div>
       <div className={styles.bracketContainer}></div>
     </div>
