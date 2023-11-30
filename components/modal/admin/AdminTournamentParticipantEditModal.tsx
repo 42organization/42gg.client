e6ac3f1aa33fa97d1233b5520cf34abb999dc2b2
Clearsu <clearwater1219@gmail.com>
2023-11-30 15:50:20 +0900

    [Feat] 토너먼트 유저 검색 구현 #1120

--- components/modal/admin/AdminTournamentParticipantEditModal.tsx
+++ components/modal/admin/AdminTournamentParticipantEditModal.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { Button, Input } from '@mui/material';
+import { Button } from '@mui/material';
 import { instanceInManage } from 'utils/axios';
 import { modalState } from 'utils/recoil/modal';
 import { toastState } from 'utils/recoil/toast';
+import TournamentSearchBarGroup from 'components/admin/tournament/TournamentSearchBarGroup';
 import styles from 'styles/admin/modal/AdminTournamentParticipantEditModal.module.scss';
 
 export default function AdminTournamentParticipantEditModal(props: {
@@ -12,13 +13,12 @@ export default function AdminTournamentParticipantEditModal(props: {
   const MAX_LENGTH = 30;
   const setModal = useSetRecoilState(modalState);
   const setSnackBar = useSetRecoilState(toastState);
+
   return (
     <div className={styles.whole}>
       <h2>참가자 수정</h2>
       <div className={styles.hr}></div>
-      <Input placeholder='인트라 ID를 입력하세요.'></Input>
-      <Button variant='outlined'>검색</Button>
-      <Button variant='contained'>추가</Button>
+      <TournamentSearchBarGroup />
       <ul>
         <li>
           jincpark
@@ -28,7 +28,6 @@ export default function AdminTournamentParticipantEditModal(props: {
         </li>
       </ul>
       <div className={styles.buttonGroup}>
-        <Button variant='contained'>적용</Button>
         <Button variant='outlined'>취소</Button>
       </div>
     </div>
