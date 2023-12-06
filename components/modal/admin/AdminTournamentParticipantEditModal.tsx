2f4c14b6b48d7b02bc01836cf824d2a510ab72ab
Clearsu <clearwater1219@gmail.com>
2023-12-06 12:57:13 +0900

    [Feat] 참가자 리스트 #1134

--- components/modal/admin/AdminTournamentParticipantEditModal.tsx
+++ components/modal/admin/AdminTournamentParticipantEditModal.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { Button } from '@mui/material';
+import { ITournamentUser } from 'types/admin/adminTournamentTypes';
 import { instanceInManage } from 'utils/axios';
+import { mockInstance } from 'utils/mockAxios';
 import { modalState } from 'utils/recoil/modal';
 import { toastState } from 'utils/recoil/toast';
 import TournamentSearchBarGroup from 'components/admin/tournament/TournamentSearchBarGroup';
@@ -12,19 +14,48 @@ export default function AdminTournamentParticipantEditModal(props: {
 }) {
   const setModal = useSetRecoilState(modalState);
   const setSnackBar = useSetRecoilState(toastState);
+  const [userToAdd, setUserToAdd] = useState<ITournamentUser>({
+    userId: 0,
+    intraId: '',
+    isJoined: false,
+  });
+  const [participantList, setParticipantList] = useState<ITournamentUser[]>([
+    { userId: 0, intraId: '', isJoined: false },
+  ]);
+
+  const fetchParticipantList = useCallback(async () => {
+    try {
+      const res: { data: { users: ITournamentUser[] } } =
+        await mockInstance.get(
+          `/admin/tournaments/${props.tournamentId}/users`
+        );
+      setParticipantList(res.data.users);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [props.tournamentId]);
+
+  useEffect(() => {
+    fetchParticipantList();
+  }, [fetchParticipantList]);
 
   return (
     <div className={styles.whole}>
       <h2>참가자 수정</h2>
       <div className={styles.hr}></div>
-      <TournamentSearchBarGroup />
+      <TournamentSearchBarGroup
+        onAddUser={setUserToAdd}
+        tournamentId={props.tournamentId}
+      />
       <ul>
-        <li>
-          jincpark
-          <Button variant='outlined' color='error'>
-            삭제
-          </Button>
-        </li>
+        {participantList.map((participant) => (
+          <li key={participant.userId}>
+            {participant.intraId}
+            <Button variant='outlined' color='error'>
+              삭제
+            </Button>
+          </li>
+        ))}
       </ul>
       <div className={styles.buttonGroup}>
         <Button
