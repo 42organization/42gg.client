d4bb3b117de6d40975fda056cd3b956adcc0f2a6
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-12-11 20:07:57 +0900

    Feat/관리자 토너먼트 페이지 유저 수정 모달 UI 및 mock api 연동 #1134 (#1140)
    * [Feat] 토너먼트 참가인원 수정 모달 연결 #1134
    
    * [Feat] 의존성 배열에 인자 추가 #1134
    
    * [Feat] 취소 버튼 시 모달 닫기 #1134
    
    * [Feat] 세로 스크롤 활성화 #1134
    
    * [Feat] 유저 추가 기능 #1134
    
    * [Feat] 참가자 리스트 #1134
    
    * [Chore] 디렉토리 명 변경 #1134
    
    * [Feat] 유저 추가 시 리스트 업데이트 #1134
    
    * [Feat] 관리자 토너먼트 유저 수정 모달 유저 삭제 기능 #1134
    
    * [Fix] 잘못된 쿼리 수정 #1134
    
    * [Feat] 참가자 및 대기자 구분 UI #1134
    
    * [Style] 스타일 변경 #1134
    
    * [Style] 스타일 변경 #1134
    
    * [Feat] 유저 삭제 후 참가자 리스트 업데이트 #1134
    
    * [Refactor] 참가자 리스트 컴포넌트 분리 #1134
    
    * [Chore] 디렉토리 이동 #1134
    
    * [Refactor] 검색결과 드롭다운 컴포넌트 분리 #1134
    
    * [Chore] 컴포넌트명 변경 #1134
    
    * [Refactor] 검색창 컴포넌트 분리 #1134
    
    * [Refactor] 검색창 컴포넌트 분리 #1134
    
    * [Feat] 신청 시간 타입 추가 #1134
    
    * [Feat] snackbar 메시지 추가 #1134
    
    * [Feat] 에러 응답 메시지 표시 #1134
    
    * [Feat] 검색창 및 취소 버튼 위치 고정 #1134
    
    * [Feat] 참가자 삭제 확인 인풋창 #1134

--- pages/api/pingpong/admin/tournaments/[tournamentId]/users/[userId].ts
+++ pages/api/pingpong/admin/tournaments/[tournamentId]/users/[userId].ts
@@ -5,12 +5,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(405).end('허용되지 않는 메소드입니다.');
     return;
   }
-  const { intraId } = req.query;
-  if (
-    intraId !== 'jincpark' &&
-    intraId !== 'junhjeon' &&
-    intraId !== 'jaehyuki'
-  ) {
+  const { userId } = req.query;
+  if (userId !== 'jincpark' && userId !== 'junhjeon' && userId !== 'jaehyuki') {
     res.status(404).end('존재하지 않는 유저입니다.');
   }
   res.status(204).end('명단에서 유저 삭제 성공');
