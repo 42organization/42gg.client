487ecca86a192d460d28b54a7b6f49dacdea7038
Clearsu <clearwater1219@gmail.com>
2023-12-12 21:48:31 +0900

    [Fix] 머지 오류 수정 #1074

--- components/tournament-record/UserTournamentBracket.tsx
+++ components/tournament-record/UserTournamentBracket.tsx
@@ -1,8 +1,9 @@
 import { Match } from '@g-loot/react-tournament-brackets/dist/src/types';
-import { useCallback, useEffect, useState } from 'react';
-import { TournamentGame } from 'types/tournamentTypes';
+import { useQuery } from 'react-query';
+import { useSetRecoilState } from 'recoil';
 import { instance } from 'utils/axios';
 import { convertTournamentGamesToBracketMatchs } from 'utils/handleTournamentGame';
+import { errorState } from 'utils/recoil/error';
 import TournamentBraket from 'components/tournament/TournamentBraket';
 import useComponentSize from 'hooks/util/useComponentSize';
 import styles from 'styles/tournament-record/UserTournamentBracket.module.scss';
@@ -18,7 +19,9 @@ export default function UserTournamentBraket({
   const [ref, size] = useComponentSize<HTMLDivElement>();
 
   const fetchTournamentGames = async () => {
-    const res = await mockInstance.get(`/tournament/${tournamentId}/games`);
+    const res = await instance.get(
+      `/pingpong/tournaments/${tournamentId}/games`
+    );
     return convertTournamentGamesToBracketMatchs(res.data.games);
   };
 
