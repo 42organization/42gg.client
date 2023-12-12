0c6d3236b98a3f9ab4b05923d0a95e94e03c912b
Clearsu <clearwater1219@gmail.com>
2023-12-12 21:04:36 +0900

    [Feat] 진행중 혹은 예정인 토너먼트 경기가 있을 경우에만 섹션 표시 #1149

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next';
 import SearchBar from 'components/main/SearchBar';
 import Section from 'components/main/Section';
+import useBeforeLiveTournamentData from 'hooks/tournament/useBeforeLiveTournamentData';
 import styles from 'styles/main/Home.module.scss';
 
 const Home: NextPage = () => {
+  const tournamentData = useBeforeLiveTournamentData();
+
   return (
     <div className={styles.container}>
       <SearchBar />
-      <Section path='tournament' sectionTitle={'Tournament'} />
+      {tournamentData && (
+        <Section path='tournament' sectionTitle={'Tournament'} />
+      )}
       <Section path='rank' sectionTitle={'Ranking'} />
       <Section path='game' sectionTitle={'Current Play'} />
     </div>
