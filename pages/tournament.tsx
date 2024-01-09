c29ee05634346e6db9d7b922eb70d2a6949627b1
Junho jeon <junhjeon@Junhoui-MacBookAir.local>
2024-01-09 15:26:58 +0900

    [Style] 위치 수정 #1232

--- pages/tournament.tsx
+++ pages/tournament.tsx
@@ -15,7 +15,14 @@ export default function Tournament() {
 
   return (
     <div className={styles.pageWrap}>
-      <h1 className={styles.title}>Tournament</h1>
+      <div className={styles.titleWrap}>
+        <h1 className={styles.title}>Tournament</h1>
+        <div className={styles.buttonWrap}>
+          <button className={styles.manual} onClick={openManual}>
+            경기 규칙
+          </button>
+        </div>
+      </div>
       <div className={styles.tournamentText}> 예정된 토너먼트 </div>
       {data?.beforeTournament.length === 0 ? (
         <div className={styles.noTournamentText}>
@@ -31,12 +38,6 @@ export default function Tournament() {
         </div>
       )}
       <div className={styles.tournamentText}> 진행중인 토너먼트 </div>
-      <div className={styles.buttonWrap}>
-        <button className={styles.manual} onClick={openManual}>
-          {' '}
-          경기 규칙
-        </button>
-      </div>
       {data?.liveTournament?.length === 0 ? (
         <div className={styles.noTournamentText}>
           진행중인 토너먼트가 없습니다.
