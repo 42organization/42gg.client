dcf4c46d7a87f1ba0830958119e24ecba2b964f0
yoouyeon <jyeon.yoon59@gmail.com>
2024-03-04 14:53:47 +0900

    [Design] 메인 페이지 헤더 디자인 적용

--- pages/recruit/index.tsx
+++ pages/recruit/index.tsx
@@ -1,7 +1,18 @@
+import Lottie from 'lottie-react';
+import recruitPingpong from 'public/lottie/recruitPingPong.json';
+import commonStyle from 'styles/recruit/common.module.scss';
+import layoutStyle from 'styles/recruit/layout.module.scss';
+import textStyle from 'styles/recruit/text.module.scss';
+
 function Recruit() {
   return (
-    <div>
-      <div>지원하기</div>
+    <div className={layoutStyle.recruitContainer}>
+      <span className={textStyle.pageTitle}>42GG 팀원 모집</span>
+      <Lottie
+        className={commonStyle.pingpongAnimation}
+        animationData={recruitPingpong}
+        width={'100%'}
+      />
     </div>
   );
 }
