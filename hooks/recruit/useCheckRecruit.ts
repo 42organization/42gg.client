09d40dc541fd3fb8da093e85213bc957efce95f0
yoouyeon <jyeon.yoon59@gmail.com>
2024-03-04 15:17:29 +0900

    [Feat] 모집 기간 아님 페이지 추가

--- hooks/recruit/useCheckRecruit.ts
+++ hooks/recruit/useCheckRecruit.ts
@@ -4,15 +4,16 @@ import { mockInstance } from 'utils/mockAxios';
 
 const useCheckRecruit = () => {
   const [isRecruiting, setIsRecruiting] = useState<boolean>(false);
-  const { data } = useQuery(['checkRecruit'], async () => {
+  const { data, isLoading } = useQuery(['checkRecruit'], async () => {
     const res = await mockInstance.get('/recruitments');
     return res.data;
   });
   useEffect(() => {
-    if (data && data.recruitments) setIsRecruiting(true);
+    if (data && data.recruitments && data.recruitments.length !== 0)
+      setIsRecruiting(true);
     else setIsRecruiting(false);
   }, [data]);
-  return { isRecruiting };
+  return { recruitments: data?.recruitments, isRecruiting, isLoading };
 };
 
 export default useCheckRecruit;
--- pages/recruit/index.tsx
+++ pages/recruit/index.tsx
@@ -1,10 +1,32 @@
 import Lottie from 'lottie-react';
 import recruitPingpong from 'public/lottie/recruitPingPong.json';
+import useCheckRecruit from 'hooks/recruit/useCheckRecruit';
 import commonStyle from 'styles/recruit/common.module.scss';
 import layoutStyle from 'styles/recruit/layout.module.scss';
 import textStyle from 'styles/recruit/text.module.scss';
 
 function Recruit() {
+  const { isRecruiting, isLoading } = useCheckRecruit();
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (!isLoading && !isRecruiting) {
+    return (
+      <div className={layoutStyle.recruitContainer}>
+        <Lottie
+          className={commonStyle.pingpongAnimation}
+          animationData={recruitPingpong}
+          width={'100%'}
+        />
+        <span className={textStyle.pageSubTitle}>
+          지금은 신규 팀원 모집 기간이 아닙니다.
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className={layoutStyle.recruitContainer}>
       <span className={textStyle.pageTitle}>42GG 팀원 모집</span>
