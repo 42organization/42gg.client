a6e1ffc4f77078cd6712822c4e4e629de3f0f8e4
kimjaehyuk <jhkim0097@gmail.com>
2024-03-05 19:48:58 +0900

    [Feat] 질문지 항목 작성 #1256

--- components/admin/recruitments/recruitmentsEdit/components/QuestionFormBuilder.tsx
+++ components/admin/recruitments/recruitmentsEdit/components/QuestionFormBuilder.tsx
@@ -1,3 +1,54 @@
+import styles from 'styles/admin/recruitments/recruitmentEdit/components/QuestionFormBuilder.module.scss';
+
+function TextInput() {
+  return <div>TextInput</div>;
+}
+
+function CheckBoxInput() {
+  return <div>CheckBoxInput</div>;
+}
+
+function RadioInput() {
+  return <div>RadioButtonInput</div>;
+}
+
+interface QuestionProps {
+  inputType: 'TEXT' | 'CHECKBOX' | 'RADIO';
+}
+
+function Question({ inputType }: QuestionProps) {
+  return (
+    <div className={styles.questionContainer}>
+      <div className={styles.questionWrapper}>
+        <input type='text' placeholder='질문' />
+        <select
+          name='type'
+          value='CUSTOM'
+          // onChange={selectChangehandler}
+        >
+          <option value='TEXT'>TEXT</option>
+          <option value='CHECKBOX'>CHECKBOX</option>
+          <option value='RADIO'>RADIO</option>
+        </select>
+      </div>
+      <div>
+        {inputType === 'TEXT' && <TextInput />}
+        {inputType === 'CHECKBOX' && <CheckBoxInput />}
+        {inputType === 'RADIO' && <RadioInput />}
+      </div>
+    </div>
+  );
+}
+
 export default function QuestionFormBuilder() {
-  return <div>QuestionFormBuilder</div>;
+  return (
+    <>
+      <div className={styles.mainContainer}>
+        <Question inputType='TEXT' />
+      </div>
+      <div className={styles.editConsole}>
+        <button>질문지 항목 추가하기</button>
+      </div>
+    </>
+  );
 }
