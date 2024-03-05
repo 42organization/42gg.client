b26958f344182868c860a4ac6ade4a4360b76643
kimjaehyuk <jhkim0097@gmail.com>
2024-03-05 16:23:08 +0900

    [Feat] setPage props 추가 #1256

--- components/admin/recruitments/recruitmentsMain.tsx
+++ components/admin/recruitments/recruitmentsMain.tsx
@@ -1,12 +1,25 @@
+import { Dispatch, SetStateAction } from 'react';
 import RecruitmentsHistoryList from 'components/admin/recruitments/recruitmentsHistoryList';
 import styles from 'styles/admin/store/StoreMain.module.scss';
+import RecruitmentEdit from './recruitmentsEdit/RecruitmentEdit';
 
-function RecruitmentsMain() {
+interface RecruitmentsMainProps {
+  setPage: Dispatch<SetStateAction<JSX.Element>>;
+}
+
+function RecruitmentsMain({ setPage }: RecruitmentsMainProps) {
   //return menutab
   return (
     <div className={styles.mainContainer}>
       <div className={styles.subContainer}>
-        <button className={styles.sectionTitle}>지원 공고 등록</button>
+        <button
+          className={styles.sectionTitle}
+          onClick={() => {
+            setPage(<RecruitmentEdit setPage={setPage} />);
+          }}
+        >
+          지원 공고 등록
+        </button>
       </div>
       <div className={styles.subContainer}>
         <div className={styles.sectionTitle}>변경 이력</div>
