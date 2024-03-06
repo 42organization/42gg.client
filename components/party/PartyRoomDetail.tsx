566ed75f01ac8c180f153171fb73795db07743e7
juha <contemplation.person@gmail.com>
2024-03-06 14:28:32 +0900

    feat: 방 나가기 버튼 위치 변경, 댓글 갯수 추가, 날짜 추가, 신고 버튼 추가

--- components/party/PartyRoomDetail.tsx
+++ components/party/PartyRoomDetail.tsx
@@ -5,12 +5,27 @@ import { mockInstance } from 'utils/mockAxios';
 type PartyRoomDetailProps = { partyRoomDetail: PartyRoomDetail };
 
 function PartyDescription({ partyRoomDetail }: PartyRoomDetailProps) {
+  const router = useRouter();
+
   return (
     <div>
+      {partyRoomDetail.myNickname && (
+        <button
+          onClick={async () => {
+            mockInstance
+              .patch(`/party/rooms/${partyRoomDetail.roomId}`)
+              .then(() => {
+                router.push(`/parties/${partyRoomDetail.roomId}`);
+              });
+          }}
+        >
+          방 나가기
+        </button>
+      )}
       <button
         onClick={() => {
           navigator.clipboard.writeText(
-            // window.location.href // TODO: 공유하기 버튼에서 dev에서 이게 정상작동 하는지 확인할 필요가 있음
+            // TODO: 공유하기 버튼에서 dev에서 이게 정상작동 하는지 확인할 필요가 있음
             `http://42gg.kr/parties/${partyRoomDetail.roomId}`
           );
         }}
@@ -26,19 +41,20 @@ function PartyDescription({ partyRoomDetail }: PartyRoomDetailProps) {
           `분 남음`}
       </span>
       <p>내용 : {partyRoomDetail.content}</p>
+      <span>주최자 : {partyRoomDetail.hostNickname}</span>
       <span>
         인원 : {partyRoomDetail.currentPeople + '/' + partyRoomDetail.maxPeople}
       </span>
-      <span>주최자 : {partyRoomDetail.hostNickname}</span>
-      <span>참여자 : {/* 호버로 인원 보여주기. */}</span>
-      <span>방 상황 : {partyRoomDetail.roomStatus}</span>
+      <span>
+        방 상황 : {partyRoomDetail.roomStatus /*TODO: 배포시 나중에 지울 것 */}
+      </span>
     </div>
   );
 }
 
 function PartyButtton({ partyRoomDetail }: PartyRoomDetailProps) {
   const router = useRouter();
-  // TODO: 호스트도 나가기 버튼 필요.
+
   return partyRoomDetail.roomStatus !== 'FINISH' ? (
     <div>
       {partyRoomDetail.hostNickname === partyRoomDetail.myNickname ? (
@@ -55,18 +71,6 @@ function PartyButtton({ partyRoomDetail }: PartyRoomDetailProps) {
             시작
           </button>
         )
-      ) : partyRoomDetail.myNickname ? (
-        <button
-          onClick={async () => {
-            mockInstance
-              .patch(`/party/rooms/${partyRoomDetail.roomId}`)
-              .then(() => {
-                router.push(`/parties/${partyRoomDetail.roomId}`);
-              });
-          }}
-        >
-          취소하기
-        </button>
       ) : (
         <button
           onClick={async () => {
@@ -88,17 +92,31 @@ function PartyButtton({ partyRoomDetail }: PartyRoomDetailProps) {
 
 function PartyComment({ partyRoomDetail }: PartyRoomDetailProps) {
   const router = useRouter();
+  const totalComments = partyRoomDetail.comments.length;
+
   return (
     <div>
       <h3>댓글</h3>
-      {/*총 댓글 수랑, 댓글 달린 시간, 신고버튼*/}
+      <span>총 {totalComments}개의 댓글이 있습니다.</span>
       {partyRoomDetail.comments.map((comment) =>
         comment.isHidden ? (
           `숨김 처리된 댓글입니다.`
         ) : (
           <div key={comment.commentId}>
-            <h4>{comment.nickname}</h4>
-            <h4>{comment.content}</h4>
+            <span>{comment.nickname}</span>
+            <span>{comment.content}</span>
+            <span>
+              {new Date(comment.createDate).getMinutes() -
+                new Date().getMinutes()}
+              분 전
+            </span>
+            <button
+              onClick={() => {
+                /* 신고 로직 */
+              }}
+            >
+              신고
+            </button>
           </div>
         )
       )}
