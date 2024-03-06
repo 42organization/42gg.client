3b591cea25588d83b2d1eca178d283444299091d
PHJoon <phj9503@gmail.com>
2024-03-06 23:48:55 +0900

    [Fix] useRecruitDetail - refetchOnWindowFocus false 설정

--- hooks/recruit/useRecruitDetail.ts
+++ hooks/recruit/useRecruitDetail.ts
@@ -3,13 +3,14 @@ import { IRecruitmentDetail } from 'types/recruit/recruitments';
 import { mockInstance } from 'utils/mockAxios';
 
 const useRecruitDetail = ({ id }: { id: number }) => {
-  const { data, isLoading } = useQuery<IRecruitmentDetail>(
-    ['recruitDetail', id],
-    async () => {
+  const { data, isLoading } = useQuery<IRecruitmentDetail>({
+    queryKey: ['recruitDetail', id],
+    queryFn: async () => {
       const res = await mockInstance.get(`/recruitments/${id}`);
       return res.data;
-    }
-  );
+    },
+    refetchOnWindowFocus: false,
+  });
 
   return { data, isLoading };
 };
