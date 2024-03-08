40864df1004bf403e7556eb046d128c1f7ad81ed
PHJoon <phj9503@gmail.com>
2024-03-08 19:31:49 +0900

    [Feat] ModalProvider - recruitment 추가

--- components/modal/ModalProvider.tsx
+++ components/modal/ModalProvider.tsx
@@ -6,6 +6,7 @@ import AdminModal from 'components/modal/modalType/AdminModal';
 import NormalModal from 'components/modal/modalType/NormalModal';
 import StoreModal from 'components/modal/modalType/StoreModal';
 import styles from 'styles/modal/Modal.module.scss';
+import RecruitmentModal from './modalType/RecruitmentModal';
 import TournamentModal from './modalType/TournamentModal';
 
 export default function ModalProvider() {
@@ -46,6 +47,8 @@ export default function ModalProvider() {
           <AdminModal />
         ) : modalType === 'TOURNAMENT' ? (
           <TournamentModal />
+        ) : modalType === 'RECRUITMENT' ? (
+          <RecruitmentModal />
         ) : null}
       </div>
     )
