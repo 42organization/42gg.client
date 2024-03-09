ec5fd98983268a7eca2fd6d06b581beed849b78a
yoouyeon <jyeon.yoon59@gmail.com>
2024-03-09 23:00:02 +0900

    [Feat] useRecruitDetail 훅 적용

--- pages/recruit/[id]/index.tsx
+++ pages/recruit/[id]/index.tsx
@@ -1,5 +1,25 @@
+import { useRouter } from 'next/router';
+import useRecruitDetail from 'hooks/recruit/useRecruitDetail';
+
 function Recruit() {
-  return <>Recruit</>;
+  const router = useRouter();
+  const { data, isLoading } = useRecruitDetail({
+    id: parseInt(router.query.id as string),
+  });
+
+  // TODO : 구체화 필요함.
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+  if (!data) {
+    return <div>공고가 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <span>{data.title}</span>
+    </>
+  );
 }
 
 export default Recruit;
