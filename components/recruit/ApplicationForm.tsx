a13ec2f227735befb0a162876eb80d41415be580
PHJoon <phj9503@gmail.com>
2024-03-09 17:49:19 +0900

    [Style] ApplicationForm 스타일 수정

--- components/recruit/ApplicationForm.tsx
+++ components/recruit/ApplicationForm.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { MutableRefObject, useRef } from 'react';
 import { useSetRecoilState } from 'recoil';
 import {
@@ -30,11 +31,12 @@ export default function ApplicationForm({ recruitId }: { recruitId: number }) {
   const { data, isLoading } = useRecruitDetail({ recruitId });
   const setModal = useSetRecoilState(modalState);
   const formRefs = useRef<IRefs[]>([]);
+  const router = useRouter();
 
   const submitApplicationForm = () => {
     const answerList = [];
     for (const answer of formRefs.current) {
-      if (answer.type === 'TEXT') {
+      if (answer.type === 'TEXT' && answer.ref?.value.length !== 0) {
         answerList.push({
           questionsId: answer.id,
           inputType: answer.type,
@@ -60,6 +62,8 @@ export default function ApplicationForm({ recruitId }: { recruitId: number }) {
         }
       }
     }
+    if (answerList.length === 0) return;
+    console.log(answerList);
     setModal({
       modalName: 'RECRUITMENT-APPLY',
       recruitId: recruitId,
@@ -67,17 +71,35 @@ export default function ApplicationForm({ recruitId }: { recruitId: number }) {
     });
   };
 
-  if (isLoading) {
-    return <div>로딩중...</div>;
-  }
+  const goBack = () => {
+    router.back();
+  };
 
-  if (!data || Object.keys(data).length === 0) {
-    return <div>지원서 항목이 없습니다</div>;
+  if (isLoading || !data || Object.keys(data).length === 0) {
+    return (
+      <div className={applicationStyle.form}>
+        <div className={applicationStyle.titleContainer}></div>
+        <div className={applicationStyle.questionContainer}>
+          <div className={applicationStyle.backTitle}>
+            {isLoading ? '로딩중...' : '지원서 항목이 없습니다'}
+          </div>
+          <Button
+            className={applicationStyle.backBtn}
+            variant='contained'
+            onClick={goBack}
+          >
+            뒤로가기
+          </Button>
+        </div>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <div className={applicationStyle.titleContainer}>{data.title}</div>
+    <div className={applicationStyle.form}>
+      <div className={applicationStyle.titleContainer}>
+        {data.title} {data.generations} 모집
+      </div>
       <div className={applicationStyle.bodyContainer}>
         {data.form.map((form: IQuestionForm, index: number) => (
           <div className={applicationStyle.questionContainer} key={index}>
@@ -110,7 +132,6 @@ export default function ApplicationForm({ recruitId }: { recruitId: number }) {
       <div className={applicationStyle.btnContainer}>
         <Button
           className={applicationStyle.submitBtn}
-          sx={{ borderRadius: '1rem', fontSize: '1.5rem' }}
           variant='contained'
           onClick={() => submitApplicationForm()}
         >
@@ -131,17 +152,18 @@ function TextForm({
   refs: MutableRefObject<IRefs[]>;
 }) {
   return (
-    <div>
+    <>
       <div className={applicationStyle.questionText}>{question}</div>
       <TextField
-        id='filled-basic'
-        label='Filled'
-        variant='filled'
+        className={applicationStyle.textField}
+        label='답변을 적어주세요'
+        multiline
+        rows={5}
         inputRef={(ref) =>
           refs.current.push({ id: qestionId, type: 'TEXT', ref: ref })
         }
       />
-    </div>
+    </>
   );
 }
 
@@ -160,12 +182,14 @@ function SingleCheckForm({
     <FormControl>
       <div className={applicationStyle.questionText}>{question}</div>
       <RadioGroup
+        className={applicationStyle.radioBoxGroup}
         aria-labelledby='radio-buttons-group-label'
         name='radio-buttons-group'
       >
         {checkList?.map((check: ICheck) => {
           return (
             <FormControlLabel
+              className={applicationStyle.radioBox}
               key={check.id}
               value={check.contents}
               control={<Radio />}
@@ -198,9 +222,9 @@ function MultiCheckForm({
   refs: MutableRefObject<IRefs[]>;
 }) {
   return (
-    <div>
+    <FormControl>
       <div className={applicationStyle.questionText}>{question}</div>
-      <FormGroup>
+      <FormGroup className={applicationStyle.checkBoxGroup}>
         {checkList?.map((check: ICheck) => {
           return (
             <FormControlLabel
@@ -220,6 +244,6 @@ function MultiCheckForm({
           );
         })}
       </FormGroup>
-    </div>
+    </FormControl>
   );
 }
