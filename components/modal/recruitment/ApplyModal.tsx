71051489779384a29f8c5705101952ce51aa1f6b
PHJoon <phj9503@gmail.com>
2024-03-11 11:42:24 +0900

    [Refactor] snackbar => mui snackBar로 변경

--- components/modal/recruitment/ApplyModal.tsx
+++ components/modal/recruitment/ApplyModal.tsx
@@ -8,17 +8,24 @@ import {
 } from 'react';
 import { useMutation } from 'react-query';
 import {
+  Alert,
+  Box,
   Button,
-  Container,
   Dialog,
-  DialogTitle,
   Slide,
-  Typography,
+  Snackbar,
+  SnackbarOrigin,
 } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
 import { IApplicantAnswer } from 'types/recruit/recruitments';
 import { mockInstance } from 'utils/mockAxios';
 import styles from 'styles/modal/recruit/ApplyModal.module.scss';
+
+interface ISnackBarState extends SnackbarOrigin {
+  snackBarOpen: boolean;
+  message: string;
+  severity: 'success' | 'error';
+}
 
 const Transition = forwardRef(function Transition(
   props: TransitionProps & {
@@ -40,10 +47,23 @@ export default function ApplyModal({
   recruitId: number;
   applicantAnswers: IApplicantAnswer[];
 }) {
-  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [snackBarState, setSnackBarState] = useState<ISnackBarState>({
+    snackBarOpen: false,
+    vertical: 'bottom',
+    horizontal: 'left',
+    message: '',
+    severity: 'error',
+  });
 
-  const onClose = () => {
+  const { snackBarOpen, vertical, horizontal, message, severity } =
+    snackBarState;
+
+  const onModalClose = () => {
     setModalOpen(false);
+  };
+
+  const onSanckBarClose = () => {
+    setSnackBarState({ ...snackBarState, snackBarOpen: false });
   };
 
   const { mutate } = useMutation(
@@ -58,72 +78,67 @@ export default function ApplyModal({
   const onApply = () => {
     mutate(applicantAnswers, {
       onSuccess: () => {
-        // setSnackBar({
-        //   toastName: 'apply',
-        //   severity: 'success',
-        //   message: '지원되었습니다.',
-        //   clicked: true,
-        // });
+        setSnackBarState((prev) => ({
+          ...prev,
+          snackBarOpen: true,
+          message: '지원되었습니다.',
+          severity: 'success',
+        }));
       },
       onError: () => {
-        // setSnackBar({
-        //   toastName: 'apply',
-        //   severity: 'error',
-        //   message: '요청에 문제가 발생했습니다.',
-        //   clicked: true,
-        // });
+        setSnackBarState((prev) => ({
+          ...prev,
+          snackBarOpen: true,
+          message: '요청에 문제가 발생했습니다.',
+          severity: 'error',
+        }));
       },
     });
   };
 
-  if (!modalOpen || applicantAnswers.length === 0) return;
+  if (!modalOpen || applicantAnswers.length === 0) return <></>;
 
   return (
-    <Dialog onClose={onClose} open={modalOpen} TransitionComponent={Transition}>
-      <DialogTitle>지원서를 제출할까요?</DialogTitle>
-      <Typography component={'div'}>
-        제출한 지원서는 제출 마감 전까지 수정하거나 삭제할 수 있습니다.
-      </Typography>
-      <Container>
-        <Button
-          className={styles.cancelBtn}
-          variant='outlined'
-          onClick={onClose}
-        >
-          취소
-        </Button>
-        <Button
-          className={styles.applyBtn}
-          variant='contained'
-          onClick={onApply}
-        >
-          제출하기
-        </Button>
-      </Container>
-    </Dialog>
+    <>
+      <Dialog
+        onClose={onModalClose}
+        open={modalOpen}
+        TransitionComponent={Transition}
+        sx={{ borderRadius: '3rem' }}
+      >
+        <Box className={styles.container}>
+          <div className={styles.title}>지원서를 제출할까요?</div>
+          <div className={styles.content}>
+            제출한 지원서는 제출 마감 전까지 수정하거나 삭제할 수 있습니다.
+          </div>
+          <Box className={styles.btnContainer}>
+            <Button
+              className={styles.cancelBtn}
+              variant='outlined'
+              onClick={onModalClose}
+            >
+              취소
+            </Button>
+            <Button
+              className={styles.applyBtn}
+              variant='contained'
+              onClick={onApply}
+            >
+              제출하기
+            </Button>
+          </Box>
+        </Box>
+      </Dialog>
+      <Snackbar
+        open={snackBarOpen}
+        anchorOrigin={{ vertical, horizontal }}
+        onClose={onSanckBarClose}
+        autoHideDuration={6000}
+      >
+        <Alert onClose={onSanckBarClose} severity={severity} variant={'filled'}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
   );
-  // return (
-  //   <div className={styles.container}>
-  //     <div className={styles.title}>지원서를 제출할까요?</div>
-  //     <div className={styles.content}>
-  //       제출한 지원서는 제출 마감 전까지 수정하거나 삭제할 수 있습니다.
-  //     </div>
-  //     <div className={styles.btnContainer}>
-  //       <Button
-  //         className={styles.cancelBtn}
-  //         variant='outlined'
-  //         onClick={onCancel}
-  //       >
-  //         취소
-  //       </Button>
-  //       <Button
-  //         className={styles.applyBtn}
-  //         variant='contained'
-  //         onClick={onApply}
-  //       >
-  //         제출하기
-  //       </Button>
-  //     </div>
-  //   </div>
-  // );
 }
