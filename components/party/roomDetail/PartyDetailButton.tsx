c1413984b7c99e6787b8703d730224d94472f1ea
juha <contemplation.person@gmail.com>
2024-03-12 20:56:03 +0900

    [Feat] party detail 버튼 컴포넌트

---
+++ components/party/roomDetail/PartyDetailButton.tsx
@@ -0,0 +1,100 @@
+import { useRouter } from 'next/router';
+import { useSetRecoilState } from 'recoil';
+import { instance } from 'utils/axios';
+import { modalState } from 'utils/recoil/modal';
+
+type ParytButtonProps = {
+  roomId?: number;
+  commentId?: number;
+};
+
+function ReportComment({ commentId }: ParytButtonProps) {
+  const setModal = useSetRecoilState(modalState);
+
+  return (
+    <button
+      onClick={() => {
+        setModal({
+          partyReport: {
+            name: 'COMMENT',
+            commentId: commentId,
+          },
+          modalName: 'PARTY-REPORT',
+        });
+      }}
+    >
+      신고
+    </button>
+  );
+}
+
+function ReportRoom({ roomId }: ParytButtonProps) {
+  const setModal = useSetRecoilState(modalState);
+
+  return (
+    <button
+      onClick={() => {
+        setModal({
+          partyReport: {
+            name: 'ROOM',
+            roomId: roomId,
+          },
+          modalName: 'PARTY-REPORT',
+        });
+      }}
+    >
+      신고
+    </button>
+  );
+}
+
+function ShareRoom() {
+  const roomId = useRouter().query.roomId;
+
+  return (
+    <button
+      onClick={() => {
+        navigator.clipboard.writeText(`http://42gg.kr/parties/${roomId}`);
+      }}
+    >
+      공유하기
+    </button>
+  );
+}
+
+function JoinRoom({ roomId }: ParytButtonProps) {
+  const handlerJoin = async () => {
+    await instance.post(`/party/rooms/${roomId}/join`).catch((error) => {
+      console.error(error);
+    });
+  };
+
+  <button onClick={handlerJoin}>참여하기</button>;
+}
+
+function LeaveRoom({ roomId }: ParytButtonProps) {
+  const handlerExit = async () => {
+    await instance.patch(`/party/rooms/${roomId}`).catch((error) => {
+      console.error(error);
+    });
+  };
+
+  return <button onClick={handlerExit}>방 나가기</button>;
+}
+
+function BackRoomList() {
+  const router = useRouter();
+
+  return <button onClick={() => router.push('/party')}></button>;
+}
+
+const PartyRoomDetailButton = {
+  ReportComment,
+  ReportRoom,
+  ShareRoom,
+  JoinRoom,
+  LeaveRoom,
+  BackRoomList,
+};
+
+export default PartyRoomDetailButton;
