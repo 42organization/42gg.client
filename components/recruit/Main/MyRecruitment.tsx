bce729a1a82eba25396a9ec253f30cd76927a565
yoouyeon <jyeon.yoon59@gmail.com>
2024-03-13 21:25:31 +0900

    [Feat] MyApplicationInfo 추가

--- components/recruit/Main/MyRecruitment.tsx
+++ components/recruit/Main/MyRecruitment.tsx
@@ -4,6 +4,7 @@ import useGetRecruitResult from 'hooks/recruit/useGetRecruitResult';
 import style from 'styles/recruit/Main/myRecruitment.module.scss';
 import textStyle from 'styles/recruit/text.module.scss';
 import CollapseListItem from './CollapseListItem';
+import MyApplicationInfo from './MyApplicationInfo';
 
 const MyRecruitment = () => {
   const router = useRouter();
@@ -14,7 +15,10 @@ const MyRecruitment = () => {
       <span className={textStyle.pageSubTitle}>나의 지원현황</span>
       <List className={style.listContainer}>
         <CollapseListItem title={'지원서 정보'}>
-          <div className={style.collapseContainer}>지원서 정보</div>
+          <MyApplicationInfo
+            status={status}
+            recruitId={router.query.id as string}
+          />
         </CollapseListItem>
         <CollapseListItem title={'지원 현황'}>
           <div className={style.collapseContainer}>지원현황</div>
