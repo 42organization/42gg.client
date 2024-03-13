2b1244d9f39aecf1fe1b65721a1053c72c5aaf41
jaeywon <dnsjwodyd@gmail.com>
2024-03-13 18:36:24 +0900

    [Feat] 방 신고 탭 구현 , usePartyRoomReport 훅 생성

---
+++ components/admin/party/AdminPartyRoomReport.tsx
@@ -0,0 +1,54 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from '@mui/material';
+import { tableFormat } from 'constants/admin/table';
+import { AdminTableHead } from 'components/admin/common/AdminTable';
+import { usePartyRoomReport } from 'hooks/party/usePartyRoomReport';
+import styles from 'styles/party/PartyMain.module.scss';
+
+const tableTitle: { [key: string]: string } = {
+  id: '번호',
+  reporterIntraId: '신고자 이름',
+  reporteeIntraId: '피신고자 이름',
+  roomId: '방',
+  message: '메세지',
+  createdAt: '시간',
+};
+
+export default function AdminPartyRoomReport() {
+  const { roomReports } = usePartyRoomReport();
+
+  return (
+    <div className={styles.userManagementWrap}>
+      <div className={styles.header}>
+        <span className={styles.title}>방 신고리스트</span>
+      </div>
+      <TableContainer className={styles.tableContainer} component={Paper}>
+        <Table className={styles.table} aria-label='UserManagementTable'>
+          <AdminTableHead tableName={'partyRoomReport'} table={tableTitle} />
+          <TableBody className={styles.tableBody}>
+            {roomReports.map((r) => (
+              <TableRow key={r.id} className={styles.tableRow}>
+                {tableFormat['partyRoomReport'].columns.map((columnName) => {
+                  return (
+                    <TableCell
+                      className={styles.tableBodyItem}
+                      key={columnName}
+                    >
+                      {/* r[columnName as keyof PartyRoomReport]?.toString() */}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
