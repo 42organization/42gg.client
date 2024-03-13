93a447ea446cb65f4bc888302fd8c50356e13cd3
yoouyeon <jyeon.yoon59@gmail.com>
2024-03-13 21:10:42 +0900

    [Feat] useGetRecruitResult 훅 추가

--- components/recruit/Main/MyRecruitment.tsx
+++ components/recruit/Main/MyRecruitment.tsx
@@ -1,9 +1,14 @@
+import { useRouter } from 'next/router';
 import { List } from '@mui/material';
+import useGetRecruitResult from 'hooks/recruit/useGetRecruitResult';
 import style from 'styles/recruit/Main/myRecruitment.module.scss';
 import textStyle from 'styles/recruit/text.module.scss';
 import CollapseListItem from './CollapseListItem';
 
 const MyRecruitment = () => {
+  const router = useRouter();
+  const { data } = useGetRecruitResult(router.query.id as string);
+  const { status, interviewDate } = data || {}; // TODO : 에러가 난 경우 적절한 처리 필요함.
   return (
     <div className={style.container}>
       <span className={textStyle.pageSubTitle}>나의 지원현황</span>
