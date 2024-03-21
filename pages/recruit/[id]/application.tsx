9fee5b9c5a051520c4182ec6a83dd8a70ce441a7
PHJoon <phj9503@gmail.com>
2024-03-21 20:33:57 +0900

    [Feat] 지원서 지원/ 확인수정 페이지 분리

--- pages/recruit/[id]/application.tsx
+++ pages/recruit/[id]/application.tsx
@@ -1,24 +1,13 @@
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { applicationAlertState } from 'utils/recoil/application';
 import ApplicationForm from 'components/recruit/Application/ApplicationForm';
 import ApplicatoinFormFooter from 'components/recruit/Application/ApplicationFormFooter';
 import ApplicationFormHeader from 'components/recruit/Application/ApplicationFormHeader';
-import ApplicationSnackBar from 'components/recruit/Application/ApplicationSnackBar';
 
 function Application() {
-  const setAlertState = useSetRecoilState(applicationAlertState);
-
-  useEffect(() => {
-    setAlertState((prev) => ({ ...prev, alertState: false }));
-  }, []);
-
   return (
     <>
       <ApplicationFormHeader />
       <ApplicationForm />
       <ApplicatoinFormFooter />
-      <ApplicationSnackBar />
     </>
   );
 }
---
+++ pages/recruit/[id]/apply.tsx
@@ -0,0 +1,13 @@
+import ApplicationForm from 'components/recruit/Application/ApplicationForm';
+import ApplicationFormHeader from 'components/recruit/Application/ApplicationFormHeader';
+
+function Application() {
+  return (
+    <>
+      <ApplicationFormHeader />
+      <ApplicationForm />
+    </>
+  );
+}
+
+export default Application;
