639206fdb0a4eadfd816a63942f721efd7fb64fa
PHJoon <phj9503@gmail.com>
2024-03-22 01:01:36 +0900

    [Fix] 기존 상태관리 다루는 함수 삭제,  폼데이터 다루는 함수 추가

--- utils/handleApplicationForm.ts
+++ utils/handleApplicationForm.ts
@@ -1,96 +1,53 @@
-import { Dispatch, SetStateAction } from 'react';
-import { SetterOrUpdater } from 'recoil';
 import { IApplicantAnswer, IQuestionForm } from 'types/recruit/recruitments';
-import {
-  IapplicationAlertState,
-  IapplicationModal,
-} from './recoil/application';
 
-export const applicationAnswerDefault = (form: IQuestionForm[] | undefined) => {
-  if (!form) return [];
-
-  const defaultAnswer = [];
-  for (const question of form) {
-    defaultAnswer.push({
-      questionId: question.questionId,
-      inputType: question.inputType,
-      checkedList: [],
-      answer: '',
-    });
-  }
-  return defaultAnswer;
-};
-interface IapplicationFormCheckProps {
-  setInvalidInput: Dispatch<SetStateAction<number>>;
-  setAlertState: Dispatch<SetStateAction<IapplicationAlertState>>;
-  setModalState: Dispatch<SetStateAction<IapplicationModal>>;
-  userAnswers: IApplicantAnswer[];
-}
-
-export const applicationFormCheck = (props: IapplicationFormCheckProps) => {
-  const { setInvalidInput, setAlertState, setModalState, userAnswers } = props;
-
-  for (const ans of userAnswers) {
-    if (
-      (ans.inputType === 'TEXT' && !ans.answer?.length) ||
-      ((ans.inputType === 'SINGLE_CHECK' || ans.inputType === 'MULTI_CHECK') &&
-        !ans.checkedList?.length)
-    ) {
-      setInvalidInput(ans.questionId);
-      setAlertState({
-        alertState: true,
-        severity: 'error',
-        message: '입력하지 않은 문항이 있습니다.',
-      });
-      return;
-    }
-  }
-  setModalState({ state: true, content: 'APPLY' });
-};
-
-interface IupdateUserAnswersProps {
-  updateAnswer: IApplicantAnswer;
-  userAnswers: IApplicantAnswer[];
-  setUserAnswers: SetterOrUpdater<IApplicantAnswer[]>;
-}
-
-export function updateUserAnswers(props: IupdateUserAnswersProps) {
-  const { updateAnswer, userAnswers, setUserAnswers } = props;
-  const updateUserAnswers = [...userAnswers];
-
-  for (let i = 0; i < userAnswers.length; i++) {
-    if (userAnswers[i].questionId === updateAnswer.questionId) {
-      updateUserAnswers.splice(i, 1, updateAnswer);
-      setUserAnswers(updateUserAnswers);
-      return;
-    }
-  }
-
-  // 새로운 값 대입
-  updateUserAnswers.push(updateAnswer);
-  updateUserAnswers.sort((a, b) => a.questionId - b.questionId);
-  setUserAnswers(updateUserAnswers);
-}
-
-export function findUserAnswer(id: number, answerList: IApplicantAnswer[]) {
-  if (!answerList) return '';
-  for (const answer of answerList) {
-    if (answer.questionId === id) {
-      return answer;
-    }
-  }
-}
-
-export const inputDefault = (form: IQuestionForm) => {
-  const textInput = {
+const answerDefault = (form: IQuestionForm): IApplicantAnswer => {
+  const textDefault = {
     questionId: form.questionId,
     inputType: form.inputType,
     answer: '',
   };
-  const checkInput = {
+  const checkDefault = {
     questionId: form.questionId,
     inputType: form.inputType,
-    checkedList: [],
+    checkedList: <number[]>[],
   };
-  return form.inputType === 'TEXT' ? textInput : checkInput;
+  return form.inputType === 'TEXT' ? textDefault : checkDefault;
+};
+
+const applicationAnswerDefault = (form: IQuestionForm[]) => {
+  const answerForms = [];
+  for (const question of form) {
+    answerForms.push(answerDefault(question));
+  }
+  return answerForms;
+};
+
+/**
+ * @param formData 지원서 폼에 입력된 데이터
+ * @param questionForms 지원서 질문 항목들
+ * @returns 폼에 입력된 데이터를 질문들에 맞춰서 IapplicantAnswer[] 형태로 반환
+ */
+export const applicationAnswerSet = (
+  formData: FormData,
+  questionForms: IQuestionForm[]
+) => {
+  const answerForms = applicationAnswerDefault(questionForms);
+  for (const [key, value] of formData.entries()) {
+    const formDataKey = parseInt(key);
+    const answer = answerForms.find(
+      (answer) => answer.questionId === formDataKey
+    );
+    if (answer) {
+      if (answer.inputType === 'TEXT') {
+        answer.answer = value as string;
+      }
+      if (
+        answer.inputType === 'SINGLE_CHECK' ||
+        answer.inputType === 'MULTI_CHECK'
+      ) {
+        answer.checkedList?.push(parseInt(value as string));
+      }
+    }
+  }
+  return answerForms;
 };
