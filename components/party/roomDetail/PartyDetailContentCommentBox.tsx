db307e20339c3004c97826e8c83faef07837ab2c
conteng <contemplation.person@gmail.com>
2024-03-30 06:07:12 +0900

    [style] css 파일 정리

--- components/party/roomDetail/PartyDetailContentCommentBox.tsx
+++ components/party/roomDetail/PartyDetailContentCommentBox.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { IoSendSharp } from 'react-icons/io5';
 import {
   PartyComment,
@@ -8,6 +9,7 @@ import {
 import { instance } from 'utils/axios';
 import { nameToRGB } from 'utils/color';
 import { dateToKRLocaleTimeString } from 'utils/handleTime';
+import { toastState } from 'utils/recoil/toast';
 import styles from 'styles/party/PartyDetailRoom.module.scss';
 import PartyRoomDetailButton from './PartyDetailButton';
 
@@ -30,8 +32,8 @@ export default function PartyDetailContentCommentBox({
   return (
     <>
       <div className={styles.contentCommentBox}>
-        <div className={styles.commentContainer}>
-          <div className={styles.comment}>댓글 ({totalComments})</div>
+        <div className={styles.commentBoxInfo}>
+          <div>댓글 ({totalComments})</div>
           <button className={styles.spreadBtn} onClick={onSpreadComment}>
             {spreadbtnName}
           </button>
@@ -41,13 +43,13 @@ export default function PartyDetailContentCommentBox({
           comments={partyRoomDetail.comments}
           isSpreadComment={isSpreadComment}
         />
+        <CommentCreateBar
+          roomId={partyRoomDetail.roomId}
+          status={partyRoomDetail.status}
+          myNickname={partyRoomDetail.myNickname}
+          fetchRoomDetail={fetchRoomDetail}
+        />
       </div>
-      <CommentCreateBar
-        roomId={partyRoomDetail.roomId}
-        status={partyRoomDetail.status}
-        myNickname={partyRoomDetail.myNickname}
-        fetchRoomDetail={fetchRoomDetail}
-      />
     </>
   );
 }
@@ -120,10 +122,26 @@ function CommentCreateBar({
   fetchRoomDetail,
 }: CommentCreateBarProps) {
   const [comment, setComment] = useState('');
+  const setSnackbar = useSetRecoilState(toastState);
 
   const handleCommentSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!comment) {
+      setSnackbar({
+        toastName: 'input warning',
+        message: '입력이 없습니다.',
+        severity: 'warning',
+        clicked: true,
+      });
+      return;
+    }
+    if (comment.length > 100) {
+      setSnackbar({
+        toastName: 'input warning',
+        message: `100자가 넘었습니다. (현재: ${comment.length}자)`,
+        severity: 'warning',
+        clicked: true,
+      });
       return;
     }
     await instance.post(`/party/rooms/${roomId}/comments`, {
@@ -142,18 +160,19 @@ function CommentCreateBar({
   }
 
   return (
-    <div className={styles.commentCreateBar}>
-      <form onSubmit={handleCommentSubmit}>
+    <form onSubmit={handleCommentSubmit}>
+      <div className={styles.commentCreateBar}>
         <input
           className={styles.textBar}
           onInput={handleCommentInput}
           type='text'
           value={comment}
+          placeholder='댓글을 입력하세요. (최대 100자)'
         />
         <button className={styles.inputBtn} type='submit'>
           <IoSendSharp />
         </button>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }
