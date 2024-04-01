70a24a70486546e47a6752e699d2f05bb7025273
juha <contemplation.person@gmail.com>
2024-04-01 18:00:13 +0900

    [Feat] alert 창으로 띄워주던 것을 토스트바로 바꿈.

--- components/modal/Party/PartyReportModal.tsx
+++ components/modal/Party/PartyReportModal.tsx
@@ -1,8 +1,10 @@
+import { AxiosError } from 'axios';
 import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { Modal, PartyReportModalData } from 'types/modalTypes';
 import { instance } from 'utils/axios';
 import { modalState } from 'utils/recoil/modal';
+import { toastState } from 'utils/recoil/toast';
 import styles from 'styles/modal/menu/ReportModal.module.scss';
 import { ModalButton, ModalButtonContainer } from '../ModalButton';
 
@@ -10,6 +12,7 @@ export function PartyReportModal({ report }: { report: PartyReportModalData }) {
   const [isLoading, setIsLoading] = useState(false);
   const [content, setContent] = useState('');
   const setModal = useSetRecoilState<Modal>(modalState);
+  const setSnackbar = useSetRecoilState(toastState);
 
   const reportHandler = async () => {
     const reportResponse: { [key: string]: string } = {
@@ -17,44 +20,48 @@ export function PartyReportModal({ report }: { report: PartyReportModalData }) {
       REJECT: '내용을 적어주세요 ❤️',
     };
 
+    if (!content.replace(/^\s+|\s+$/g, '')) {
+      alert('칸을 입력하지 않았습니다.');
+      throw new Error('REJECT');
+    }
+
     try {
-      if (!content.replace(/^\s+|\s+$/g, '')) {
-        throw new Error('칸을 입력하지 않았습니다.');
-      }
       switch (report.name) {
         case 'COMMENT':
-          await instance
-            .post(`/party/reports/comments/${report.commentId}`, {
-              content: content,
-            })
-            .catch((e) => {
-              throw new Error(e.response.data.message);
-            });
+          await instance.post(`/party/reports/comments/${report.commentId}`, {
+            content: content,
+          });
           break;
         case 'ROOM':
-          await instance
-            .post(`/party/reports/rooms/${report.roomId}`, {
-              content: content,
-            })
-            .catch((e) => {
-              throw new Error(e.response.data.message);
-            });
+          await instance.post(`/party/reports/rooms/${report.roomId}`, {
+            content: content,
+          });
           break;
         case 'NOSHOW':
-          await instance
-            .post(
-              `/party/reports/rooms/${report.roomId}/users/${report.userIntraId}`,
-              { content: content }
-            )
-            .catch((e) => {
-              throw new Error(e.response.data.message);
-            });
+          await instance.post(
+            `/party/reports/rooms/${report.roomId}/users/${report.userIntraId}`,
+            { content: content }
+          );
           break;
       }
       setModal({ modalName: null });
-      alert(reportResponse.SUCCESS);
+      setSnackbar({
+        toastName: `report request`,
+        severity: 'success',
+        message: `${reportResponse.SUCCESS}`,
+        clicked: true,
+      });
     } catch (e) {
-      alert(e);
+      const error = e as AxiosError;
+      if (error.response && error.response.data) {
+        const message = (error.response.data as { message: string }).message;
+        setSnackbar({
+          toastName: `bad request`,
+          severity: 'error',
+          message: `🔥 ${message} 🔥`,
+          clicked: true,
+        });
+      }
       throw new Error('REJECT');
     }
   };
@@ -75,7 +82,7 @@ export function PartyReportModal({ report }: { report: PartyReportModalData }) {
     <div className={styles.container}>
       <div>
         <div className={styles.title}>42GG</div>
-        <div className={styles.phrase}>{`${reportName}`}</div>
+        <div className={styles.phrase}>{reportName}</div>
       </div>
       <form>
         <div className={styles.contentWrapper}>
