9e41c60b52c10d9ee3fa13033ebaef8aa07524dd
jaeywon <dnsjwodyd@gmail.com>
2024-04-02 11:21:13 +0900

    [Fix] 각각의 관리탭 페이지라우터로 변경

---
+++ pages/admin/partyTemplate.tsx
@@ -0,0 +1,9 @@
+import AdminPartyTemplate from 'components/admin/party/PartyTemplate';
+
+export default function PartyTemplate() {
+  return (
+    <>
+      <AdminPartyTemplate />
+    </>
+  );
+}
