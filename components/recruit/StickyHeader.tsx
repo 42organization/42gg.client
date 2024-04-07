21cb1c03dd67c2dc106cda44e7e53d221fc9dd70
yoouyeon <jyeon.yoon59@gmail.com>
2024-04-08 00:24:32 +0900

    [Feat] 메인 페이지인 경우에는 메인 이동 버튼 제거

--- components/recruit/StickyHeader.tsx
+++ components/recruit/StickyHeader.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { HomeOutlined } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import commonStyle from 'styles/recruit/common.module.scss';
@@ -6,20 +6,23 @@ import textStyle from 'styles/recruit/text.module.scss';
 
 const StickyHeader = ({ headerTitle }: { headerTitle: string }) => {
   const router = useRouter();
+  const pathName = usePathname();
   const goHome = () => {
     router.push('/recruit');
   };
   return (
     <div className={commonStyle.stickyHeader}>
       <span className={textStyle.headerTitle}>{headerTitle}</span>
-      <Button
-        className={commonStyle.homeBtn}
-        variant='text'
-        startIcon={<HomeOutlined />}
-        onClick={goHome}
-      >
-        모집 메인
-      </Button>
+      {pathName !== '/recruit' && (
+        <Button
+          className={commonStyle.homeBtn}
+          variant='text'
+          startIcon={<HomeOutlined />}
+          onClick={goHome}
+        >
+          모집 메인
+        </Button>
+      )}
     </div>
   );
 };
