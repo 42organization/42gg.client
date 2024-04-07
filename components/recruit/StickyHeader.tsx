83cf8af0aa3efb14f4679402dcdd19d725a5a86a
yoouyeon <jyeon.yoon59@gmail.com>
2024-04-08 00:11:44 +0900

    [Feat] StickyHeader에 모집 메인 버튼 추가

--- components/recruit/StickyHeader.tsx
+++ components/recruit/StickyHeader.tsx
@@ -1,12 +1,25 @@
+import { useRouter } from 'next/navigation';
+import { HomeOutlined } from '@mui/icons-material';
+import { Button } from '@mui/material';
 import commonStyle from 'styles/recruit/common.module.scss';
 import textStyle from 'styles/recruit/text.module.scss';
 
 const StickyHeader = ({ headerTitle }: { headerTitle: string }) => {
+  const router = useRouter();
+  const goHome = () => {
+    router.push('/recruit');
+  };
   return (
     <div className={commonStyle.stickyHeader}>
-      <div className={commonStyle.stickyContainer}>
-        <span className={textStyle.pageTitle}>{headerTitle}</span>
-      </div>
+      <span className={textStyle.headerTitle}>{headerTitle}</span>
+      <Button
+        className={commonStyle.homeBtn}
+        variant='text'
+        startIcon={<HomeOutlined />}
+        onClick={goHome}
+      >
+        모집 메인
+      </Button>
     </div>
   );
 };
