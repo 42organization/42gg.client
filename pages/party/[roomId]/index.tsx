8360fd95e69f7d5d36d44f228f1004fda548e596
juha <contemplation.person@gmail.com>
2024-04-08 21:54:38 +0900

    [Bug] 버그 찾는 콘솔 로그

--- pages/party/[roomId]/index.tsx
+++ pages/party/[roomId]/index.tsx
@@ -15,6 +15,8 @@ export default function PartyDetailPage() {
     PartyRoomDetail | undefined
   >(undefined);
 
+  console.log('roomId:', roomId);
+
   useEffect(() => {
     fetchRoomDetail();
   }, []);
