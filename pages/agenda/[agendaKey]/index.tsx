1592dc40c6d0393abc20a52d25e2fe68e6b5d678
irenee-14 <ireneee145@gmail.com>
2024-07-10 12:28:07 +0900

    Feat: agenda 상단 상세정보 컴포넌트

--- pages/agenda/[agendaKey]/index.tsx
+++ pages/agenda/[agendaKey]/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import AgendaInfo from 'components/agenda/agendaDetail/AgendaInfo';
+import styles from 'styles/agenda/agendaDetail/AgendaDetail.module.scss';
 
 export default function AgendaDetail() {
   const router = useRouter();
@@ -8,9 +10,16 @@ export default function AgendaDetail() {
 
   return (
     <>
-      <div>agenda 상세정보</div>
-      <div key={teamUID}>
-        <Link href={`/agenda/${agendaKey}/${teamUID}`}>1번 팀</Link>
+      <div className={styles.agendaDetailWrap}>
+        <AgendaInfo />
+        {/* <AgendaTap />
+        <AgendaContents />
+        <AgendaParticipant />
+        <AgendaAnnounce />
+        <AgendaCondition /> */}
+        <div key={teamUID}>
+          <Link href={`/agenda/${agendaKey}/${teamUID}`}>1번 팀</Link>
+        </div>
       </div>
     </>
   );
