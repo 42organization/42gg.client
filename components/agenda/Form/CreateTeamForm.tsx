e5faa3756965154a26c36a618b827e0b2983e282
siwolee <siwolee@student.42seoul.kr>
2024-07-15 10:42:25 +0900

    feat: finish agenda-create page / submit btn

--- components/agenda/Form/CreateTeamForm.tsx
+++ components/agenda/Form/CreateTeamForm.tsx
@@ -4,7 +4,7 @@ import DescriptionInput from 'components/agenda/Input/DescriptionInput';
 import ImageInput from 'components/agenda/Input/ImageInput';
 import TimeInput from 'components/agenda/Input/TimeInput';
 import TitleInput from 'components/agenda/Input/TitleInput';
-import styles from 'styles/agenda/Form/CreateTeamForm.module.scss';
+import styles from 'styles/agenda/Form/Form.module.scss';
 import SubmitInputBtn from '../button/SubmitInputButton';
 
 interface CreateTeamFormProps {
@@ -30,12 +30,12 @@ const CreateTeamForm = ({ handleSubmit }: CreateTeamFormProps) => {
         <p>D-{20}일</p>
       </div>
 
-      <h3 className={styles.label}>등록 가능 팀 제한</h3>
+      <h3 className={styles.container_label}>등록 가능 팀 제한</h3>
       <div className={styles.countContainer}>
         <CountInput name='minTeam' label='최소 팀' min={3} max={50} />
         <CountInput name='maxTeam' label='최대 팀' min={3} max={50} />
       </div>
-      <h3 className={styles.label}>팀당 인원 제한</h3>
+      <h3 className={styles.container_label}>팀당 인원 제한</h3>
       <CheckboxInput name='isSolo' label='개인' />
       <div className={styles.countContainer}>
         <CountInput name='minMember' label='최소' min={1} max={10} value={3} />
@@ -43,7 +43,10 @@ const CreateTeamForm = ({ handleSubmit }: CreateTeamFormProps) => {
       </div>
       <ImageInput name='image' label='포스터 파일 첨부하기' />
       <CheckboxInput name='isPublic' label='대회 유무' />
-      <SubmitInputBtn name='submit' label='팀 만들기' />
+      <div className={styles.buttonContainer}>
+        <SubmitInputBtn name='cancel' label='취소하기' /> {/*새로만들어야 함*/}
+        <SubmitInputBtn name='submit' label='팀 만들기' />
+      </div>
     </form>
   );
 };
--- pages/agenda/[agendaKey]/[teamUID]/create-team.tsx
+++ pages/agenda/[agendaKey]/[teamUID]/create-team.tsx
@@ -28,7 +28,6 @@ const CreateTeam = () => {
       <h2 className={styles.title}>팀 만들기</h2>
       <p className={styles.description}>팀 인원 : 3명-8명까지 가능</p>
       <CreateTeamForm handleSubmit={submitTeamForm} />
-      <button className={styles.submitBtn}>팀 만들기</button>
     </div>
   );
 };
