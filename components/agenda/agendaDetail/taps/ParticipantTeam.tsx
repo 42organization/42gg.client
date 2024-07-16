469abf9b720f864529c5684399703ed0355ec684
irenee-14 <ireneee145@gmail.com>
2024-07-17 00:33:35 +0900

    Feat: 대회 상세정보 - 팀별 참가 시 팀 정보 컴포넌트 추가

---
+++ components/agenda/agendaDetail/taps/ParticipantTeam.tsx
@@ -0,0 +1,33 @@
+import ColorList from 'components/agenda/agendaDetail/taps/ColorList';
+import TeamLeaderIcon from 'public/image/agenda/rock-and-roll-hand.svg';
+import styles from 'styles/agenda/agendaDetail/taps/ParticipantTeam.module.scss';
+export default function participantTeam() {
+  // 팀장이름
+  const host = '팀장 이름';
+  const curPeople = 1;
+  const maxPeople = 4;
+  return (
+    <>
+      <div className={styles.participantTeamContainer}>
+        <div className={styles.titleWarp}>
+          <div className={styles.infoWarp}>
+            <div className={styles.teamTitle}>참가팀</div>
+
+            <div className={styles.teamLeader}>
+              <TeamLeaderIcon className={styles.LeaderIcon} />
+              <p>{host}</p>
+            </div>
+          </div>
+
+          <div className={styles.headCount}>
+            {curPeople} / {maxPeople}
+          </div>
+        </div>
+
+        {/* <div className={styles.coalition}> */}
+        <ColorList />
+        {/* </div> */}
+      </div>
+    </>
+  );
+}
