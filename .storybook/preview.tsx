d970ce8b185fd55c47c5f9ecde6e432ffcc5f5ae
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-07-17 12:34:23 +0900

    [Feat] 11StreetGothic 폰트 추가
    - 스토리북에도 적용 가능하게도 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,6 +4,7 @@ import { StoryFn } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
 import { QueryClient } from 'react-query';
 import { QueryClientProvider } from 'react-query';
+import '../styles/globals.css';
 
 const queryClient = new QueryClient();
 
