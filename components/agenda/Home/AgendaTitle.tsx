818a8459089fe9bc0c32d320e573b4a04f85c378
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-07-17 17:10:07 +0900

    [Feat] AgendaTitle, MyAgendaBtn 벡터 이미지 추가 및 적용

--- components/agenda/Home/AgendaTitle.tsx
+++ components/agenda/Home/AgendaTitle.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image';
 import styles from 'styles/agenda/Home/AgendaTitle.module.scss';
 
 const AgendaTitle = () => {
   return (
     <div className={styles.agendaTitleContainer}>
       <div>AGENDA</div>
-      <button className={styles.agendaCreateBtn}>개최신청 👉🏻</button>
+      <button className={styles.agendaCreateBtn}>
+        <div>개최신청</div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src='/image/agenda/Vector.svg'
+            width={35}
+            height={35}
+            alt='Create Agenda'
+            className={styles.imageBox}
+          />
+        </div>
+      </button>
     </div>
   );
 };
--- components/agenda/Home/MyAgendaBtn.tsx
+++ components/agenda/Home/MyAgendaBtn.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
 import styles from 'styles/agenda/Home/MyAgendaBtn.module.scss';
 
 const MyAgendaBtn = () => {
-  return <button className={styles.myagendaBtn}>my agenda 👉🏻</button>;
+  return (
+    <button className={styles.myagendaBtn}>
+      <div>my agenda</div>
+      <div className={styles.imageWrapper}>
+        <Image
+          src='/image/agenda/Vector.svg'
+          width={35}
+          height={35}
+          alt='Go To My Agenda'
+          className={styles.imageBox}
+        />
+      </div>
+    </button>
+  );
 };
 
 export default MyAgendaBtn;
