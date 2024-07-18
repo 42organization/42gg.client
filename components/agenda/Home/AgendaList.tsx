0e246c051e949337a2ccfd02c828fb965df649cd
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-07-18 13:25:02 +0900

    [Feat] 팀원 작업과 합치기
    - Link 태그 통해서 클릭시, 팀원이 만든 페이지 (아젠다 생성, 대회 상세) 이동 하게 설정

--- components/agenda/Home/AgendaList.tsx
+++ components/agenda/Home/AgendaList.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styles from 'styles/agenda/Home/AgendaList.module.scss';
 
@@ -8,6 +9,7 @@ const AgendaList = () => {
       <div className={styles.agendaListTextWrapper}>AGENDA LIST</div>
       <div className={styles.agendaListItemContainer}>
         <AgendaListBtn />
+        <AgendaListBtn />
       </div>
     </div>
   );
@@ -15,57 +17,59 @@ const AgendaList = () => {
 
 const AgendaListBtn = () => {
   return (
-    <button className={styles.agendaListItemBtn}>
-      <div className={styles.agendaInfoContainer}>
-        <div className={styles.agendaDateBox}>
-          <div className={styles.agendaDateMonth}>12</div>
-          <div className={styles.agendaDateDay}>31</div>
-        </div>
-
-        <div className={styles.agendaInfoWrapper}>
-          <div className={styles.agendaItemTitleBox}>PUSH SWAP 경진대회</div>
-
-          <div className={styles.agendaItemTimeBox}>
-            <div className={styles.agendaItemTimeWrapper}>
-              <div className={styles.imageWrapper}>
-                <Image
-                  src='/image/agenda/Time.svg'
-                  width={15}
-                  height={15}
-                  alt='Time'
-                  className={styles.imageBox}
-                />
-              </div>
-              <div>12.31</div>
-              <div>14:00 ~ 17:00</div>
-            </div>
-
-            <div className={styles.agendaItemTimeWrapper}>
-              <div className={styles.imageWrapper}>
-                <Image
-                  src='/image/agenda/People.svg'
-                  width={15}
-                  height={15}
-                  alt='count'
-                  className={styles.imageBox}
-                />
-              </div>
-              <div>20/100</div>
-            </div>
+    <Link href={`/agenda/1`}>
+      <button className={styles.agendaListItemBtn}>
+        <div className={styles.agendaInfoContainer}>
+          <div className={styles.agendaDateBox}>
+            <div className={styles.agendaDateMonth}>12</div>
+            <div className={styles.agendaDateDay}>31</div>
           </div>
 
-          <div className={styles.agendaItemTagBox}>
-            <AgendaTag tagName='#공식' />
-            <AgendaTag tagName='#팀' />
-            <AgendaTag tagName='#과제' />
+          <div className={styles.agendaInfoWrapper}>
+            <div className={styles.agendaItemTitleBox}>PUSH SWAP 경진대회</div>
+
+            <div className={styles.agendaItemTimeBox}>
+              <div className={styles.agendaItemTimeWrapper}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    src='/image/agenda/Time.svg'
+                    width={15}
+                    height={15}
+                    alt='Time'
+                    className={styles.imageBox}
+                  />
+                </div>
+                <div>12.31</div>
+                <div>14:00 ~ 17:00</div>
+              </div>
+
+              <div className={styles.agendaItemTimeWrapper}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    src='/image/agenda/People.svg'
+                    width={15}
+                    height={15}
+                    alt='count'
+                    className={styles.imageBox}
+                  />
+                </div>
+                <div>20/100</div>
+              </div>
+            </div>
+
+            <div className={styles.agendaItemTagBox}>
+              <AgendaTag tagName='#공식' />
+              <AgendaTag tagName='#팀' />
+              <AgendaTag tagName='#과제' />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.agendaItemDeadLineBox}>
-        <div className={styles.agendaDeadLineText}>모집마감</div>
-        <div className={styles.agendaDeadLine}>D-2</div>
-      </div>
-    </button>
+        <div className={styles.agendaItemDeadLineBox}>
+          <div className={styles.agendaDeadLineText}>모집마감</div>
+          <div className={styles.agendaDeadLine}>D-2</div>
+        </div>
+      </button>
+    </Link>
   );
 };
 
--- components/agenda/Home/AgendaTitle.tsx
+++ components/agenda/Home/AgendaTitle.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from 'styles/agenda/Home/AgendaTitle.module.scss';
 
 const AgendaTitle = () => {
   return (
     <div className={styles.agendaTitleContainer}>
       <div>AGENDA</div>
-      <button className={styles.agendaCreateBtn}>
-        <div>개최신청</div>
-        <div className={styles.imageWrapper}>
-          <Image
-            src='/image/agenda/Vector.svg'
-            width={35}
-            height={35}
-            alt='Create Agenda'
-            className={styles.imageBox}
-          />
-        </div>
-      </button>
+      <Link href={`/agenda/create`}>
+        <button className={styles.agendaCreateBtn}>
+          <div>개최신청</div>
+          <div className={styles.imageWrapper}>
+            <Image
+              src='/image/agenda/Vector.svg'
+              width={35}
+              height={35}
+              alt='Create Agenda'
+              className={styles.imageBox}
+            />
+          </div>
+        </button>
+      </Link>
     </div>
   );
 };
