6f7b8f00ccba77a256e21dc299a06fe7e0ff47ab
irenee-14 <ireneee145@gmail.com>
2024-07-23 01:10:19 +0900

    Feat: 개인/팀, 호스트/참가자/미참가자 별 버튼 구분 및 스토리북 추가

--- components/agenda/agendaDetail/AgendaInfo.tsx
+++ components/agenda/agendaDetail/AgendaInfo.tsx
@@ -1,9 +1,14 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { AgendaStatus } from 'constants/agenda/agenda';
+import {
+  AgendaLocation,
+  AgendaStatus,
+  Coalition,
+} from 'constants/agenda/agenda';
 import { ShareBtn } from 'components/agenda/button/Buttons';
 import { UploadBtn } from 'components/agenda/button/UploadBtn';
 import styles from 'styles/agenda/agendaDetail/AgendaInfo.module.scss';
+
 // 목데이터 생성
 const mockData = {
   agendaTitle: '아 기다리고기다리던대회',
@@ -17,13 +22,23 @@ const mockData = {
   agendaCurrentTeam: 5,
   agendaMinPeople: 2,
   agendaMaxPeople: 5,
-  agendaPoster: null, // 이미지가 없는 경우 null로 설정
+  agendaPoster: null,
   agendaHost: 'iamgroot',
   agendaLocation: '서울',
   agendaStatus: AgendaStatus.ON_GOING,
   createdAt: new Date('2024-07-01'),
   announcementTitle: '대회 공지사항',
   isOfficial: true,
+};
+
+// 개인상세조회
+const profileData = {
+  userIntra: 'iamgroot',
+  userContent: '안녕하세요',
+  userGithub: 'test@github.abc',
+  userCoalition: Coalition.GUN,
+  userLocation: AgendaLocation.SEOUL,
+  ticketCount: 1,
 };
 
 const copyLink = () => {
@@ -36,27 +51,82 @@ const participationIn = () => {
   alert('참여신청이 완료되었습니다.');
 };
 
-export default function AgendaInfo() {
+const hostMode = () => {
+  alert('주최자 관리 버튼입니다.');
+};
+
+const makeTeam = () => {
+  alert('팀 만들기 버튼입니다.');
+};
+
+const submitResults = () => {
+  alert('결과 입력 버튼입니다.');
+};
+
+const isTeam = (agendaData) => {
+  return agendaData.agendaMinPeople !== agendaData.agendaMaxPeople;
+};
+
+const getIsHost = (profileData, agendaData) => {
+  return profileData.userIntra === agendaData.agendaHost;
+};
+
+const getIsParticipant = (teamList) => {
+  return teamList === 200;
+};
+
+// 버튼 텍스트 결정 함수
+const determineButtonText = (agendaStatus, isHost, isParticipant, isTeam) => {
+  if (agendaStatus === AgendaStatus.CONFIRM) {
+    return isHost ? '결과입력' : '';
+  } else if (agendaStatus === AgendaStatus.ON_GOING) {
+    if (isTeam) {
+      return isHost ? '주최자 관리' : !isParticipant ? '팀 만들기' : '';
+    } else {
+      return isHost ? '주최자 관리' : !isParticipant ? '참가하기' : '';
+    }
+  }
+  return '';
+};
+
+// export default function AgendaInfo() {
+// storybook에서 사용하기 위해 인자 넣어줌
+export default function AgendaInfo({
+  agendaData,
+  profileData,
+  teamListStatus,
+}) {
+  /*
   const router = useRouter();
   const { agendaKey } = router.query;
-  // axios로 받아온 데이터
-  // agenda?agenda_id=${agendaKey}
   const [agendaData, setAgendaData] = useState(null);
+  const [teamListStatus, setTeamListStatus] = useState(null);
 
   useEffect(() => {
     // axios.get(`/api/agenda?agenda_id=${agendaKey}`)
     //   .then(response => setAgendaData(response.data))
     //   .catch(error => console.error(error));
 
-    // 목데이터 설정
     setAgendaData(mockData);
+    setTeamListStatus(204);
   }, [agendaKey]);
 
   if (!agendaData) {
     return <div>Loading...</div>;
   }
+*/
 
-  const { agendaTitle, agendaHost } = agendaData;
+  const { agendaTitle, agendaHost, agendaStatus } = agendaData;
+
+  const isHost = getIsHost(profileData, agendaData);
+  const isParticipant = getIsParticipant(teamListStatus);
+
+  const buttonText = determineButtonText(
+    agendaStatus,
+    isHost,
+    isParticipant,
+    isTeam(agendaData)
+  );
 
   return (
     <>
@@ -64,15 +134,25 @@ export default function AgendaInfo() {
         <div className={styles.infoWarp}>
           <div className={styles.contentWarp}>
             <div className={styles.enrollWarp}>
-              <UploadBtn text='참여하기' onClick={participationIn} />
+              {buttonText !== '' && (
+                <UploadBtn
+                  text={buttonText}
+                  onClick={
+                    buttonText === '팀 만들기'
+                      ? makeTeam
+                      : buttonText === '참가하기'
+                      ? participationIn
+                      : buttonText === '주최자 관리'
+                      ? hostMode
+                      : submitResults
+                  }
+                />
+              )}
             </div>
-            {/* 태그 */}
-
             <div className={styles.titleWarp}>
               <h2>{agendaTitle}</h2>
               <ShareBtn onClick={copyLink} />
             </div>
-
             <div className={styles.organizerWrap}>
               <span>주최자 : {agendaHost}</span>
             </div>
