bc7b66c7ba507ad47c2838bf7f2f9c0fe10ab180
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-07-26 15:18:12 +0900

    Feat: AgendaUserSearchBar Component

--- pages/agenda/profile/index.tsx
+++ pages/agenda/profile/index.tsx
@@ -1,7 +1,14 @@
+import AgendaUserSearchBar from 'components/agenda/Profile/AgendaUserSearchBar';
+import styles from 'styles/agenda/Profile/AgendaProfile.module.scss';
+
 export default function AgendaProfile() {
   return (
     <>
-      <div>아젠다 프로필</div>
+      <div className={styles.agendaProfileContainer}>
+        <div className={styles.agendaUserSearchBarWrap}>
+          <AgendaUserSearchBar />
+        </div>
+      </div>
     </>
   );
 }
