54e74482d7e7473fc4ee265bb9761d77ac412e97
irenee-14 <ireneee145@gmail.com>
2024-07-26 11:48:25 +0900

    Feat: agenda admin 세부 페이지 추가

--- components/admin/agenda/SideNavAgenda.tsx
+++ components/admin/agenda/SideNavAgenda.tsx
@@ -16,6 +16,30 @@ export default function SideNavParty() {
       >
         <TbMessageReport className={styles.logo} />
       </SideNavContent>
+      <SideNavContent
+        type={'agenda'}
+        url={'/teamList'}
+        menuName={'팀 관리'}
+        currentPath={currentPath}
+      >
+        <TbMessageReport className={styles.logo} />
+      </SideNavContent>
+      <SideNavContent
+        type={'agenda'}
+        url={'/userList'}
+        menuName={'유저 관리'}
+        currentPath={currentPath}
+      >
+        <TbMessageReport className={styles.logo} />
+      </SideNavContent>
+      <SideNavContent
+        type={'agenda'}
+        url={'/ticket'}
+        menuName={'티켓 관리'}
+        currentPath={currentPath}
+      >
+        <TbMessageReport className={styles.logo} />
+      </SideNavContent>
     </>
   );
 }
---
+++ pages/admin/agenda/userList.tsx
@@ -0,0 +1,4 @@
+import styles from 'styles/admin/takgu/announcement/Announcement.module.scss';
+export default function Announcement() {
+  return <div className={styles.container}>userList</div>;
+}
