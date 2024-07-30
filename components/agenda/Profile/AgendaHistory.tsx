dd1e5b41df11f55ac3749034c4e53c3af08c5626
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-07-30 22:41:39 +0900

    Feat: AgendaHistory

--- components/agenda/Profile/AgendaHistory.tsx
+++ components/agenda/Profile/AgendaHistory.tsx
@@ -1,10 +1,14 @@
+import HistoryItem from 'components/agenda/Profile/HistoryItem';
 import styles from 'styles/agenda/Profile/AgendaHistory.module.scss';
 
 const AgendaHistory = () => {
   return (
     <div className={styles.agendaHistory}>
       <div className={styles.historyTitleText}>아젠다 기록</div>
-      <div className={styles.historyItems}></div>
+      <div className={styles.historyItems}>
+        <HistoryItem />
+        <HistoryItem />
+      </div>
     </div>
   );
 };
