31945de877b492789e6e36fea1ff54ab71ccbd8f
siwolee <siwolee@student.42seoul.kr>
2024-08-01 10:03:20 +0900

    fix: errorChecker 합침-나눠져 있으니까 순서가 꼬여서 _app.tsx로 옮겨야 했음

--- components/error/ErrorChecker.tsx
+++ components/error/ErrorChecker.tsx
@@ -1,22 +1,29 @@
+import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { errorState } from 'utils/recoil/error';
-import ErrorPage from 'components/error/Error';
+import AgendaErrorPage from 'components/error/AgendaError';
+import TakguErrorPage from 'components/error/Error';
 import styles from 'styles/takgu/Layout/Layout.module.scss';
-
 interface ErrorCheckerProps {
   children: React.ReactNode;
 }
 
+// 에러 체커 컴포넌트
+// 추후 takgu쪽과 디자인을 어느정도 맞춘 후 통합할 예정
 export default function ErrorChecker({ children }: ErrorCheckerProps) {
   const error = useRecoilValue(errorState);
+  const router = useRouter();
+  const app = router.pathname ? router.pathname.split('/')[1] : '';
 
   return error === '' ? (
     <>{children}</>
-  ) : (
+  ) : app === 'takgu' ? (
     <div className={styles.appContainer}>
       <div className={styles.background}>
-        <ErrorPage />
+        <TakguErrorPage />
       </div>
     </div>
+  ) : (
+    <AgendaErrorPage />
   );
 }
