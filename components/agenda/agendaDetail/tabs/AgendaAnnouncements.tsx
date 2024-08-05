a52c3aeaa6498e3748364adb2d3f17864534b3fb
irenee-14 <ireneee145@gmail.com>
2024-08-05 14:03:46 +0900

    Feat: 공지사항 입력 버튼 추가

--- components/agenda/agendaDetail/tabs/AgendaAnnouncements.tsx
+++ components/agenda/agendaDetail/tabs/AgendaAnnouncements.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { AnnouncementProps } from 'types/agenda/agendaDetail/announcementTypes';
 import AnnouncementItem from 'components/agenda/agendaDetail/tabs/AnnouncementItem';
+import { UploadBtn } from 'components/agenda/button/UploadBtn';
 import styles from 'styles/agenda/agendaDetail/tabs/AgendaAnnouncements.module.scss';
 
 const mockData = [
@@ -18,7 +19,11 @@ const mockData = [
   },
 ];
 
-export default function AgendaAnnouncements() {
+const newAnnoucnemet = () => {
+  alert('새로운 공지사항을 추가합니다.');
+};
+
+export default function AgendaAnnouncements({ isHost }: { isHost: boolean }) {
   const [announcementData, setannouncementData] = useState<
     AnnouncementProps[] | null
   >(null);
@@ -43,6 +48,12 @@ export default function AgendaAnnouncements() {
             createdAt={item.createdAt}
           />
         ))}
+
+        {isHost ? (
+          <div className={styles.buttonWarp}>
+            <UploadBtn text='공지사항 추가' onClick={newAnnoucnemet} />
+          </div>
+        ) : null}
       </div>
     </>
   );
