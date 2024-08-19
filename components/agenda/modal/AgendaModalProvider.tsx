81f7a52c844b7bfab0c278cde41c4397e521b524
irenee-14 <ireneee145@gmail.com>
2024-08-19 16:11:27 +0900

    Fix: modal 클릭이벤트 범위 변경 및 onCancel, onProceed 후 closeModal 실행되도록 변경

--- components/agenda/modal/AgendaModalProvider.tsx
+++ components/agenda/modal/AgendaModalProvider.tsx
@@ -1,5 +1,6 @@
 import { useRecoilState } from 'recoil';
 import { agendaModalState } from 'utils/recoil/agenda/modalState';
+import ModifyModal from 'components/agenda/modal/ModifyModal';
 import NoticeModal from 'components/agenda/modal/NoticeModal';
 import ProceedCheckModal from 'components/agenda/modal/ProceedCheckModal';
 import { useModal } from 'components/agenda/modal/useModal';
@@ -10,7 +11,6 @@ const AgendaModalProvider = () => {
   const { closeModal } = useModal();
 
   let modalContent;
-
   if (modalProps) {
     switch (modalProps.type) {
       case 'proceedCheck':
@@ -23,11 +23,20 @@ const AgendaModalProvider = () => {
         modalContent = null;
     }
   }
+  const closeModalHandler = (e: React.MouseEvent) => {
+    if (e.target instanceof HTMLDivElement && e.target.id === 'modalOutside') {
+      closeModal();
+    }
+  };
 
   return (
     <>
       {modalProps && (
-        <div className={styles.backdrop} id='modalOutside' onClick={closeModal}>
+        <div
+          className={styles.backdrop}
+          id='modalOutside'
+          onClick={closeModalHandler}
+        >
           {modalContent}
         </div>
       )}
