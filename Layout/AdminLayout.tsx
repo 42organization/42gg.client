1d9b1ececd36926980619488285af826c43f89a3
irenee-14 <ireneee145@gmail.com>
2024-08-20 12:35:01 +0900

    Feat: 경로에 따른 modal provider 분리

--- Layout/AdminLayout.tsx
+++ Layout/AdminLayout.tsx
@@ -1,6 +1,8 @@
-// import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import AdminReject from 'components/admin/AdminReject';
 import AdminLayout from 'components/admin/Layout';
+import AgendaModalProvider from 'components/agenda/modal/AgendaModalProvider';
+import ModalProvider from 'components/takgu/modal/ModalProvider';
 import { useUser } from 'hooks/agenda/Layout/useUser';
 
 type AdminLayoutProps = {
@@ -9,12 +11,27 @@ type AdminLayoutProps = {
 
 function AdminAppLayout({ children }: AdminLayoutProps) {
   const user = useUser();
+  const presentPath = usePathname();
 
+  // 사용자 정보가 없거나 관리자가 아닐 경우
   if (!user || !user.intraId) return null;
 
   if (!user.isAdmin) return <AdminReject />;
 
-  return <AdminLayout>{children}</AdminLayout>;
+  // 모달 제공자 결정
+  let ModalProviderComponent;
+  if (presentPath.includes('admin/takgu')) {
+    ModalProviderComponent = ModalProvider;
+  } else if (presentPath.includes('admin/agenda')) {
+    ModalProviderComponent = AgendaModalProvider;
+  }
+
+  return (
+    <AdminLayout>
+      {children}
+      {ModalProviderComponent && <ModalProviderComponent />}
+    </AdminLayout>
+  );
 }
 
 export default AdminAppLayout;
