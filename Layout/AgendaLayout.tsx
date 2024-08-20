3d8bede1bea6f952e76af7e2796a2bfae03620e6
siwolee <siwolee@student.42seoul.kr>
2024-08-20 21:14:26 +0900

    style: header

--- Layout/AgendaLayout.tsx
+++ Layout/AgendaLayout.tsx
@@ -1,6 +1,8 @@
 // import { useRouter } from 'next/router';
+import AgendaHeader from 'components/agenda/Layout/AgendaHeader';
 import AgendaModalProvider from 'components/agenda/modal/AgendaModalProvider';
 import Footer from 'components/takgu/Layout/Footer';
+import Agenda from 'pages/agenda';
 import { useUser } from 'hooks/agenda/Layout/useUser';
 import styles from 'styles/agenda/Layout/Layout.module.scss';
 
@@ -16,6 +18,7 @@ function AgendaAppLayout({ children }: AgendaLayoutProps) {
   return (
     <>
       <div className={styles.background}>
+        <AgendaHeader />
         {children}
         <Footer />
       </div>
--- components/agenda/Layout/AgendaHeader.tsx
+++ components/agenda/Layout/AgendaHeader.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
 import { FiMenu } from 'react-icons/fi';
+import Logo from 'public/image/main-logo.svg';
 import styles from 'styles/agenda/Layout/Header.module.scss';
-
 export default function AgendaHeader() {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerWrap}>
         <div className={styles.headerLeft}>
-          <FiMenu className={styles.menuIcon} />
-          <Link className={styles.logoWrap} href={'/agenda'}>
-            42AGENDA
-          </Link>
+          <Logo className={styles.logo} />
+          {/* <img src='/image/main-logo.svg' className={styles.logo} /> */}
         </div>
+        <FiMenu className={styles.menuIcon} />
       </div>
     </div>
   );
