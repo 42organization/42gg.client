ec35ee3142d6fda0fab3ea72acb6a5079cbe17fe
siwolee <siwolee@student.42seoul.kr>
2024-08-21 17:15:02 +0900

    fix: 페이지네이션으로 바뀌어서 파싱 터진 부분

--- pages/agenda/ticket/history.tsx
+++ pages/agenda/ticket/history.tsx
@@ -1,23 +1,26 @@
+import { TicketHistoryAPIProps } from 'types/aganda/ticketTypes';
 import TicketHistory from 'components/agenda/Ticket/TicketHistory';
+import PageNation from 'components/Pagination';
 import useFetchGet from 'hooks/agenda/useFetchGet';
 
-interface TicketHistoryProps {
-  createdAt: string; // 발급 시작한 시
-  issuedFrom: string; // Agenda 이름 or 42Intra
-  issuedFromKey: string | null; // AgendaKey UUID or null
-  usedTo: string; // Agenda 이름 or NotUsed or NotApporve
-  usedToKey: string | null; // AgendaKey UUID or null
-  approved: boolean; // true 발급완료, false 발급대기
-  approvedAt: string | null; // 발급된 시간 or null
-  isUsed: boolean; // true 사용된 티켓, false 사용하지 않음
-  usedAt: string | null; //
-}
-
 const TicketHistoryPage = () => {
-  const history = useFetchGet<TicketHistoryProps[]>(
+  const history = useFetchGet<TicketHistoryAPIProps>(
     '/ticket/history?page=1&size=20'
   );
-  return <TicketHistory data={history.data} />;
+  if (!history.data) history.data = { totalSize: 0, content: [] };
+
+  return (
+    <div>
+      <TicketHistory data={history.data.content} />
+      <PageNation
+        curPage={1}
+        totalPages={1}
+        pageChangeHandler={(pageNumber: number) => {
+          console.log(pageNumber);
+        }}
+      />
+    </div>
+  );
 };
 
 export default TicketHistoryPage;
--- types/aganda/ticketTypes.ts
+++ types/aganda/ticketTypes.ts
@@ -1,1 +1,17 @@
 // ticketType :
+export interface TicketHistoryAPIProps {
+  totalSize: number;
+  content: TicketHistoryProps[] | null;
+}
+
+export interface TicketHistoryProps {
+  createdAt: string; // 발급 시작한 시
+  issuedFrom: string; // Agenda 이름 or 42Intra
+  issuedFromKey: string | null; // AgendaKey UUID or null
+  usedTo: string; // Agenda 이름 or NotUsed or NotApporve
+  usedToKey: string | null; // AgendaKey UUID or null
+  approved: boolean; // true 발급완료, false 발급대기
+  approvedAt: string | null; // 발급된 시간 or null
+  isUsed: boolean; // true 사용된 티켓, false 사용하지 않음
+  usedAt: string | null; //}
+}
