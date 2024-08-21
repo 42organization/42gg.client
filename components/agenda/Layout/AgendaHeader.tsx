e9ef03e357225cccefa2eabc59596a957b30e5ae
siwolee <siwolee@student.42seoul.kr>
2024-08-21 12:42:30 +0900

    feat: 메뉴 상태 추가

--- components/agenda/Layout/AgendaHeader.tsx
+++ components/agenda/Layout/AgendaHeader.tsx
@@ -1,17 +1,46 @@
 import Link from 'next/link';
+import { createContext, Dispatch, SetStateAction, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import Logo from 'public/image/main-logo.svg';
 import styles from 'styles/agenda/Layout/Header.module.scss';
+
+interface HeaderContextState {
+  openMenuState: boolean;
+  setOpenMenuBarState: Dispatch<SetStateAction<boolean>>;
+  resetOpenMenuBarState: () => void;
+}
+
+const HeaderContext = createContext<HeaderContextState | null>(null);
+
 export default function AgendaHeader() {
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const resetMenuHandler = () => {
+    setMenu(false);
+  };
+
+  const HeaderState: HeaderContextState = {
+    openMenuState: menu,
+    setOpenMenuBarState: setMenu,
+    resetOpenMenuBarState: resetMenuHandler,
+  };
+
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.headerWrap}>
-        <div className={styles.headerLeft}>
-          <Logo className={styles.logo} />
-          {/* <img src='/image/main-logo.svg' className={styles.logo} /> */}
+    <HeaderContext.Provider value={HeaderState}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerWrap}>
+          <div className={styles.headerLeft}>
+            <Link href='/'>
+              <Logo className={styles.logo} />
+            </Link>
+          </div>
+          <FiMenu
+            className={styles.menuIcon}
+            onClick={() => HeaderState?.setOpenMenuBarState(!menu)}
+          />
         </div>
-        <FiMenu className={styles.menuIcon} />
       </div>
-    </div>
+      {HeaderState?.openMenuState && <div>menu</div>}
+    </HeaderContext.Provider>
   );
 }
