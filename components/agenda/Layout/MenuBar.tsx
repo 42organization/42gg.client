7b9956f5c804d6468c7a38cdd00a456c16deba34
siwolee <siwolee@student.42seoul.kr>
2024-08-21 16:06:36 +0900

    feat: admin 추가 및 내용물 변경

--- components/agenda/Layout/MenuBar.tsx
+++ components/agenda/Layout/MenuBar.tsx
@@ -1,13 +1,26 @@
+import { useUser } from 'hooks/agenda/Layout/useUser';
 import styles from 'styles/agenda/Layout/MenuBar.module.scss';
 import MenuBarContent from './MenuBarContent';
 
 const MenuBar = () => {
+  const user = useUser();
+
   return (
     <div className={styles.container}>
-      <MenuBarContent content='Agenda' href='/agenda/ticket' as='h1' />
+      <MenuBarContent
+        content={`Hello. ${user?.intraId}`}
+        href='/agenda/profile'
+        as='h2'
+      />
+      <MenuBarContent content='Agenda' href='/agenda' as='h1' />
+      <div className={styles.divider} />
       <MenuBarContent content='진행중인 대회' href='/agenda' />
-      <MenuBarContent content='프로필' href='/agenda/profile' />
       <MenuBarContent content='티켓 확인하기' href='/agenda/ticket' />
+      {user?.isAdmin && (
+        <>
+          <MenuBarContent content='admin' href='/agenda/admin' as='h1' />
+        </>
+      )}
     </div>
   );
 };
