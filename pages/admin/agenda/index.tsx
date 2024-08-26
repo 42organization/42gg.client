8c1ea020ef5a24abeb8a4c3c9666d11df849472c
irenee-14 <ireneee145@gmail.com>
2024-08-26 13:14:04 +0900

    Feat: admin takgu, agenda index 페이지

---
+++ pages/admin/agenda/index.tsx
@@ -0,0 +1,18 @@
+const AdminAgenda = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2rem',
+      }}
+    >
+      agenda admin main page
+    </div>
+  );
+};
+
+export default AdminAgenda;
---
+++ pages/admin/takgu/index.tsx
@@ -0,0 +1,18 @@
+const AdminTakgu = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2rem',
+      }}
+    >
+      42gg admin main page
+    </div>
+  );
+};
+
+export default AdminTakgu;
