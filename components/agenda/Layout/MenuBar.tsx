c86c0220cdecd811b1c41366788f0c144ab14dec
siwolee <siwolee@student.42seoul.kr>
2024-08-29 00:44:28 +0900

    feat: logout 모달(api필요)

--- components/agenda/Layout/MenuBar.tsx
+++ components/agenda/Layout/MenuBar.tsx
@@ -1,9 +1,39 @@
+import { useRouter } from 'next/router';
+import { useQueryClient } from 'react-query';
 import { useUser } from 'hooks/agenda/Layout/useUser';
 import styles from 'styles/agenda/Layout/MenuBar.module.scss';
 import MenuBarContent from './MenuBarContent';
+import { useModal } from '../modal/useModal';
 
 const MenuBar = ({ isActive }: { isActive: boolean }) => {
   const user = useUser();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { openModal, closeModal } = useModal();
+
+  const handleLogout = () => {
+    openModal({
+      type: 'proceedCheck',
+      title: '로그아웃',
+      description: '로그아웃 하시겠습니까?',
+      onProceed: () => {
+        // fetch('/api/user/logout', {
+        //   method: 'POST',
+        //   credentials: 'include',
+        // }).then((res) => {
+        //   if (res.status === 204) {
+        //     queryClient.removeQueries('user').then(() => {
+        //       router.push('/agenda/login');
+        //     });
+        //   }
+        // });
+
+        alert('로그아웃 api가 필요합니다.');
+        closeModal();
+      },
+      onCancel: closeModal,
+    });
+  };
 
   return (
     <>
@@ -26,6 +56,7 @@ const MenuBar = ({ isActive }: { isActive: boolean }) => {
             <MenuBarContent content='admin' href='/agenda/admin' as='h1' />
           </>
         )}
+        <MenuBarContent content='로그아웃' onClick={handleLogout} />
       </div>
 
       <div
