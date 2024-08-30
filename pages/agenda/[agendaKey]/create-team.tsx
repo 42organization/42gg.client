84a58eb157d41e1503972331f9c8f730879467a0
siwolee <siwolee@student.42seoul.kr>
2024-08-30 16:01:20 +0900

    style: mobile: 전체 컨테이너 간격 조정

--- pages/agenda/[agendaKey]/create-team.tsx
+++ pages/agenda/[agendaKey]/create-team.tsx
@@ -13,15 +13,17 @@ const CreateTeam = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>팀 만들기</h2>
-      <p className={styles.description}>팀 인원 : 3명-8명까지 가능</p>
-      <CreateTeamForm
-        location={agendaInfo?.agendaLocation || null}
-        agendaKey={agendaKey as string}
-        handleConvert={backPage}
-        onProceed={backPage}
-      />
+    <div className={styles.pageContainer}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>팀 만들기</h2>
+        <p className={styles.description}>팀 인원 : 3명-8명까지 가능</p>
+        <CreateTeamForm
+          location={agendaInfo?.agendaLocation || null}
+          agendaKey={agendaKey as string}
+          handleConvert={backPage}
+          onProceed={backPage}
+        />
+      </div>
     </div>
   );
 };
--- pages/agenda/create.tsx
+++ pages/agenda/create.tsx
@@ -10,17 +10,19 @@ export const saveLocal = () => {
 
 const CreateAgenda = () => {
   return (
-    <div className={styles.container}>
-      <div>
-        <CancelBtn onClick={saveLocal} />
+    <div className={styles.pageContainer}>
+      <div className={styles.container}>
+        <div>
+          <CancelBtn onClick={saveLocal} />
+        </div>
+        <h2 className={styles.title}>새로운 아젠다 만들기</h2>
+        <p className={styles.description}>당부사항</p>
+        <AgendaForm
+          onProceed={() => {
+            router.push(`/agenda`);
+          }}
+        />
       </div>
-      <h2 className={styles.title}>새로운 아젠다 만들기</h2>
-      <p className={styles.description}>당부사항</p>
-      <AgendaForm
-        onProceed={() => {
-          router.push(`/agenda`);
-        }}
-      />
     </div>
   );
 };
