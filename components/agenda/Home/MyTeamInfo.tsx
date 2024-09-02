f6f86a7ffd415f73b07da6a6abad6464446c8aa2
siwolee <siwolee@student.42seoul.kr>
2024-09-02 17:14:24 +0900

    fix: 태깅 수정

--- components/agenda/Home/MyTeamInfo.tsx
+++ components/agenda/Home/MyTeamInfo.tsx
@@ -15,9 +15,8 @@ const MyTeamInfo = ({
   if (!myTeamInfo) {
     return <div>참가중인 일정이 없습니다.</div>;
   }
-  // myTeamInfo.isOfficial = true; //css test
   const startDate = new Date(myTeamInfo.agendaStartTime as string);
-  //아래 주석달린 부분은 api 변경시 추가 예정입니다.
+
   return (
     <div className={styles.Container} key={idx}>
       <div className={agendastyles.agendaDateBox}>
@@ -31,17 +30,17 @@ const MyTeamInfo = ({
       </div>
       <div className={styles.infoContainer}>
         <div className={styles.teamInfoContainer}>
-          <h2 className={styles.teamTitle}>
-            팀이름 : {myTeamInfo.agendaTitle}
-          </h2>
-          {/* <div className={styles.agendaStatus}>
-          {myTeamInfo.isOfficial ? <AgendaTag tagName='공식' /> : ''}
-        </div> */}
+          <h2 className={styles.teamTitle}>{myTeamInfo.teamName}</h2>
+          <div className={styles.agendaTagBox}>
+            {myTeamInfo.teamStatus === 'OPEN' ? (
+              <AgendaTag tagName='모집중' />
+            ) : (
+              <AgendaTag tagName='확정팀' />
+            )}
+          </div>
         </div>
         <div className={styles.agendaInfoContainer}>
-          <h3 className={styles.agendaTitle}>
-            아젠다 : {myTeamInfo.agendaTitle}
-          </h3>
+          <h3 className={styles.agendaTitle}>{myTeamInfo.agendaTitle}</h3>
           <div className={styles.agendaTagBox}>
             {myTeamInfo.isOfficial ? <AgendaTag tagName='공식' /> : ''}
           </div>
