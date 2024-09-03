2407e21061ba4a7e2de3a8811474e93ea51392ee
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-09-03 15:43:22 +0900

    Feat: 주최자 관리 버튼들 추가 + 버튼 클릭 이벤트 설정

--- pages/agenda/[agendaKey]/host/modify.tsx
+++ pages/agenda/[agendaKey]/host/modify.tsx
@@ -1,14 +1,47 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { UploadBtn } from 'components/agenda/button/UploadBtn';
+import { useModal } from 'components/agenda/modal/useModal';
 import { useAgendaInfo } from 'hooks/agenda/useAgendaInfo';
+import useFetchRequest from 'hooks/agenda/useFetchRequest';
 import { useUser } from 'hooks/takgu/Layout/useUser';
-import styles from 'styles/agenda/pages/CreateAgenda.module.scss';
+import styles from 'styles/agenda/pages/agendakey/host/modify.module.scss';
 
 const ModifyAgenda = () => {
   const router = useRouter();
   const { agendaKey } = router.query;
   const agendaData = useAgendaInfo(agendaKey as string);
   const user = useUser();
+  const { sendRequest } = useFetchRequest();
+  const { openModal } = useModal();
+
+  const handleClick = (description: string, onProceed: () => void) => {
+    openModal({
+      type: 'proceedCheck',
+      description: description,
+      onProceed: onProceed,
+    });
+  };
+
+  const newAnnouncement = () => {
+    router.push(`/agenda/${agendaKey}/host/createAnnouncement`);
+  };
+
+  const confirmAgenda = () => {
+    handleClick('아젠다를 확정지으시겠습니까?', () => {
+      sendRequest('PATCH', `confirm`, {}, { agenda_key: agendaKey });
+    });
+  };
+
+  const cancelAgenda = () => {
+    handleClick('아젠다를 취소하시겠습니까?', () => {
+      sendRequest('PATCH', `cancel`, {}, { agenda_key: agendaKey });
+    });
+  };
+
+  const resultAgenda = () => {
+    router.push(`/agenda/${agendaKey}/host/result`);
+  };
 
   useEffect(() => {
     // 주최자 확인
@@ -23,10 +56,10 @@ const ModifyAgenda = () => {
   return (
     <>
       <div className={styles.container}>
-        {/* 공지사항 추가 */}
-        {/* 아젠다 확정 */}
-        {/* 아젠다 취소 */}
-        {/* 아젠다 결과작성 */}
+        <UploadBtn text='공지사항 추가' onClick={newAnnouncement} />
+        <UploadBtn text='아젠다 확정' onClick={confirmAgenda} />
+        <UploadBtn text='아젠다 취소' onClick={cancelAgenda} />
+        <UploadBtn text='아젠다 결과 작성' onClick={resultAgenda} />
       </div>
     </>
   );
