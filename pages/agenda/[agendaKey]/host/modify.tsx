b6f28d2ec3f1e897b7e9ca65538a13aaaf39b4a6
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-09-03 12:46:51 +0900

    Feat: 주최자 권한 아니면 alert 및 뒤로가기

--- pages/agenda/[agendaKey]/host/modify.tsx
+++ pages/agenda/[agendaKey]/host/modify.tsx
@@ -1,33 +1,32 @@
 import { useRouter } from 'next/router';
-import { CancelBtn } from 'components/agenda/button/Buttons';
-import AgendaForm from 'components/agenda/Form/AgendaForm';
-import { saveLocal } from 'pages/agenda/create';
+import { useEffect } from 'react';
 import { useAgendaInfo } from 'hooks/agenda/useAgendaInfo';
+import { useUser } from 'hooks/takgu/Layout/useUser';
 import styles from 'styles/agenda/pages/CreateAgenda.module.scss';
 
 const ModifyAgenda = () => {
   const router = useRouter();
   const { agendaKey } = router.query;
-  const data = useAgendaInfo(agendaKey as string);
+  const agendaData = useAgendaInfo(agendaKey as string);
+  const user = useUser();
+
+  useEffect(() => {
+    // 주최자 확인
+    if (agendaData && user) {
+      if (agendaData.agendaHost !== user.intraId) {
+        alert('주최자만 접근 가능한 페이지입니다.');
+        router.back();
+      }
+    }
+  }, [agendaData, user, router]);
 
   return (
     <>
       <div className={styles.container}>
-        <div>
-          <CancelBtn onClick={saveLocal} />
-        </div>
-        <h2 className={styles.title}>아젠다 수정하기</h2>
-        <p className={styles.description}>당부사항</p>
-        {data && (
-          <AgendaForm
-            isEdit={true}
-            data={data}
-            stringKey={agendaKey as string}
-            onProceed={() => {
-              router.push(`/agenda/${agendaKey}`);
-            }}
-          />
-        )}
+        {/* 공지사항 추가 */}
+        {/* 아젠다 확정 */}
+        {/* 아젠다 취소 */}
+        {/* 아젠다 결과작성 */}
       </div>
     </>
   );
