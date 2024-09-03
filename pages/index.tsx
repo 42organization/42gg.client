f96e87982529467e279c1d882e0779770bb9af68
siwolee <siwolee@student.42seoul.kr>
2024-09-03 15:52:25 +0900

    style: index 페이지 레이아웃 복구

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,68 +17,58 @@ const Index: NextPage = () => {
   };
 
   return (
-    <>
-      <AgendaHeader />
-      <div className={styles.background} />
-      <div className={styles.app}>
-        <div className={styles.layout}>
-          <div className={styles.top}>
-            <h2
-              className={styles.title}
-              onClick={() => handleNavigation('/agenda')}
-            >
-              Agenda
-            </h2>
-            <PageController handleNavigation={handleNavigation} />
-          </div>
-          <div className={styles.flex}>
-            <div className={styles.ticket}>
-              <h2 className={styles.title}>Ticket</h2>
-              <button
-                className={styles.container}
-                onClick={() => handleNavigation('/agenda/ticket')}
-              >
-                <Image
-                  src='/image/ticket.png'
-                  alt='ticket'
-                  width={0}
-                  height={0}
-                  style={{ width: '100%', height: '100%' }}
-                />
-              </button>
-            </div>
-            <div className={styles.pingpong}>
-              <h2
-                className={styles.title}
-                onClick={() => handleNavigation('/takgu')}
-              >
-                PingPong
-              </h2>
-              <button
-                className={styles.container}
-                onClick={() => handleNavigation('/takgu')}
-              >
-                <PingpongIcon width='100%' height='100%' />
-              </button>
-            </div>
-          </div>
-
-          <div className={styles.match}>
-            <h2
-              className={styles.title}
-              // onClick={() => handleNavigation('/takgu')}
-            >
-              Outer match
-            </h2>
-            <button className={styles.container}>
-              아우터 매치 준비중입니다.
-            </button>
-          </div>
-        </div>
-        <Footer />
-        <AgendaModalProvider />
+    <div className={styles.layout}>
+      <div className={styles.top}>
+        <h2
+          className={styles.title}
+          onClick={() => handleNavigation('/agenda')}
+        >
+          Agenda
+        </h2>
+        <PageController handleNavigation={handleNavigation} />
       </div>
-    </>
+      <div className={styles.flex}>
+        <div className={styles.ticket}>
+          <h2 className={styles.title}>Ticket</h2>
+          <button
+            className={styles.container}
+            onClick={() => handleNavigation('/agenda/ticket')}
+          >
+            <Image
+              src='/image/ticket.png'
+              alt='ticket'
+              width={0}
+              height={0}
+              style={{ width: '100%', height: '100%' }}
+            />
+          </button>
+        </div>
+        <div className={styles.pingpong}>
+          <h2
+            className={styles.title}
+            onClick={() => handleNavigation('/takgu')}
+          >
+            PingPong
+          </h2>
+          <button
+            className={styles.container}
+            onClick={() => handleNavigation('/takgu')}
+          >
+            <PingpongIcon width='100%' height='100%' />
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.match}>
+        <h2
+          className={styles.title}
+          // onClick={() => handleNavigation('/takgu')}
+        >
+          Outer match
+        </h2>
+        <button className={styles.container}>아우터 매치 준비중입니다.</button>
+      </div>
+    </div>
   );
 };
 
