f6a598e3da7aca6d0fb2ace3ab1c9a5dab81e61b
irenee-14 <ireneee145@gmail.com>
2024-09-04 16:50:23 +0900

    Style: 팀별 참가 시 webview로 변경하면 내팀/모집중팀/확정팀 가로로 나열

--- components/agenda/agendaDetail/tabs/ParticipantTeamList.tsx
+++ components/agenda/agendaDetail/tabs/ParticipantTeamList.tsx
@@ -42,70 +42,72 @@ export default function ParticipantTeamList({
 
   return (
     <>
-      <div className={styles.participantsWarp}>
-        <div className={styles.participantsTitle}>내 팀</div>
-        {myTeam ? (
-          <>
-            <ParticipantTeam
-              key={myTeam.teamName}
-              teamKey={myTeam.teamKey}
-              teamName={myTeam.teamName}
-              teamLeaderIntraId={myTeam.teamLeaderIntraId}
-              teamMateCount={myTeam.teamMateCount}
-              maxMateCount={maxTeam}
-              coalitions={myTeam.coalitions}
-            />
-          </>
-        ) : (
-          noParticipants
-        )}
-      </div>
-
-      <div className={styles.participantsWarp}>
-        <div className={styles.participantsTitle}>
-          모집중인 팀 {openTeamsCount}
-        </div>
-        {openTeams && openTeamsCount > 0 ? (
-          <>
-            {openTeams.map((team) => (
+      <div className={styles.participantsContainer}>
+        <div className={styles.participantsWarp}>
+          <div className={styles.participantsTitle}>내 팀</div>
+          {myTeam ? (
+            <>
               <ParticipantTeam
-                key={team.teamName}
-                teamKey={team.teamKey}
-                teamName={team.teamName}
-                teamLeaderIntraId={team.teamLeaderIntraId}
-                teamMateCount={team.teamMateCount}
-                maxMateCount={maxPeople}
-                coalitions={team.coalitions}
+                key={myTeam.teamName}
+                teamKey={myTeam.teamKey}
+                teamName={myTeam.teamName}
+                teamLeaderIntraId={myTeam.teamLeaderIntraId}
+                teamMateCount={myTeam.teamMateCount}
+                maxMateCount={maxTeam}
+                coalitions={myTeam.coalitions}
               />
-            ))}
-            <PageNation {...openTeamPageNationProps} />
-          </>
-        ) : (
-          noParticipants
-        )}
-      </div>
-
-      <div className={styles.participantsWarp}>
-        <div className={styles.participantsTitle}>
-          확정완료 팀 {confirmedTeamsCount} / {maxTeam}
+            </>
+          ) : (
+            noParticipants
+          )}
         </div>
-        {confirmedTeams && confirmedTeamsCount > 0 ? (
-          <>
-            {confirmedTeams.map((team) => (
-              <ParticipantTeam
-                key={team.teamName}
-                teamName={team.teamName}
-                teamLeaderIntraId={team.teamLeaderIntraId}
-                teamMateCount={team.teamMateCount}
-                maxMateCount={maxPeople}
-                coalitions={team.coalitions}
-              />
-            ))}
-            <PageNation {...confirmedTeamPageNationProps} />
-          </>
-        ) : (
-          noParticipants
-        )}
+
+        <div className={styles.participantsWarp}>
+          <div className={styles.participantsTitle}>
+            모집중인 팀 {openTeamsCount}
+          </div>
+          {openTeams && openTeamsCount > 0 ? (
+            <>
+              {openTeams.map((team) => (
+                <ParticipantTeam
+                  key={team.teamName}
+                  teamKey={team.teamKey}
+                  teamName={team.teamName}
+                  teamLeaderIntraId={team.teamLeaderIntraId}
+                  teamMateCount={team.teamMateCount}
+                  maxMateCount={maxPeople}
+                  coalitions={team.coalitions}
+                />
+              ))}
+              <PageNation {...openTeamPageNationProps} />
+            </>
+          ) : (
+            noParticipants
+          )}
+        </div>
+
+        <div className={styles.participantsWarp}>
+          <div className={styles.participantsTitle}>
+            확정완료 팀 {confirmedTeamsCount} / {maxTeam}
+          </div>
+          {confirmedTeams && confirmedTeamsCount > 0 ? (
+            <>
+              {confirmedTeams.map((team) => (
+                <ParticipantTeam
+                  key={team.teamName}
+                  teamName={team.teamName}
+                  teamLeaderIntraId={team.teamLeaderIntraId}
+                  teamMateCount={team.teamMateCount}
+                  maxMateCount={maxPeople}
+                  coalitions={team.coalitions}
+                />
+              ))}
+              <PageNation {...confirmedTeamPageNationProps} />
+            </>
+          ) : (
+            noParticipants
+          )}
+        </div>
       </div>
     </>
   );
