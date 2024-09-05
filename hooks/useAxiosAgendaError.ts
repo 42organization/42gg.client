6b47dee22d31c9292c103aa1d68a14dc6bb0b2ab
Kimmy Haecho <118002501+siwolee@users.noreply.github.com>
2024-09-05 21:52:46 +0900

    fix: 400 404 에러페이지이동 취소

--- hooks/useAxiosAgendaError.ts
+++ hooks/useAxiosAgendaError.ts
@@ -55,20 +55,6 @@ export default function useAxiosAgendaError() {
           }
         } else return defaultReturn();
       }
-      case 400:
-        if (error.config.method === 'get') {
-          setError({
-            msg: '데이터를 가져올 수 없습니다.',
-            status: error.response.status,
-          });
-        }
-        break;
-      case 404:
-        setError({
-          msg: '문제가 발생했습니다.',
-          status: error.response.status,
-        });
-        break;
       case 500:
         setError({
           msg: '서버 오류가 발생했습니다',
