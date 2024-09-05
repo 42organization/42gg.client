a0221ce7d8cd7dcd910b01f4325ee4e7a4fee584
Kimmy Haecho <118002501+siwolee@users.noreply.github.com>
2024-09-05 16:02:55 +0900

    fix: agenda useUser setError axios핸들러로 이동

--- utils/recoil/agendaError.ts
+++ utils/recoil/agendaError.ts
@@ -1,12 +1,12 @@
 import { atom } from 'recoil';
 import { v1 } from 'uuid';
 
-interface ErrorState {
+export interface ErrorStateType {
   msg: string;
   status: number;
 }
 
-export const agendaErrorState = atom<ErrorState>({
+export const agendaErrorState = atom<ErrorStateType>({
   key: `errorState/${v1()}`,
   default: { msg: '', status: 0 },
 });
