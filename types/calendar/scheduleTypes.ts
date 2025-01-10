949c8f7d464934a8cd3fe0c98d2dd6fa3c09ff13
wwwlnyy <wwwlnyy@gmail.com>
2025-01-10 19:15:23 +0900

    Feat: 스케줄 관련 hook 생성

---
+++ types/calendar/scheduleTypes.ts
@@ -0,0 +1,19 @@
+export interface Schedule {
+  id?: number; //post할땐 필요없음 get에서 필요
+  classification?: string; //전체일정, 개인일정은 get 할때만
+  eventTag?: string; //EVENT
+  jobTag?: string; //JOB_NOTICE
+  techTag?: string; //JOB_NOTICE
+  author?: string; //전체일정
+  title: string;
+  content: string;
+  link: string;
+  startTime: string;
+  endTime: string;
+  sharedCount?: number; //상세조회에서 필요
+  status?: string; //get에서 필요
+  alarm?: true; //개인일정
+  groupId?: number; //개인일정
+  groupTitle?: string; //개인일정
+  groupColor?: string; //개인일정
+}
