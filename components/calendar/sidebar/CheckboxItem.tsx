b20c32e4da2310290570f255c27474dfe4ed26c3
이나영 <wwwlnyy@gmail.com>
2025-04-03 19:41:58 +0900

    Fix: detail 모달에서 content 길어질 때 깨지는 문제 해결

--- components/calendar/sidebar/CheckboxItem.tsx
+++ components/calendar/sidebar/CheckboxItem.tsx
@@ -90,7 +90,7 @@ const CheckboxItem = ({
               className={styles.groupField}
             />
           ) : (
-            <span>{name}</span>
+            <div style={{ maxWidth: 100, overflowX: 'scroll' }}>{name}</div>
           )
         }
         sx={{
